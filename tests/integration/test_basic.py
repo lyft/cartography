8a02a98016357b64c4bf95e2a52d0fc69f898bb5
Evan Davis <evandavis@lyft.com>
2019-05-07 11:12:28 -0700

    Add basic integration test.

---
+++ tests/integration/test_basic.py
@@ -0,0 +1,7 @@
+from neo4j.v1 import GraphDatabase
+
+
+def test_neo4j_connection():
+    driver = GraphDatabase.driver("bolt://localhost:7474")
+    with driver.session() as session:
+        session.run("CALL db.schema();")
