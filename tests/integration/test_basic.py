be832cc55c7937eafe5c1889d682b0f64bdb872e
Evan Davis <evandavis@lyft.com>
2019-05-07 11:15:46 -0700

    Try default bolt port.

--- tests/integration/test_basic.py
+++ tests/integration/test_basic.py
@@ -2,6 +2,6 @@ from neo4j.v1 import GraphDatabase
 
 
 def test_neo4j_connection():
-    driver = GraphDatabase.driver("bolt://localhost:7474")
+    driver = GraphDatabase.driver("bolt://localhost:7687")
     with driver.session() as session:
         session.run("CALL db.schema();")
