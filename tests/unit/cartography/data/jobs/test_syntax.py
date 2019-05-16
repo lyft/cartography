5fd7ddea569862b3e4627676f0fc1464e2a61a64
Evan Davis <evandavis@lyft.com>
2019-05-16 16:38:39 -0700

    remove fixture

--- tests/unit/cartography/data/jobs/test_syntax.py
+++ tests/unit/cartography/data/jobs/test_syntax.py
@@ -6,7 +6,7 @@ else:
     from importlib_resources import contents, read_text
 
 
-def test_analysis_jobs_actually_execute(neo4j_session):
+def test_analysis_jobs_actually_execute():
     for job_name in contents('cartography.data.jobs.analysis'):
         if not job_name.endswith('.json'):
             continue
@@ -14,7 +14,7 @@ def test_analysis_jobs_actually_execute(neo4j_session):
         json.loads(blob)
 
 
-def test_cleanup_jobs_actually_execute(neo4j_session):
+def test_cleanup_jobs_actually_execute():
     for job_name in contents('cartography.data.jobs.cleanup'):
         if not job_name.endswith('json'):
             continue
