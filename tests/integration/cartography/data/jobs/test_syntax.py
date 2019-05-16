9500cf7c24d3b4be857907b8568ea3b70c4a2c17
Evan Davis <evandavis@lyft.com>
2019-05-16 16:17:25 -0700

    Add syntax tests for jobs.

---
+++ tests/integration/cartography/data/jobs/test_syntax.py
@@ -0,0 +1,16 @@
+if sys.version_info >= (3, 7):
+    from importlib.resources import listdir
+else:
+    from importlib_resources import listdir
+
+import cartography.util
+
+
+def test_analysis_jobs_actually_execute(neo4j_session):
+    for job_name in listdir('cartography.data.jobs.analysis'):
+        cartography.util.run_analysis_job(job_name, neo4j_session, {})
+
+
+def test_cleanup_jobs_actually_execute(neo4j_session):
+    for job_name in listdir('cartography.data.jobs.cleanup'):
+        cartography.util.run_cleanup_job(job_name, neo4j_session, {})
