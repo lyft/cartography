2abed2c269fd5d86804c32608b30cf24be16ca4c
Evan Davis <evandavis@lyft.com>
2019-05-16 16:26:59 -0700

    okokokok

--- tests/integration/cartography/data/jobs/test_syntax.py
+++ tests/integration/cartography/data/jobs/test_syntax.py
@@ -1,16 +1,16 @@
 import cartography.util
 import sys
 if sys.version_info >= (3, 7):
-    from importlib.resources import listdir
+    from importlib.resources import contents
 else:
-    from importlib_resources import listdir
+    from importlib_resources import contents
 
 
 def test_analysis_jobs_actually_execute(neo4j_session):
-    for job_name in listdir('cartography.data.jobs.analysis'):
+    for job_name in contents('cartography.data.jobs.analysis'):
         cartography.util.run_analysis_job(job_name, neo4j_session, {})
 
 
 def test_cleanup_jobs_actually_execute(neo4j_session):
-    for job_name in listdir('cartography.data.jobs.cleanup'):
+    for job_name in contents('cartography.data.jobs.cleanup'):
         cartography.util.run_cleanup_job(job_name, neo4j_session, {})
