25bd451f14a975a453774d3868ade86bdcd69fef
Evan Davis <me@ezdwt.com>
2019-05-16 15:58:51 -0700

    Add integration tests to exercise basic AWS IAM intel module functionality (loading data into Neo4j). (#69)
    * Add neo4j service to Travis CI configuration.
    
    * Add basic integration test.
    
    * Add integration tests to Makefile.
    
    * Try default bolt port.
    
    * Add experimental IAM integration test.
    
    * Fix fixture name.
    
    * Break query
    
    * Revert "Break query"
    
    This reverts commit e287affb2d2f9f5d70bf8047a2c76af120d927b0.
    
    * Make module-scoped fixture available to all integration tests.
    
    * Fix flake errors
    
    * remove blank line at end of file
    
    * Add coverage
    
    * neo4j.v1 -> neo4j
    
    * Add more IAM integration tests.
    
    * Add case to roles test.
    
    * enable missing
    
    * Add CLI integration test.
    
    * Add assertion to CLI test
    
    * Add instructions for running automated tests
    
    * Fix typo.

---
+++ tests/data/aws/iam.py
@@ -0,0 +1,139 @@
+import datetime
+
+
+LIST_USERS = {
+    "Users": [
+        {
+            "UserName": "example-user-0",
+            "PasswordLastUsed": datetime.datetime(2019, 1, 1, 0, 0, 1),
+            "CreateDate": datetime.datetime(2019, 1, 1, 0, 0, 1),
+            "UserId": "AIDA00000000000000000",
+            "Path": "/",
+            "Arn": "arn:aws:iam::000000000000:user/example-user-0"
+        },
+        {
+            "UserName": "example-user-1",
+            "PasswordLastUsed": datetime.datetime(2019, 1, 1, 0, 0, 1),
+            "CreateDate": datetime.datetime(2019, 1, 1, 0, 0, 1),
+            "UserId": "AIDA00000000000000001",
+            "Path": "/",
+            "Arn": "arn:aws:iam::000000000000:user/example-user-1"
+        }
+    ]
+}
+
+
+LIST_GROUPS = {
+    "Groups": [
+        {
+            "Path": "/",
+            "CreateDate": datetime.datetime(2019, 1, 1, 0, 0, 1),
+            "GroupId": "AGPA000000000000000000",
+            "Arn": "arn:aws:iam::000000000000:group/example-group-0",
+            "GroupName": "example-group-0"
+        },
+        {
+            "Path": "/",
+            "CreateDate": datetime.datetime(2019, 1, 1, 0, 0, 1),
+            "GroupId": "AGPA000000000000000001",
+            "Arn": "arn:aws:iam::000000000000:group/example-group-1",
+            "GroupName": "example-group-1"
+        }
+    ]
+}
+
+
+LIST_POLICIES = {
+    "Policies": [
+        {
+            "PolicyName": "example-policy-0",
+            "PermissionsBoundaryUsageCount": 0,
+            "CreateDate": datetime.datetime(2019, 1, 1, 0, 0, 1),
+            "AttachmentCount": 0,
+            "IsAttachable": True,
+            "PolicyId": "ANPA00000000000000000",
+            "DefaultVersionId": "v1",
+            "Path": "/",
+            "Arn": "arn:aws:iam::173840052742:policy/example-policy-0",
+            "UpdateDate": datetime.datetime(2019, 1, 1, 0, 0, 1)
+        },
+        {
+            "PolicyName": "example-policy-1",
+            "PermissionsBoundaryUsageCount": 0,
+            "CreateDate": datetime.datetime(2019, 1, 1, 0, 0, 1),
+            "AttachmentCount": 0,
+            "IsAttachable": True,
+            "PolicyId": "ANPA00000000000000001",
+            "DefaultVersionId": "v1",
+            "Path": "/",
+            "Arn": "arn:aws:iam::173840052742:policy/example-policy-1",
+            "UpdateDate": datetime.datetime(2019, 1, 1, 0, 0, 1)
+        }
+    ]
+}
+
+
+LIST_ROLES = {
+    "Roles": [
+        {
+            "AssumeRolePolicyDocument": {
+                "Version": "2012-10-17",
+                "Statement": [
+                    {
+                        "Action": "sts:AssumeRole",
+                        "Effect": "Allow",
+                        "Principal": {
+                            "AWS": "arn:aws:iam::000000000000:root"
+                        }
+                    }
+                ]
+            },
+            "MaxSessionDuration": 3600,
+            "RoleId": "AROA00000000000000000",
+            "CreateDate": datetime.datetime(2019, 1, 1, 0, 0, 1),
+            "RoleName": "example-role-0",
+            "Path": "/",
+            "Arn": "arn:aws:iam::000000000000:role/example-role-0"
+        },
+        {
+            "AssumeRolePolicyDocument": {
+                "Version": "2012-10-17",
+                "Statement": [
+                    {
+                        "Action": "sts:AssumeRole",
+                        "Effect": "Allow",
+                        "Principal": {
+                            "AWS": "arn:aws:iam::000000000000:role/example-role-0"
+                        }
+                    }
+                ]
+            },
+            "MaxSessionDuration": 3600,
+            "RoleId": "AROA00000000000000001",
+            "CreateDate": datetime.datetime(2019, 1, 1, 0, 0, 1),
+            "RoleName": "example-role-1",
+            "Path": "/",
+            "Arn": "arn:aws:iam::000000000000:role/example-role-1"
+        },
+        {
+            "AssumeRolePolicyDocument": {
+                "Version": "2012-10-17",
+                "Statement": [
+                    {
+                        "Action": "sts:AssumeRole",
+                        "Effect": "Allow",
+                        "Principal": {
+                            "Service": "ec2.amazonaws.com"
+                        }
+                    }
+                ]
+            },
+            "MaxSessionDuration": 3600,
+            "RoleId": "AROA00000000000000002",
+            "CreateDate": datetime.datetime(2019, 1, 1, 0, 0, 1),
+            "RoleName": "example-role-2",
+            "Path": "/",
+            "Arn": "arn:aws:iam::000000000000:role/example-role-2"
+        },
+    ]
+}
