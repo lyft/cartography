f0240d1ba20369193e78d3ef22df190e6227d61d
Thomas Hui Li <thomasl@lyft.com>
2019-05-29 13:11:06 -0700

    Made integration tests configurable (#75)
    * passes integration tests
    
    * updated integration tests with new environment variable NEO4J_URL
    
    * resolved comments and updated testing doc
    
    * deleted duplicate empty line

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -1,10 +1,11 @@
 import neo4j
 import pytest
+from tests.integration import settings
 
 
 @pytest.fixture(scope="module")
 def neo4j_session():
-    driver = neo4j.GraphDatabase.driver("bolt://localhost:7687")
+    driver = neo4j.GraphDatabase.driver(settings.get("NEO4J_URL"))
     with driver.session() as session:
         yield session
         session.run("MATCH (n) DETACH DELETE n;")
