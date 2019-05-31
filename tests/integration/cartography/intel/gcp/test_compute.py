1154abee18721baca50bfb20e82daf8155114e5a
Alex Chantavy <achantavy@lyft.com>
2019-05-31 16:04:10 -0700

    Add mock data to use in test_load_* functions. Remove fixture and explicitly add tests for test_transform_and_load_* functions.

--- tests/integration/cartography/intel/gcp/test_compute.py
+++ tests/integration/cartography/intel/gcp/test_compute.py
@@ -1,4 +1,3 @@
-import pytest
 import cartography.intel.gcp.compute
 import tests.data.gcp.compute
 
@@ -6,33 +5,38 @@ import tests.data.gcp.compute
 TEST_UPDATE_TAG = 123456789
 
 
-@pytest.fixture()
-def load_compute_objects(neo4j_session):
+def _ensure_local_neo4j_has_test_instance_data(neo4j_session):
+    cartography.intel.gcp.compute.load_gcp_instances(
+        neo4j_session,
+        tests.data.gcp.compute.TRANSFORMED_GCP_INSTANCES,
+        TEST_UPDATE_TAG
+    )
+
+
+def _ensure_local_neo4j_has_test_vpc_data(neo4j_session):
+    cartography.intel.gcp.compute.load_gcp_vpcs(
+        neo4j_session,
+        tests.data.gcp.compute.TRANSFORMED_GCP_VPCS,
+        TEST_UPDATE_TAG
+    )
+
+
+def _ensure_local_neo4j_has_test_subnet_data(neo4j_session):
+    cartography.intel.gcp.compute.load_gcp_subnets(
+        neo4j_session,
+        tests.data.gcp.compute.TRANSFORMED_GCP_SUBNETS,
+        TEST_UPDATE_TAG
+    )
+
+
+def test_transform_and_load_vpcs(neo4j_session):
     """
-    Ensure the test neo4j instance has the dummy data that we need for the integration tests
-    :param neo4j_session: The neo4j session fixture
-    :return: Nothing
+    Test that we can correctly transform and load VPC nodes to Neo4j.
     """
-    # Load VPCs
     vpc_res = tests.data.gcp.compute.VPC_RESPONSE
     vpc_list = cartography.intel.gcp.compute.transform_gcp_vpcs(vpc_res)
     cartography.intel.gcp.compute.load_gcp_vpcs(neo4j_session, vpc_list, TEST_UPDATE_TAG)
 
-    # Load subnets
-    subnet_res = tests.data.gcp.compute.VPC_SUBNET_RESPONSE
-    subnet_list = cartography.intel.gcp.compute.transform_gcp_subnets(subnet_res)
-    cartography.intel.gcp.compute.load_gcp_subnets(neo4j_session, subnet_list, TEST_UPDATE_TAG)
-
-    # Load the Instance, NIC, and AccessConfig data
-    instance_responses = [tests.data.gcp.compute.GCP_LIST_INSTANCES_RESPONSE]
-    instance_list = cartography.intel.gcp.compute.transform_gcp_instances(instance_responses)
-    cartography.intel.gcp.compute.load_gcp_instances(neo4j_session, instance_list, TEST_UPDATE_TAG)
-
-
-def test_load_vpcs(load_compute_objects, neo4j_session):
-    """
-    Test that we can correctly transform and load VPC nodes to Neo4j
-    """
     query = """
     MATCH(vpc:GCPVpc{id:{VpcId}})
     RETURN vpc.id, vpc.partial_uri, vpc.auto_create_subnetworks
@@ -49,10 +53,85 @@ def test_load_vpcs(load_compute_objects, neo4j_session):
     assert actual_nodes == expected_nodes
 
 
-def test_vpc_to_subnets(load_compute_objects, neo4j_session):
+def test_transform_and_load_subnets(neo4j_session):
+    """
+    Ensure we can transform and load subnets.
+    """
+    subnet_res = tests.data.gcp.compute.VPC_SUBNET_RESPONSE
+    subnet_list = cartography.intel.gcp.compute.transform_gcp_subnets(subnet_res)
+    cartography.intel.gcp.compute.load_gcp_subnets(neo4j_session, subnet_list, TEST_UPDATE_TAG)
+
+    query = """
+    MATCH(subnet:GCPSubnet)
+    RETURN subnet.id, subnet.region, subnet.gateway_address, subnet.ip_cidr_range, subnet.private_ip_google_access
+    """
+    nodes = neo4j_session.run(query)
+    actual_nodes = set([(
+        n['subnet.id'],
+        n['subnet.region'],
+        n['subnet.gateway_address'],
+        n['subnet.ip_cidr_range'],
+        n['subnet.private_ip_google_access']
+    ) for n in nodes])
+
+    expected_nodes = set([
+        ('projects/project-abc/regions/europe-west2/subnetworks/default',
+         'europe-west2',
+         '10.0.0.1',
+         '10.0.0.0/20',
+         False)
+    ])
+    assert actual_nodes == expected_nodes
+
+
+def test_transform_and_load_gcp_instances_and_nics(neo4j_session):
+    """
+    Ensure that we can correctly transform and load GCP instances.
+    """
+    instance_responses = [tests.data.gcp.compute.GCP_LIST_INSTANCES_RESPONSE]
+    instance_list = cartography.intel.gcp.compute.transform_gcp_instances(instance_responses)
+    cartography.intel.gcp.compute.load_gcp_instances(neo4j_session, instance_list, TEST_UPDATE_TAG)
+
+    instance_id1 = 'projects/project-abc/zones/europe-west2-b/instances/instance-1-test'
+    instance_id2 = 'projects/project-abc/zones/europe-west2-b/instances/instance-1'
+
+    nic_query = """
+    MATCH(i:GCPInstance)-[r:NETWORK_INTERFACE]->(nic:GCPNetworkInterface)
+    RETURN i.id, i.zone_name, i.project_id, i.hostname, r.lastupdated, nic.nic_id, nic.private_ip
+    """
+    objects = neo4j_session.run(nic_query)
+    actual_nodes = set([(
+        o['i.id'],
+        o['i.zone_name'],
+        o['i.project_id'],
+        o['nic.nic_id'],
+        o['nic.private_ip'],
+        o['r.lastupdated']
+    ) for o in objects])
+
+    expected_nodes = set([
+        (instance_id1,
+         'europe-west2-b',
+         'project-abc',
+         'projects/project-abc/zones/europe-west2-b/instances/instance-1-test/networkinterfaces/nic0',
+         '10.0.0.3',
+         TEST_UPDATE_TAG),
+        (instance_id2,
+         'europe-west2-b',
+         'project-abc',
+         'projects/project-abc/zones/europe-west2-b/instances/instance-1/networkinterfaces/nic0',
+         '10.0.0.2',
+         TEST_UPDATE_TAG)
+    ])
+    assert actual_nodes == expected_nodes
+
+
+def test_vpc_to_subnets(neo4j_session):
     """
     Ensure that subnets are connected to VPCs.
     """
+    _ensure_local_neo4j_has_test_vpc_data(neo4j_session)
+    _ensure_local_neo4j_has_test_subnet_data(neo4j_session)
     query = """
     MATCH(vpc:GCPVpc{id:{VpcId}})-[:RESOURCE]->(subnet:GCPSubnet)
     RETURN vpc.id, subnet.id, subnet.region, subnet.gateway_address, subnet.ip_cidr_range,
@@ -83,48 +162,11 @@ def test_vpc_to_subnets(load_compute_objects, neo4j_session):
     assert actual_nodes == expected_nodes
 
 
-def test_load_gcp_instances_and_network_objects(load_compute_objects, neo4j_session):
+def test_nics_to_access_configs(neo4j_session):
     """
-    Ensure that we can correctly transform and load GCP instances.
+    Ensure that network interfaces and access configs are attached
     """
-    instance_query = """
-    MATCH(i:GCPInstance)
-    RETURN i.id, i.zone_name, i.project_id, i.hostname
-    """
-    nodes = neo4j_session.run(instance_query)
-    actual_nodes = set([(n['i.id'], n['i.zone_name'], n['i.project_id'], n['i.hostname']) for n in nodes])
-    expected_nodes = set([
-        ('projects/project-abc/zones/europe-west2-b/instances/instance-1-test', 'europe-west2-b', 'project-abc', None),
-        ('projects/project-abc/zones/europe-west2-b/instances/instance-1', 'europe-west2-b', 'project-abc', None)
-    ])
-    assert actual_nodes == expected_nodes
-
-
-def test_load_network_interfaces(load_compute_objects, neo4j_session):
-    """
-    Ensure the network interfaces are attached to GCP instances.
-    """
-    nic_query = """
-    MATCH(i:GCPInstance)-[r:NETWORK_INTERFACE]->(nic:GCPNetworkInterface)
-    return r.lastupdated, nic.nic_id, nic.private_ip
-    """
-    objects = neo4j_session.run(nic_query)
-    actual_nodes = set([(o['nic.nic_id'], o['nic.private_ip'], o['r.lastupdated']) for o in objects])
-    expected_nodes = set([
-        ('projects/project-abc/zones/europe-west2-b/instances/instance-1-test/networkinterfaces/nic0',
-         '10.0.0.3',
-         TEST_UPDATE_TAG),
-        ('projects/project-abc/zones/europe-west2-b/instances/instance-1/networkinterfaces/nic0',
-         '10.0.0.2',
-         TEST_UPDATE_TAG)
-    ])
-    assert actual_nodes == expected_nodes
-
-
-def test_load_access_configs(load_compute_objects, neo4j_session):
-    """
-    Ensure the access configs are attached to the network interfaces
-    """
+    _ensure_local_neo4j_has_test_instance_data(neo4j_session)
     ac_query = """
     MATCH (nic:GCPNetworkInterface)-[r:RESOURCE]->(ac:GCPNicAccessConfig)
     return nic.nic_id, ac.access_config_id, ac.public_ip
@@ -146,10 +188,12 @@ def test_load_access_configs(load_compute_objects, neo4j_session):
     assert actual_nodes == expected_nodes
 
 
-def test_nic_to_subnet_connectivity(load_compute_objects, neo4j_session):
+def test_nic_to_subnets(neo4j_session):
     """
     Ensure that network interfaces are attached to subnets
     """
+    _ensure_local_neo4j_has_test_subnet_data(neo4j_session)
+    _ensure_local_neo4j_has_test_instance_data(neo4j_session)
     subnet_query = """
     MATCH (nic:GCPNetworkInterface{id:{NicId}})-[:PART_OF_SUBNET]->(subnet:GCPSubnet)
     return nic.nic_id, nic.private_ip, subnet.id,  subnet.gateway_address, subnet.ip_cidr_range
