3100ed873a91b4cd4563fca8db06c67aa8e7b2ee
Evan Davis <evandavis@lyft.com>
2019-06-04 14:35:21 -0700

    refactor

--- cartography/intel/aws/__init__.py
+++ cartography/intel/aws/__init__.py
@@ -10,17 +10,7 @@ logger = logging.getLogger(__name__)
 
 def _sync_one_account(session, boto3_session, account_id, regions, sync_tag, common_job_parameters):
     # IAM
-    # TODO move this to IAM module
-    logger.info("Syncing IAM for account '%s'.", account_id)
-    iam.sync_users(session, boto3_session, account_id, sync_tag, common_job_parameters)
-    iam.sync_groups(session, boto3_session, account_id, sync_tag, common_job_parameters)
-    iam.sync_policies(session, boto3_session, account_id, sync_tag, common_job_parameters)
-    iam.sync_roles(session, boto3_session, account_id, sync_tag, common_job_parameters)
-    iam.sync_group_memberships(session, boto3_session, account_id, sync_tag, common_job_parameters)
-    iam.sync_group_policies(session, boto3_session, account_id, sync_tag, common_job_parameters)
-    iam.sync_role_policies(session, boto3_session, account_id, sync_tag, common_job_parameters)
-    iam.sync_user_access_keys(session, boto3_session, account_id, sync_tag, common_job_parameters)
-    run_cleanup_job('aws_import_principals_cleanup.json', session, common_job_parameters)
+    iam.sync(session, boto3_session, account_id, sync_tag, common_job_parameters)
 
     # S3
     s3.sync(session, boto3_session, account_id, sync_tag, common_job_parameters)
