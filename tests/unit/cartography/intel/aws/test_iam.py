lyft/cartography

commit 82c59c074e5598b0c4f0a31b3ff5f243aec84d7b

Use policyuniverse to parse IAM statements and find role assumption permissions. (#83)
* Use policyuniverse to parse IAM statements and find role assumption permissions.

* Use a better variable name.

cartography/intel/aws/iam.py
@@ -1,4 +1,5 @@
 import logging
+import policyuniverse.statement
 
 from cartography.util import run_cleanup_job
 
@@ -246,26 +247,9 @@ def load_group_memberships(session, group_memberships, aws_update_tag):
 def _find_roles_assumable_in_policy(policy_data):
     ret = []
     for statement in policy_data["PolicyDocument"]["Statement"]:
-        if isinstance(statement, str):
-            continue
-        actions = statement.get('Action')
-        if not actions:
-            continue
-        if isinstance(actions, str):
-            actions = [actions]
-
-        for action in actions:
-            # TODO actions may contain wildcards, e.g. sts:* -- this can be solved by using policyuniverse to
-            # TODO expand the policy before checking if the sts:AssumeRole action is allowed
-            if action == "sts:AssumeRole":
-                # TODO blanket allows may be modified by subsequent denies... -- does policyuniverse handle?
-                if statement["Effect"] == "Allow":
-                    role_arns = statement["Resource"]
-
-                    if isinstance(role_arns, str):
-                        role_arns = [role_arns]
-
-                    ret.extend(role_arns)
+        parsed_statement = policyuniverse.statement.Statement(statement)
+        if parsed_statement.effect == 'Allow' and 'sts:assumerole' in parsed_statement.actions_expanded:
+            ret.extend(list(parsed_statement.resources))
     return ret
 
 

tests/unit/cartography/intel/aws/test_iam.py
@@ -1,0 +1,75 @@
+from collections import namedtuple
+
+from cartography.intel.aws import iam
+
+
+def test__find_roles_assumable_in_policy():
+    Case = namedtuple('Case', ['message', 'statements', 'expected'])
+
+    cases = (
+        Case(
+            'basic',
+            [{
+                "Action": "sts:AssumeRole",
+                "Resource": "arn:aws:iam::000000000000:role/example-role-0",
+                "Effect": "Allow",
+            }],
+            ["arn:aws:iam::000000000000:role/example-role-0"]
+        ),
+        Case(
+            'must handle policies containing lists of statements',
+            [
+                {
+                    "Action": "sts:AssumeRole",
+                    "Resource": "arn:aws:iam::000000000000:role/example-role-0",
+                    "Effect": "Allow",
+                },
+                {
+                    "Action": "sts:AssumeRole",
+                    "Resource": "arn:aws:iam::000000000000:role/example-role-1",
+                    "Effect": "Allow",
+                }
+            ],
+            [
+                "arn:aws:iam::000000000000:role/example-role-0",
+                "arn:aws:iam::000000000000:role/example-role-1",
+            ]
+        ),
+        Case(
+            'must handle statements containing lists of actions',
+            [{
+                "Action": ["sts:AssumeRole", "s3:GetObject"],
+                "Resource": "arn:aws:iam::000000000000:role/example-role-0",
+                "Effect": "Allow",
+            }],
+            ["arn:aws:iam::000000000000:role/example-role-0"]
+        ),
+        Case(
+            'must handle actions containing wildcard characters',
+            [{
+                "Action": "*",
+                "Resource": "arn:aws:iam::000000000000:role/example-role-0",
+                "Effect": "Allow",
+            }],
+            ["arn:aws:iam::000000000000:role/example-role-0"]
+        ),
+        Case(
+            'must not modify resource casing',
+            [{
+                "Action": "sts:AssumeRole",
+                "Resource": "arn:aws:iam::000000000000:role/EXAMPLE-role-0",
+                "Effect": "Allow",
+            }],
+            ["arn:aws:iam::000000000000:role/EXAMPLE-role-0"]
+        ),
+    )
+
+    for case in cases:
+        actual = iam._find_roles_assumable_in_policy(
+            {
+                "PolicyDocument": {
+                    "Statement": case.statements,
+                }
+            }
+        )
+        assert actual == case.expected, case.message

commit 94d6f1a4401f9ba3d00b7dd4deab3cce68905b08

Fix IAM statement parsing bug which would cause single-statement policies to crash sync (#84)
* Fix regression introduced in #83

* Improve test case.

* fix

cartography/intel/aws/iam.py
@@ -246,7 +246,10 @@ def load_group_memberships(session, group_memberships, aws_update_tag):
 
 def _find_roles_assumable_in_policy(policy_data):
     ret = []
-    for statement in policy_data["PolicyDocument"]["Statement"]:
+    statements = policy_data["PolicyDocument"]["Statement"]
+    if isinstance(statements, dict):
+        statements = [statements]
+    for statement in statements:
         parsed_statement = policyuniverse.statement.Statement(statement)
         if parsed_statement.effect == 'Allow' and 'sts:assumerole' in parsed_statement.actions_expanded:
             ret.extend(list(parsed_statement.resources))

tests/unit/cartography/intel/aws/test_iam.py
@@ -36,6 +36,15 @@ def test__find_roles_assumable_in_policy():
             ]
         ),
         Case(
+            'must handle policies containing a single statement',
+            {
+                "Action": "sts:AssumeRole",
+                "Resource": "arn:aws:iam::000000000000:role/example-role-0",
+                "Effect": "Allow",
+            },
+            ["arn:aws:iam::000000000000:role/example-role-0"]
+        ),
+        Case(
             'must handle statements containing lists of actions',
             [{
                 "Action": ["sts:AssumeRole", "s3:GetObject"],
@@ -61,7 +70,7 @@ def test__find_roles_assumable_in_policy():
                 "Effect": "Allow",
             }],
             ["arn:aws:iam::000000000000:role/EXAMPLE-role-0"]
-        ),
+        )
     )
 
     for case in cases: