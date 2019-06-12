d2f87d7765d5d9e407c8b3bf1b5e4c822304e74d
Evan Davis <evandavis@lyft.com>
2019-06-12 14:44:58 -0700

    add auth

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -5,7 +5,7 @@ from tests.integration import settings
 
 @pytest.fixture(scope="module")
 def neo4j_session():
-    driver = neo4j.GraphDatabase.driver(settings.get("NEO4J_URL"))
+    driver = neo4j.GraphDatabase.driver(settings.get("NEO4J_URL"), auth=('neo4j', 'neo4j'))
     with driver.session() as session:
         yield session
         session.run("MATCH (n) DETACH DELETE n;")
