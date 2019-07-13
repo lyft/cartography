927e1a06b520faf02794735c7b715729128ce4ff
Taya Steere <37319913+tayasteere@users.noreply.github.com>
2019-07-12 18:21:30 -0700

    Added integration tests, fixed test data format

--- tests/data/crxcavator/crxcavator.py
+++ tests/data/crxcavator/crxcavator.py
@@ -1,51 +1,86 @@
 # flake8: noqa
 REPORT_RESPONSE = {
-    'ppedokobpbdajgiejhnjfbdjlgobcpkp',
-    [
-        {'data':
+    'f06981cbc72a3c6e2e9e736cbdaef4865a4571bc':
+        [
             {
-                'risk':
-                    {
-                        'total': 437,
-                        'metadata': {}
-                    },
-                'webstore':
-                    {
-                        'address': '',
-                        'email': '',
-                        'icon': 'https://lh3.googleusercontent.com/6vvUTv5SIaggk-mliDV6o_CfcDIIKu7MR5gL8Ep5QtODWrYOPj_hgywZQ-FImelit_Id1thKNSU=w128-h128-e365',
-                        'last_updated': '2016-02-22', 'name': 'f*ck overlays',
-                        'offered_by': '',
-                        'permission_warnings': [
-                            'Your data on all websites'
-                        ],
-                        'privacy_policy': '',
-                        'rating': 4.6778846,
-                        'rating_users': 208,
-                        'short_description': 'adds a context menu to delete sign-up/sign-in overlays that prevent you from viewing a page',
-                        'size': '13.95KiB',
-                        'support_site': '',
-                        'users': 38241,
-                        'version': '',
-                        'website': '',
-                        'type': 'Extension',
-                        'price': ''
-                    }
-            },
-            'extension_id': 'ppedokobpbdajgiejhnjfbdjlgobcpkp',
-            'version': '1.1.2'
-        }
+                'data':
+                {
+                    'risk':
+                        {
+                            'total': 437,
+                            'metadata': {}
+                        },
+                    'webstore':
+                        {
+                            'address': '',
+                            'email': '',
+                            'icon': 'https://lh3.googleusercontent.com/fake',
+                            'last_updated': '2016-02-22',
+                            'name': 'CartographyIntegrationTest',
+                            'offered_by': '',
+                            'permission_warnings': [
+                                'Your data on all websites'
+                            ],
+                            'privacy_policy': '',
+                            'rating': 4.6778846,
+                            'rating_users': 208,
+                            'short_description': 'fake extension for Cartography integration testing',
+                            'size': '13.95KiB',
+                            'support_site': '',
+                            'users': 38241,
+                            'version': '',
+                            'website': '',
+                            'type': 'Extension',
+                            'price': ''
+                        }
+                },
+                'extension_id': 'f06981cbc72a3c6e2e9e736cbdaef4865a4571bc',
+                'version': '1.0'
+            }
     ]
 }
 
-USER_RESPONSE = {
-    'ppmacakdbohpklnhpaecpoohbkapggpo',
+TRANSFORMED_EXTENSIONS_DATA = [
     {
-        '1.7':
-            {
-                'name': 'MindMeister Quick Access',
-                'users':
-                    ['user@example.com']
-            }
+        'id': 'f06981cbc72a3c6e2e9e736cbdaef4865a4571bc|1.0',
+        'extension_id': 'f06981cbc72a3c6e2e9e736cbdaef4865a4571bc',
+        'version': '1.0',
+        'risk_total': 437,
+        'risk_metadata': '{}',
+        'address': '',
+        'email': '',
+        'icon': 'https://lh3.googleusercontent.com/fake',
+        'last_updated': '2016-02-22',
+        'name': 'CartographyIntegrationTest',
+        'offered_by': '',
+        'permissions_warnings': 'Your data on all websites',
+        'privacy_policy': '',
+        'rating': 4.6778846,
+        'rating_users': 208,
+        'short_description': 'fake extension for Cartography integration testing',
+        'size': '13.95KiB',
+        'support_site': '',
+        'users': 38241,
+        'website': '',
+        'type': 'Extension',
+        'price': ''
     }
+]
+
+USER_RESPONSE = {
+    'f06981cbc72a3c6e2e9e736cbdaef4865a4571bc':
+        {'4.8':
+             {'name': 'CartographyIntegrationTest',
+              'users': ['user@example.com']
+              }
+         }
 }
+
+TRANSFORMED_USER_DATA = ['user@example.com']
+
+TRANSFORMED_USER_EXTENSION_DATA = [
+    {
+        'id': 'f06981cbc72a3c6e2e9e736cbdaef4865a4571bc|1.0',
+        'user': 'user@example.com'
+    }
+]
---
+++ tests/integration/cartography/intel/crxcavator/test_crxcavator.py
@@ -0,0 +1,178 @@
+import cartography.intel.crxcavator.crxcavator
+import tests.data.crxcavator.crxcavator
+
+
+TEST_UPDATE_TAG = 123456789
+
+
+def _ensure_local_neo4j_has_test_extensions_data(neo4j_session):
+    cartography.intel.crxcavator.crxcavator.load_extensions(
+        tests.data.crxcavator.crxcavator.TRANSFORMED_EXTENSIONS_DATA,
+        neo4j_session,
+        TEST_UPDATE_TAG
+    )
+
+
+def _ensure_local_neo4j_has_test_user_extensions_data(neo4j_session):
+    cartography.intel.crxcavator.crxcavator.load_user_extensions(
+        tests.data.crxcavator.crxcavator.TRANSFORMED_USER_DATA,
+        tests.data.crxcavator.crxcavator.TRANSFORMED_USER_EXTENSION_DATA,
+        neo4j_session,
+        TEST_UPDATE_TAG
+    )
+
+
+def test_transform_and_load_extensions(neo4j_session):
+    """
+    Test that we can correctly transform and load ChromeExtension nodes to Neo4j.
+    """
+    extension_res = tests.data.crxcavator.crxcavator.REPORT_RESPONSE
+    extension_list = cartography.intel.crxcavator.crxcavator.transform_extensions(extension_res)
+    cartography.intel.crxcavator.crxcavator.load_extensions(
+        extension_list,
+        neo4j_session,
+        TEST_UPDATE_TAG)
+
+    query = """
+    MATCH(ext:ChromeExtension{id:{ExtensionId}})
+    RETURN
+    ext.id,
+    ext.extension_id,
+    ext.version,
+    ext.risk_total,
+    ext.risk_metadata,
+    ext.address,
+    ext.email,
+    ext.icon,
+    ext.last_updated,
+    ext.name,
+    ext.offered_by,
+    ext.permissions_warnings,
+    ext.privacy_policy,
+    ext.rating,
+    ext.rating_users,
+    ext.short_description,
+    ext.size,
+    ext.support_site,
+    ext.users,
+    ext.website,
+    ext.type,
+    ext.price
+    """
+    expected_extension_id = 'f06981cbc72a3c6e2e9e736cbdaef4865a4571bc|1.0'
+    nodes = neo4j_session.run(
+        query,
+        ExtensionId=expected_extension_id
+    )
+    actual_nodes = set([(
+        n['ext.id'],
+        n['ext.extension_id'],
+        n['ext.version'],
+        n['ext.risk_total'],
+        n['ext.risk_metadata'],
+        n['ext.address'],
+        n['ext.email'],
+        n['ext.icon'],
+        n['ext.last_updated'],
+        n['ext.name'],
+        n['ext.offered_by'],
+        n['ext.permissions_warnings'],
+        n['ext.privacy_policy'],
+        n['ext.rating'],
+        n['ext.rating_users'],
+        n['ext.short_description'],
+        n['ext.size'],
+        n['ext.support_site'],
+        n['ext.users'],
+        n['ext.website'],
+        n['ext.type'],
+        n['ext.price']
+    ) for n in nodes])
+    expected_nodes = set([
+        (expected_extension_id,
+         'f06981cbc72a3c6e2e9e736cbdaef4865a4571bc',
+         '1.0',
+         437,
+         '{}',
+         '',
+         '',
+         'https://lh3.googleusercontent.com/fake',
+         '2016-02-22',
+         'CartographyIntegrationTest',
+         '',
+         'Your data on all websites',
+         '',
+         4.6778846,
+         208,
+         'fake extension for Cartography integration testing',
+         '13.95KiB',
+         '',
+         38241,
+         '',
+         'Extension',
+         ''
+         )
+    ])
+    assert actual_nodes == expected_nodes
+
+
+def test_transform_and_load_user_extensions(neo4j_session):
+    """
+    Ensure we can transform and load users and extension mapping.
+    """
+    users_res = tests.data.crxcavator.crxcavator.USER_RESPONSE
+    type(users_res)
+    users_list, user_extensions_list = cartography.intel.crxcavator.crxcavator.transform_user_extensions(users_res)
+    cartography.intel.crxcavator.crxcavator.load_user_extensions(users_list,
+                                                                 user_extensions_list,
+                                                                 neo4j_session,
+                                                                 TEST_UPDATE_TAG)
+
+    query = """
+    MATCH(user:GSuiteUser{id:{UserId}})
+    RETURN user.id, user.email
+    """
+    expected_user_id = 'user@example.com'
+    nodes = neo4j_session.run(query,
+                              UserId=expected_user_id)
+
+    actual_nodes = set([(
+        n['user.id'],
+        n['user.email']
+    ) for n in nodes])
+
+    expected_nodes = set([
+        ('user@example.com',
+         'user@example.com')
+    ])
+    assert actual_nodes == expected_nodes
+
+
+def test_user_to_extension(neo4j_session):
+    """
+    Ensure that users are connected to extensions.
+    """
+    _ensure_local_neo4j_has_test_extensions_data(neo4j_session)
+    _ensure_local_neo4j_has_test_user_extensions_data(neo4j_session)
+    query = """
+    MATCH(user:GSuiteUser)-[:INSTALLS]->(ext:ChromeExtension{id:{ExtensionId}})
+    RETURN user.id, ext.id, ext.name
+    """
+    expected_extension_id = 'f06981cbc72a3c6e2e9e736cbdaef4865a4571bc|1.0'
+    nodes = neo4j_session.run(
+        query,
+        ExtensionId=expected_extension_id
+    )
+    actual_nodes = set([(
+        n['user.id'],
+        n['ext.id'],
+        n['ext.name'],
+    ) for n in nodes])
+
+    expected_nodes = set([
+        ('user@example.com',
+         'f06981cbc72a3c6e2e9e736cbdaef4865a4571bc|1.0',
+         'CartographyIntegrationTest'
+         )
+    ])
+    assert actual_nodes == expected_nodes
