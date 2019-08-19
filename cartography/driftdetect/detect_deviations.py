52f2474b1db00c198e1ee0eb4916f22540ec2fb4
Thomas Li <thomasl@lyft.com>
2019-08-19 15:12:56 -0700

    separated execution and setup of drift detection

--- cartography/driftdetect/detect_deviations.py
+++ cartography/driftdetect/detect_deviations.py
@@ -19,27 +19,14 @@ def run_drift_detection(config):
             return
         state_serializer = StateSchema()
         shortcut_serializer = ShortcutSchema()
-        shortcut_data = FileSystem.load(os.path.join(config.query_directory, "shortcut.json"))
-        shortcut = shortcut_serializer.load(shortcut_data)
-        start_state_data = FileSystem.load(
-            os.path.join(
-                config.query_directory, shortcut.shortcuts.get(
-                    config.start_state,
-                    config.start_state,
-                ),
-            ),
+        new_results, missing_results, end_state = detect_drift_from_fp(
+            config.query_directory,
+            FileSystem,
+            state_serializer,
+            shortcut_serializer,
+            config.start_state,
+            config.end_state,
         )
-        start_state = state_serializer.load(start_state_data)
-        end_state_data = FileSystem.load(
-            os.path.join(
-                config.query_directory, shortcut.shortcuts.get(
-                    config.end_state,
-                    config.end_state,
-                ),
-            ),
-        )
-        end_state = state_serializer.load(end_state_data)
-        new_results, missing_results = perform_drift_detection(start_state, end_state)
         report_drift(new_results, missing_results, end_state.name, end_state.properties)
     except ValidationError as err:
         msg = "Unable to create DriftStates from files {},{} for \n{} in directory {}.".format(
@@ -57,6 +44,41 @@ def run_drift_detection(config):
             config.query_directory,
         )
         logger.exception(msg)
+
+
+def detect_drift_from_fp(query_directory, storage, state_serializer, shortcut_serializer, start_state_fp, end_state_fp):
+    """
+    Performs Drift Detection between two files.
+    :param query_directory: The query directory.
+    :param storage: The storage object.
+    :param state_serializer: The serializer for states.
+    :param shortcut_serializer: The serializer for shortcuts.
+    :param start_state_fp: The start state filepath
+    :param end_state_fp: The end state filepath
+    :return: the new_results, missing_results, and end state
+    """
+    shortcut_data = storage.load(os.path.join(query_directory, "shortcut.json"))
+    shortcut = shortcut_serializer.load(shortcut_data)
+    start_state_data = storage.load(
+        os.path.join(
+            query_directory, shortcut.shortcuts.get(
+                start_state_fp,
+                start_state_fp,
+            ),
+        ),
+    )
+    start_state = state_serializer.load(start_state_data)
+    end_state_data = storage.load(
+        os.path.join(
+            query_directory, shortcut.shortcuts.get(
+                end_state_fp,
+                end_state_fp,
+            ),
+        ),
+    )
+    end_state = state_serializer.load(end_state_data)
+    new_results, missing_results = perform_drift_detection(start_state, end_state)
+    return new_results, missing_results, end_state
 
 
 def perform_drift_detection(start_state, end_state):
