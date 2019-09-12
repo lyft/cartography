44653897e92aa5192cb9c135da8a9e4c5aa55641
Taya Steere <37319913+tayasteere@users.noreply.github.com>
2019-09-12 16:13:11 -0700

    fix test

--- tests/integration/cartography/intel/crxcavator/test_crxcavator.py
+++ tests/integration/cartography/intel/crxcavator/test_crxcavator.py
@@ -148,10 +148,9 @@ def test_transform_and_load_user_extensions(neo4j_session):
     """
     users_res = tests.data.crxcavator.crxcavator.USER_RESPONSE
     type(users_res)
-    users_list, extensions_list, user_extensions_list = \
+    extensions_list, user_extensions_list = \
         cartography.intel.crxcavator.crxcavator.transform_user_extensions(users_res)
     cartography.intel.crxcavator.crxcavator.load_user_extensions(
-        users_list,
         user_extensions_list,
         neo4j_session,
         TEST_UPDATE_TAG,
