feb6ae360518084b4c2504061aad1a2c2ffde4fa
Zachary Manesiotis <zack.manesiotis@gmail.com>
2019-09-12 12:34:27 -0400

    added error handling

--- cartography/intel/gcp/storage.py
+++ cartography/intel/gcp/storage.py
@@ -25,8 +25,34 @@ def get_bucket_metadata(storage, bucket, credentials):
     :param credentials: The GoogleCredentials object
     :return: Metadata for specified bucket
     """
-    req = storage.buckets().get(bucket=bucket)
-    return req.execute()
+    try:
+        req = storage.buckets().get(bucket=bucket)
+        res = req.execute()
+        return res
+    except HttpError as e:
+        reason = _get_error_reason(e)
+        if reason == 'notFound':
+            logger.debug(
+                (
+                    "The bucket %s was not found - returned a 404 not found error."
+                    "Full details: %s"
+                ),
+                bucket,
+                e,
+            )
+            return None
+        elif reason == 'forbidden':
+            logger.debug(
+                (
+                    "You do not have storage.bucket.get access to the bucket %s. "
+                    "Full details: %s"
+                ),
+                bucket,
+                e,
+            )
+            return None
+        else:
+            raise
 
 
 def list_buckets(storage, project, credentials):
@@ -37,8 +63,33 @@ def list_buckets(storage, project, credentials):
     :param credentials: The GoogleCredentials object
     :return: List of storage objects
     """
-    req = storage.buckets().list(project=project)
-    res = req.execute()
-    all_objects = []
-    all_objects.extend(res.get('items', []))
-    return all_objects
+    try:
+        req = storage.buckets().list(project=project)
+        res = req.execute()
+        all_objects = []
+        all_objects.extend(res.get('items', []))
+        return all_objects
+    except HttpError as e:
+        reason = _get_error_reason(e)
+        if reason == 'invalid':
+            logger.debug(
+                (
+                    "The project %s is invalid - returned a 400 invalid error."
+                    "Full details: %s"
+                ),
+                project,
+                e,
+            )
+            return None
+        elif reason == 'forbidden':
+            logger.debug(
+                (
+                    "You do not have storage.bucket.list access to the project %s. "
+                    "Full details: %s"
+                ),
+                project,
+                e,
+            )
+            return None
+        else:
+            raise
