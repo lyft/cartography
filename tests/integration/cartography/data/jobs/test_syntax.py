847feea215c2d7a9717ab771646af7853d85210f
Evan Davis <evandavis@lyft.com>
2019-10-02 11:11:25 -0700

    Add UPDATE_TAG

--- tests/integration/cartography/data/jobs/test_syntax.py
+++ tests/integration/cartography/data/jobs/test_syntax.py
@@ -12,6 +12,7 @@ else:
 def test_analysis_jobs_cypher_syntax(neo4j_session):
     parameters = {
         'AWS_ID': None,
+        'UPDATE_TAG': None,
     }
 
     for job_name in contents('cartography.data.jobs.analysis'):
