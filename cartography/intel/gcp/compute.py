fafb8539bd01361e69e2fd7389d864e379f06931
Dean Liu <dliu@lyft.com>
2019-10-08 15:27:11 -0700

    ws

--- cartography/intel/gcp/compute.py
+++ cartography/intel/gcp/compute.py
@@ -877,7 +877,6 @@ def sync_gcp_instances(neo4j_session, compute, project_id, zones, gcp_update_tag
     :param common_job_parameters: dict of other job parameters to pass to Neo4j
     :return: Nothing
     """
-
     instance_responses = get_gcp_instance_responses(project_id, zones, compute)
     instance_list = transform_gcp_instances(instance_responses)
     load_gcp_instances(neo4j_session, instance_list, gcp_update_tag)
