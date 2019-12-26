708f96c54aa07614108c11c66cac13cbcd30c22f
Taya Steere <37319913+tayasteere@users.noreply.github.com>
2019-12-26 11:37:37 -0800

    crxcavator crash fix before merge from master

--- cartography/intel/crxcavator/crxcavator.py
+++ cartography/intel/crxcavator/crxcavator.py
@@ -298,6 +298,9 @@ def transform_user_extensions(user_extension_json):
                 'name': details[1]['name'],
             })
             for user in details[1]['users']:
+                if user is None:
+                    print(f"bad user for {extension_id}|{version}")
+                    continue
                 users_set.add(user)
                 extensions_by_user.append({
                     'id': f"{extension_id}|{version}",
