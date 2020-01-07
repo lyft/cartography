58396e840e5f0bc4b511e8ec2a6ee1b9568fa4ab
Andrew Johnson <AndrewJ@lyft.com>
2020-01-06 16:33:28 -0800

    fix lint

--- tests/data/aws/route53.py
+++ tests/data/aws/route53.py
@@ -6,7 +6,9 @@ NS_RECORD = {
         {"Value": "ns-856.awsdns-43.net."},
         {"Value": "ns-1418.awsdns-49.org."},
         {"Value": "ns-1913.awsdns-47.co.uk."},
-        {"Value": "ns-192.awsdns-24.com."}]}
+        {"Value": "ns-192.awsdns-24.com."},
+    ],
+}
 
 CNAME_RECORD = {
     "Name": "subdomain.lyft.com.",
@@ -14,23 +16,32 @@ CNAME_RECORD = {
     "SetIdentifier": "ca",
     "GeoLocation": {
         "CountryCode": "US",
-        "SubdivisionCode": "CA"},
+        "SubdivisionCode": "CA",
+    },
     "AliasTarget": {
         "HostedZoneId": "FAKEZONEID",
         "DNSName": "fakeelb.elb.us-east-1.amazonaws.com.",
-        "EvaluateTargetHealth": False}}
+        "EvaluateTargetHealth": False,
+    },
+}
 
-ZONE_RECORDS = [{
-    "Id": "/hostedzone/FAKEZONEID1",
-    "Name": "test.com.",
-    "CallerReference": "BD057866-DA11-69AA-AE7C-339CDB669D49",
-    "Config": {
-        "PrivateZone": False},
-    "ResourceRecordSetCount": 8},
+ZONE_RECORDS = [
     {
-    "Id": "/hostedzone/FAKEZONEID2",
-    "Name": "test.com.",
-    "CallerReference": "BD057866-DA11-69AA-AE7C-339CDB669D49",
-    "Config": {
-        "PrivateZone": False},
-        "ResourceRecordSetCount": 8}]
+        "Id": "/hostedzone/FAKEZONEID1",
+        "Name": "test.com.",
+        "CallerReference": "BD057866-DA11-69AA-AE7C-339CDB669D49",
+        "Config": {
+            "PrivateZone": False,
+        },
+        "ResourceRecordSetCount": 8,
+    },
+    {
+        "Id": "/hostedzone/FAKEZONEID2",
+        "Name": "test.com.",
+        "CallerReference": "BD057866-DA11-69AA-AE7C-339CDB669D49",
+        "Config": {
+            "PrivateZone": False,
+        },
+        "ResourceRecordSetCount": 8,
+    },
+]
