95b6c467921093ed17ce140056a05a068c60806c
Alex Chantavy <achantavy@lyft.com>
2020-01-09 11:28:27 -0800

    Make linter happy and update schema docs

--- cartography/intel/aws/ec2.py
+++ cartography/intel/aws/ec2.py
@@ -167,7 +167,8 @@ def load_ec2_instances(neo4j_session, data, region, current_aws_account_id, aws_
     SET instance.publicdnsname = {PublicDnsName}, instance.privateipaddress = {PrivateIpAddress},
     instance.imageid = {ImageId}, instance.instancetype = {InstanceType}, instance.monitoringstate = {MonitoringState},
     instance.state = {State}, instance.launchtime = {LaunchTime}, instance.launchtimeunix = {LaunchTimeUnix},
-    instance.region = {Region}, instance.lastupdated = {aws_update_tag}, instance.iaminstanceprofile = {IamInstanceProfile}
+    instance.region = {Region}, instance.lastupdated = {aws_update_tag},
+    instance.iaminstanceprofile = {IamInstanceProfile}
     WITH instance
     MERGE (subnet:EC2Subnet{subnetid: {SubnetId}})
     ON CREATE SET subnet.firstseen = timestamp()
