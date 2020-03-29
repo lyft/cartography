dea927a9a0a1414c8e4fbfc943ec6eeb76371f71
Marco Lancini <info@marcolancini.it>
2020-03-29 16:48:39 +0100

    Add tests

---
+++ tests/integration/cartography/intel/aws/test_eks.py
@@ -0,0 +1,30 @@
+import cartography.intel.aws.eks
+import tests.data.aws.eks
+
+TEST_ACCOUNT_ID = '000000000000'
+TEST_REGION = 'eu-west-1'
+TEST_UPDATE_TAG = 123456789
+
+
+def test_load_eks_clusters(neo4j_session):
+    data = tests.data.aws.eks.DESCRIBE_CLUSTERS
+
+    cartography.intel.aws.eks.load_eks_clusters(
+        neo4j_session,
+        data,
+        TEST_REGION,
+        TEST_ACCOUNT_ID,
+        TEST_UPDATE_TAG,
+    )
+    expected_nodes = {
+        "arn:aws:eks:eu-west-1:111111111111:cluster/cluster_1",
+        "arn:aws:eks:eu-west-1:111111111111:cluster/cluster_2"
+    }
+
+    nodes = neo4j_session.run(
+        """
+        MATCH (r:EKSCluster) RETURN r.arn;
+        """
+    )
+    actual_nodes = {n['r.arn'] for n in nodes}
+    assert actual_nodes == expected_nodes
