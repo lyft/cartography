d7cb4506ffa2576193f29ed7e0a1f20b2828eac3
Alex Chantavy <achantavy@lyft.com>
2020-04-16 10:08:16 -0700

    Move statsd initialization to sync.run_with_config() and add doc strings (#287)
    * Add missing docstring for statsd CLI params
    
    * Move statsd client initialization to sync.run_with_config() to be more consistent with other inits
    
    * Correct docstring

--- cartography/config.py
+++ cartography/config.py
@@ -31,6 +31,12 @@ class Config:
     :param okta_saml_role_regex: The regex used to map okta groups to AWS roles. Optional.
     :type github_config: str
     :param github_config: Base64 encoded config object for GitHub ingestion. Optional
+    :type statsd_enabled: bool
+    :param statsd_enabled: Whether to collect statsd metrics such as sync execution times. Optional.
+    :type statsd_host: str
+    :param statsd_host: If statsd_enabled is True, send metrics to this host. Optional.
+    :type: statsd_port: int
+    :param statsd_port: If statsd_enabled is True, send metrics to this port on statsd_host. Optional.
     """
 
     def __init__(
