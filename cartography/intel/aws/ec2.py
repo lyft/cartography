8aeefdfbf34e9bde14ca510390f25fbe788234af
Jordan Sokolic <jordan.sokolic@ridewithvia.com>
2020-06-06 20:23:40 +0300

    call TGW sync

--- cartography/intel/aws/ec2.py
+++ cartography/intel/aws/ec2.py
@@ -1450,3 +1450,4 @@ def sync(neo4j_session, boto3_session, regions, account_id, sync_tag, common_job
     sync_load_balancers(neo4j_session, boto3_session, regions, account_id, sync_tag, common_job_parameters)
     sync_load_balancer_v2s(neo4j_session, boto3_session, regions, account_id, sync_tag, common_job_parameters)
     sync_vpc_peering(neo4j_session, boto3_session, regions, account_id, sync_tag, common_job_parameters)
+    sync_transit_gateways(neo4j_session, boto3_session, regions, account_id, sync_tag, common_job_parameters)
