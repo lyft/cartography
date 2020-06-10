962053549330d613d70cdf1328dbaf5a2d0f53f8
Thien Phan <thienphan@csod.com>
2020-06-09 18:03:17 -0700

    enabled s3

--- cartography/intel/aws/__init__.py
+++ cartography/intel/aws/__init__.py
@@ -24,7 +24,7 @@ logger = logging.getLogger(__name__)
 
 def _sync_one_account(neo4j_session, boto3_session, account_id, sync_tag, common_job_parameters):
     iam.sync(neo4j_session, boto3_session, account_id, sync_tag, common_job_parameters)
-    # s3.sync(neo4j_session, boto3_session, account_id, sync_tag, common_job_parameters)
+    s3.sync(neo4j_session, boto3_session, account_id, sync_tag, common_job_parameters)
 
     try:
         regions = ec2.get_ec2_regions(boto3_session)
@@ -38,7 +38,7 @@ def _sync_one_account(neo4j_session, boto3_session, account_id, sync_tag, common
             account_id,
         )
         return
-        
+
     lambda_function.sync(neo4j_session, boto3_session, regions, account_id, sync_tag, common_job_parameters)
     dynamodb.sync(neo4j_session, boto3_session, regions, account_id, sync_tag, common_job_parameters)
     ec2.sync(neo4j_session, boto3_session, regions, account_id, sync_tag, common_job_parameters)
