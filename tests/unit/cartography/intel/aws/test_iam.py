0459cb317ee23f5b99b9668c19b283544db4594e
Roshini Saravanakumar <sroshini@climate.com>
2020-06-12 14:24:57 -0700

    Fix IAM principal parsing + unit test

--- tests/unit/cartography/intel/aws/test_iam.py
+++ tests/unit/cartography/intel/aws/test_iam.py
@@ -5,6 +5,12 @@ SINGLE_STATEMENT = {
     "Action": "*",
 }
 
+SINGLE_PRINCIPAL = {
+    "AWS": "test-role-1",
+    "Service": ["test-service-1", "test-service-2"],
+    "Federated": "test-provider-1",
+}
+
 
 def test__generate_policy_statements():
     statements = iam._transform_policy_statements(SINGLE_STATEMENT, "test_policy_id")
@@ -12,3 +18,13 @@ def test__generate_policy_statements():
     assert isinstance(statements[0]["Action"], list)
     assert isinstance(statements[0]["Resource"], list)
     assert statements[0]["id"] == "test_policy_id/statement/1"
+
+
+def test__parse_principal_entries():
+    principal_entries = iam._parse_principal_entries(SINGLE_PRINCIPAL)
+    assert isinstance(principal_entries, list)
+    assert len(principal_entries) == 4
+    assert principal_entries[0] == ("AWS", "test-role-1")
+    assert principal_entries[1] == ("Service", "test-service-1")
+    assert principal_entries[2] == ("Service", "test-service-2")
+    assert principal_entries[3] == ("Federated", "test-provider-1")
