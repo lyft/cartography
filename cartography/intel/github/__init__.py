b52c24d824d1f2dcea1832eef7e3b3bba634f958
Alex Chantavy <achantavy@lyft.com>
2020-06-22 22:06:56 -0700

    Fix comments and docs

--- cartography/intel/github/__init__.py
+++ cartography/intel/github/__init__.py
@@ -14,14 +14,18 @@ logger = logging.getLogger(__name__)
 @timeit
 def start_github_ingestion(neo4j_session, config):
     """
-    If this module is configured, perform ingestion of CRXcavator data. Otherwise warn and exit
+    If this module is configured, perform ingestion of Github  data. Otherwise warn and exit
     :param neo4j_session: Neo4J session for database interface
     :param config: A cartography.config object
     :return: None
     """
     if not config.github_config:
-        logger.warning('GitHub import is not configured - skipping this module. See docs to configure.')
+        logger.debug('GitHub import is not configured - skipping this module. See docs to configure.')
         return
+
+    # TODO - ask taya why this rather than `Github()`.
+    # TODO - ask taya how to get this in confidant
+    # TODO - why is b64 necessary?
     auth_tokens = json.loads(base64.b64decode(config.github_config).decode())
     common_job_parameters = {
         "UPDATE_TAG": config.update_tag,
