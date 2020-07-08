4a60653d5f343c95867e198d8c43b05e049f35b4
Danny Robinson <34581601+mdrdannyr@users.noreply.github.com>
2020-07-09 00:11:33 +0300

    Fix #341: NoSuchEntityException (#342)

--- cartography/intel/aws/iam.py
+++ cartography/intel/aws/iam.py
@@ -97,10 +97,15 @@ def get_user_managed_policy_data(boto3_session, user_list):
         name = user["UserName"]
         arn = user["Arn"]
         resource_user = resource_client.User(name)
-        policies[arn] = {
-            p.policy_name: p.default_version.document["Statement"]
-            for p in resource_user.attached_policies.all()
-        }
+        try:
+            policies[arn] = {
+                p.policy_name: p.default_version.document["Statement"]
+                for p in resource_user.attached_policies.all()
+            }
+        except resource_client.meta.client.exceptions.NoSuchEntityException:
+            logger.warning(
+                f"Could not get policies for user {name} due to NoSuchEntityException; skipping.",
+            )
     return policies
 
 
