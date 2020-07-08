735d359288f3a6b500a297d8d57fb2e6e2945ecb
Alex Chantavy <achantavy@lyft.com>
2020-07-07 21:03:47 -0700

    Fetch github users and repos using pagination with GraphQL

--- cartography/intel/github/github.py
+++ cartography/intel/github/github.py
@@ -1,3 +1,4 @@
+import json
 import logging
 from string import Template
 
@@ -21,6 +22,153 @@ def call_github_api(query, variables, token, api_url):
     response = requests.post(api_url, json={'query': query, 'variables': variables}, headers=headers)
     response.raise_for_status()
     return response.json()
+
+
+def get_org_users_and_repos_with_cursor(token, api_url, organization, member_cursor=None, repo_cursor=None):
+    """
+    Retrieve organization members and repos from Github GraphQL API using the given member_cursor and repo_cursor
+    parameters used for pagination.
+    :param token: The API token as string. Must have repo:* and org:read permissions.
+    :param api_url: The Github API endpoint as string.
+    :param organization: The name of the target Github organization as string.
+    :param member_cursor: The GraphQL cursor string (behaves like a page number) for Github member users in the given
+    organization. If None, the Github API will return the first page of members.
+    :param repo_cursor: The GraphQL cursor string (behaves like a page number) for Github repos in the given
+    organization. If None, the Github API will return the first page of repos.
+    :return: The raw response object from the requests.get().json() call.
+    """
+    query = """
+            query($login: String!, $memberCursor: String, $repoCursor: String) {
+            organization(login: $login)
+                {
+                    repositories(first: 100, after: $repoCursor){
+                        pageInfo{
+                            endCursor
+                            hasNextPage
+                        }
+                        nodes{
+                            id
+                            name
+                            nameWithOwner
+                            primaryLanguage{
+                                name
+                            }
+                            url
+                            sshUrl
+                            createdAt
+                            description
+                            updatedAt
+                            homepageUrl
+                            languages(first: 25){
+                                totalCount
+                                nodes{
+                                    name
+                                    id
+                                }
+                            }
+                            defaultBranchRef{
+                              name
+                              id
+                            }
+                            isPrivate
+                            isArchived
+                            isDisabled
+                            isLocked
+                            owner{
+                                login
+                                id
+                                __typename
+                            }
+                        }
+                    }
+                    membersWithRole(first:100, after: $memberCursor){
+                        edges {
+                            cursor
+                            hasTwoFactorEnabled
+                            node {
+                                login
+                                name
+                                isSiteAdmin
+                            }
+                            role
+                        }
+                        pageInfo{
+                            endCursor
+                            hasNextPage
+                        }
+                    }
+                }
+                rateLimit {
+                    limit
+                    cost
+                    remaining
+                    resetAt
+                }
+            }
+            """
+    gql_vars = {
+        'login': organization,
+        'memberCursor': member_cursor,
+        'repoCursor': repo_cursor,
+    }
+    gql_vars_json = json.dumps(gql_vars)
+    response = call_github_api(query, gql_vars_json, token, api_url)
+    return response
+
+
+@timeit
+def get_org_users_and_repos(token, api_url, organization):
+    """
+    Fetches and stores each page of users and repos returned from the Github GraphQL API. We perform pagination on more
+    than one resource here in order to save on API rate limits.  See
+    https://developer.github.com/v4/guides/resource-limitations/#rate-limit.
+    :param token: The API token as string. Must have repo:* and org:read permissions.
+    :param api_url: The Github API endpoint as string.
+    :param organization: The name of the target Github organization as string.
+    :return: A dict of dicts. Has shape {'repo_pages': {<page1>, ..., <pageN>}, 'user_pages': {page1>, ..., <pageM>}}
+    """
+    # Track which page of data from each resource type to retrieve. If `None`, the API will return the first page.
+    repo_cursor = None
+    user_cursor = None
+
+    # Whether there are additional pages to fetch for each resource.
+    repo_has_next_page = True
+    user_has_next_page = True
+
+    # It is very likely that we will have an unequal number of repo pages and user pages in our Github organization. As
+    # such, if we have fetched all repos but not all users, repo_has_next_page will be set to False and repo_cursor will
+    # be set to None. However, user_has_next_page will still be True and the same GraphQL query will be called with
+    # repo_cursor=None, causing the API to return the very first repo page again. To avoid accumulating duplicate pages,
+    # we use dicts where each key is the cursor and each value is the contents of that page.
+    # Fetching old pages is slightly wasteful in terms of data transfer, but there is no difference in API quota cost.
+    repo_pages = {}
+    user_pages = {}
+
+    while repo_has_next_page or user_has_next_page:
+        resp = get_org_users_and_repos_with_cursor(token, api_url, organization, user_cursor, repo_cursor)
+        resp_repos = resp['data']['organization']['repositories']
+        resp_users = resp['data']['organization']['membersWithRole']
+
+        # Update the cursor and determine if we need to continue fetching this resource
+        repo_cursor = resp_repos['pageInfo']['endCursor']
+        repo_pages[repo_cursor] = resp_repos['nodes']
+        if not resp_repos['pageInfo']['hasNextPage']:
+            repo_has_next_page = False
+
+        user_cursor = resp_users['pageInfo']['endCursor']
+        user_pages[user_cursor] = resp_users['edges']
+        if not resp_users['pageInfo']['hasNextPage']:
+            user_has_next_page = False
+
+        logger.debug(
+            f"User cursor:{user_cursor}, repo cursor:{repo_cursor},"
+            f"repo_has_next_page: {repo_has_next_page}, user_has_next_page: {user_has_next_page}",
+        )
+        logger.debug(
+            f"Cost of this API call: {resp['data']['rateLimit']['cost']} unit(s),"
+            f"Remaining: {resp['data']['rateLimit']['remaining']}",
+        )
+    return {'repo_pages': repo_pages, 'user_pages': user_pages}
 
 
 @timeit
