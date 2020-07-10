0a54206ddc82cc4269f98da6709bfd8da10233fc
Alex Chantavy <achantavy@lyft.com>
2020-07-10 10:34:41 -0700

    Retrieve GitHub users, refactor GitHub module (#337)
    * Fix doc code to not use reserved `str` word
    * Fetch GitHub users and repos using pagination with GraphQL
    * Split Github GraphQL to 2 functions for clarity, split Github module
    * Add GitHub users.load() and integration test
    * Add timeouts for GitHub requests
    * Use GitHub URL as ID for repo objects, refactor repo ingestion, update tests
    * Attach GitHub organization data to users
    * Correctly handle null branches

--- cartography/intel/github/__init__.py
+++ cartography/intel/github/__init__.py
@@ -4,7 +4,8 @@ import logging
 
 from requests import exceptions
 
-from cartography.intel.github.github import sync_github
+import cartography.intel.github.repos
+import cartography.intel.github.users
 from cartography.util import run_cleanup_job
 from cartography.util import timeit
 
@@ -14,14 +15,15 @@ logger = logging.getLogger(__name__)
 @timeit
 def start_github_ingestion(neo4j_session, config):
     """
-    If this module is configured, perform ingestion of CRXcavator data. Otherwise warn and exit
+    If this module is configured, perform ingestion of Github  data. Otherwise warn and exit
     :param neo4j_session: Neo4J session for database interface
     :param config: A cartography.config object
     :return: None
     """
     if not config.github_config:
-        logger.warning('GitHub import is not configured - skipping this module. See docs to configure.')
+        logger.debug('GitHub import is not configured - skipping this module. See docs to configure.')
         return
+
     auth_tokens = json.loads(base64.b64decode(config.github_config).decode())
     common_job_parameters = {
         "UPDATE_TAG": config.update_tag,
@@ -29,7 +31,14 @@ def start_github_ingestion(neo4j_session, config):
     # run sync for the provided github tokens
     for auth_data in auth_tokens['organization']:
         try:
-            sync_github(
+            cartography.intel.github.users.sync(
+                neo4j_session,
+                common_job_parameters,
+                auth_data['token'],
+                auth_data['url'],
+                auth_data['name'],
+            )
+            cartography.intel.github.repos.sync(
                 neo4j_session,
                 common_job_parameters,
                 auth_data['token'],
