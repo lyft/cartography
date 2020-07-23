d5d859cad615f9ad159b55f4c9aeb5fba89a331c
Alex Chantavy <achantavy@lyft.com>
2020-07-22 22:59:08 -0700

    Remove whitespace

--- cartography/intel/aws/permission_relationships.py
+++ cartography/intel/aws/permission_relationships.py
@@ -312,7 +312,7 @@ def parse_permission_relationships_file(file_path):
             relationship_mapping = yaml.load(f, Loader=yaml.FullLoader)
         return relationship_mapping
     except FileNotFoundError:
-        logger.warning(f"Permission relationships mapping file {file_path} not found, skipping ingestion ")
+        logger.warning(f"Permission relationships mapping file {file_path} not found, skipping ingestion")
         return []
 
 
