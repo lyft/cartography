e69862ed34440d4e6c5ba540993d4927c9eda3b4
Alex Chantavy <achantavy@lyft.com>
2020-11-10 13:24:09 -0800

    Comment out import

--- cartography/intel/aws/ec2/__init__.py
+++ cartography/intel/aws/ec2/__init__.py
@@ -5,7 +5,7 @@ from .instances import sync_ec2_instances
 from .key_pairs import sync_ec2_key_pairs
 from .load_balancer_v2s import sync_load_balancer_v2s
 from .load_balancers import sync_load_balancers
-from .network_interfaces import sync_network_interfaces
+# from .network_interfaces import sync_network_interfaces
 from .security_groups import sync_ec2_security_groupinfo
 from .subnets import sync_subnets
 from .tgw import sync_transit_gateways
