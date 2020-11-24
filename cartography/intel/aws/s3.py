ac64d5eb263ee790c0b0ead9d74950cd08e85709
shabin-slr <shabinmohd@gmail.com>
2020-11-24 12:33:47 +0400

    Re-use S3 client for AWS region while getting bucket details

--- cartography/intel/aws/s3.py
+++ cartography/intel/aws/s3.py
@@ -28,12 +28,19 @@ def get_s3_bucket_details(boto3_session, bucket_data):
     """
     Iterates over all S3 buckets. Yields bucket name (string) and pairs of S3 bucket policies (JSON) and ACLs (JSON)
     """
+    # a local store for s3 clients so that we may re-use clients for an AWS region
+    s3_regional_clients = {}
+
     # client = boto3_session.client('s3')
     for bucket in bucket_data['Buckets']:
         # Use us-east-1 region if no region was recognized for buckets
         # It was found that client.get_bucket_location does not return a region for buckets
         # in us-east-1 region
-        client = boto3_session.client('s3', bucket['Region'])
+        client = s3_regional_clients.get(bucket['Region'])
+        if(client == None):
+            client = boto3_session.client('s3', bucket['Region'])
+            s3_regional_clients[bucket['Region']] = client   
+        
         acl = get_acl(bucket, client)
         policy = get_policy(bucket, client)
         yield bucket['Name'], acl, policy
