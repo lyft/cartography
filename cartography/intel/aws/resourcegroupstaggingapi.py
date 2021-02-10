6016d4a106f86ffa19c859e79685f294cf7deadb
Alex Chantavy <achantavy@lyft.com>
2021-02-10 13:43:53 -0800

    Oops linter again

--- cartography/intel/aws/resourcegroupstaggingapi.py
+++ cartography/intel/aws/resourcegroupstaggingapi.py
@@ -121,7 +121,12 @@ def cleanup(neo4j_session, common_job_parameters):
 
 @timeit
 def sync(
-    neo4j_session, boto3_session, regions, aws_update_tag, common_job_parameters, tag_resource_type_mappings=TAG_RESOURCE_TYPE_MAPPINGS,
+    neo4j_session,
+    boto3_session,
+    regions,
+    aws_update_tag,
+    common_job_parameters,
+    tag_resource_type_mappings=TAG_RESOURCE_TYPE_MAPPINGS,
 ):
     for region in regions:
         logger.info("Syncing AWS tags for region '%s'.", region)
