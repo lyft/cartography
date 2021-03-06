2ff6c96420e7d84700905c422b20039457deb955
cpollard0 <chris.pollard@rocketmail.com>
2021-03-06 11:32:16 -0500

    Lint

--- tests/data/aws/ec2/internet_gateway.py
+++ tests/data/aws/ec2/internet_gateway.py
@@ -1,47 +1,47 @@
 DESCRIBE_GATEWAYS = {
-     "InternetGateways": [
-         {
-             "Attachments": [
-                 {
-                     "State": "available",
-                     "VpcId": "vpc-XXXXXXX",
-                 },
-             ],
-             "InternetGatewayId": "igw-XXXXXXX",
-             "OwnerId": "012345678912",
-             "Tags": [
-                 {
-                     "Key": "Name",
-                     "Value": "InternetGateway",
-                 },
-             ],
-         },
-         {
-             "Attachments": [
-                 {
-                     "State": "available",
-                     "VpcId": "vpc-XXXXXXX",
-                 },
-             ],
-             "InternetGatewayId": "igw-7e3a7c18",
-             "OwnerId": "012345678912",
-             "Tags": [
-                 {
-                     "Key": "AWSServiceAccount",
-                     "Value": "697148468905",
-                 },
-             ],
-         },
-         {
-             "Attachments": [
-                 {
-                     "State": "available",
-                     "VpcId": "vpc-XXXXXXX",
-                 },
-             ],
-             "InternetGatewayId": "igw-f1c81494",
-             "OwnerId": "012345678912",
-             "Tags": [],
-         },
-     ],
- }
+    "InternetGateways": [
+        {
+            "Attachments": [
+                {
+                    "State": "available",
+                    "VpcId": "vpc-XXXXXXX",
+                },
+            ],
+            "InternetGatewayId": "igw-XXXXXXX",
+            "OwnerId": "012345678912",
+            "Tags": [
+                {
+                    "Key": "Name",
+                    "Value": "InternetGateway",
+                },
+            ],
+        },
+        {
+            "Attachments": [
+                {
+                    "State": "available",
+                    "VpcId": "vpc-XXXXXXX",
+                },
+            ],
+            "InternetGatewayId": "igw-7e3a7c18",
+            "OwnerId": "012345678912",
+            "Tags": [
+                {
+                    "Key": "AWSServiceAccount",
+                    "Value": "697148468905",
+                },
+            ],
+        },
+        {
+            "Attachments": [
+                {
+                    "State": "available",
+                    "VpcId": "vpc-XXXXXXX",
+                },
+            ],
+            "InternetGatewayId": "igw-f1c81494",
+            "OwnerId": "012345678912",
+            "Tags": [],
+        },
+    ],
+}
