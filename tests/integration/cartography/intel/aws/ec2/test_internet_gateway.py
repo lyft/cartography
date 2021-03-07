0b2530a27bc47a111b2cae4779b6b5b9afd688e5
cpollard0 <chris.pollard@rocketmail.com>
2021-03-07 15:13:23 -0500

    Break out account relationship test

--- tests/integration/cartography/intel/aws/ec2/test_internet_gateway.py
+++ tests/integration/cartography/intel/aws/ec2/test_internet_gateway.py
@@ -1,5 +1,6 @@
 import cartography.intel.aws.ec2
 import tests.data.aws.ec2.internet_gateway
+import tests.integration.cartography.intel.aws.common
 
 TEST_ACCOUNT_ID = '012345678912'
 TEST_REGION = 'us-east-1'
@@ -31,12 +32,33 @@ def test_load_internet_gateways(neo4j_session):
 
     assert actual_nodes == expected_nodes
 
+
+def test_load_internet_gateway_relationships(neo4j_session):
+    tests.integration.cartography.intel.aws.common.create_test_account(neo4j_session, TEST_ACCOUNT_ID, TEST_UPDATE_TAG)
+
+    data = tests.data.aws.ec2.internet_gateway.DESCRIBE_GATEWAYS
+    cartography.intel.aws.ec2.internet_gateways.load_internet_gateways(
+        neo4j_session,
+        data,
+        TEST_REGION,
+        TEST_ACCOUNT_ID,
+        TEST_UPDATE_TAG,
+    )
+
+    expected = {
+        (TEST_ACCOUNT_ID, 'igw-1234XXX'),
+        (TEST_ACCOUNT_ID, 'igw-7e3a7c18'),
+        (TEST_ACCOUNT_ID, 'igw-f1c81494'),
+    }
+
     # Fetch relationships
     result = neo4j_session.run(
         """
-        MATCH (n1:AWSInternetGateway)<-[:RESOURCE]-(n2:AWSAccount) RETURN n1.id;
+        MATCH (n1:AWSInternetGateway)<-[:RESOURCE]-(n2:AWSAccount) RETURN n1.id, n2.id;
         """,
     )
-    actual = {n['n1.id'] for n in result}
+    actual = {
+        (n['n1.id'], n['n2.id']) for n in result
+    }
 
-    assert actual == expected_nodes
+    assert actual == expected
