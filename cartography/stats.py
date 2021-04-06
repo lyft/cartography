dc982ef616280c42e6e4704796afb8210c97d7e5
Payal Chheda <pchheda@lyft.com>
2021-04-05 22:43:57 -0700

    Add _StatsClientWrapper

--- cartography/stats.py
+++ cartography/stats.py
@@ -6,6 +6,20 @@ from statsd import StatsClient
 logger = logging.getLogger(__name__)
 
 
+class _StatsClientWrapper:
+    def __init__(self, client: StatsClient):
+        self._client = client
+
+    def set_client(self, client: StatsClient) -> None:
+        self._client = client
+
+    def get_client(self) -> StatsClient:
+        return self._client
+
+    def is_enabled(self) -> bool:
+        return self._client is not None
+
+
 class ScopedStatsClient:
     """
     A Proxy object for an underlying statsd client.
@@ -18,7 +32,7 @@ class ScopedStatsClient:
     newer_client.incr('bad') # Metric name = a.subsystem.bad
     """
 
-    def __init__(self, client: StatsClient, prefix: str = None):
+    def __init__(self, client: _StatsClientWrapper, prefix: str = None):
         self._client = client
         self._scope_prefix = prefix
 
@@ -30,11 +44,11 @@ class ScopedStatsClient:
         if not self._scope_prefix:
             prefix = scope
         else:
-            prefix = self._scope_prefix + "." + scope
+            prefix = f"{self._scope_prefix}.{scope}"
         return ScopedStatsClient(self._client, prefix)
 
     def is_enabled(self) -> bool:
-        return self._client is not None
+        return self._client.is_enabled()
 
     def incr(self, stat: str, count: int = 1, rate: float = 1.0) -> None:
         """
@@ -48,7 +62,7 @@ class ScopedStatsClient:
             if self._scope_prefix:
                 stat = f"{self._scope_prefix}.{stat}"
             logger.info(f"Stat being incremented is {stat}")
-            self._client.incr(stat, count, rate)
+            self._client.get_client().incr(stat, count, rate)
 
     def timer(self, stat: str, rate: float = 1.0):
         """
@@ -61,13 +75,16 @@ class ScopedStatsClient:
         if self.is_enabled():
             if self._scope_prefix:
                 stat = f"{self._scope_prefix}.{stat}"
-            return self._client.timer(stat, rate)
+            return self._client.get_client().timer(stat, rate)
         return None
+
+    def set_stats_client(self, stats_client: StatsClient) -> None:
+        self._client.set_client(stats_client)
 
 
 # Global _scoped_stats_client
 # Will be set when cartography.config.statsd_enabled is True
-_scoped_stats_client: ScopedStatsClient = ScopedStatsClient(None)
+_scoped_stats_client: ScopedStatsClient = ScopedStatsClient(_StatsClientWrapper(None))
 
 
 def set_stats_client(stats_client: StatsClient) -> None:
@@ -75,7 +92,7 @@ def set_stats_client(stats_client: StatsClient) -> None:
     This is used to set the module level stats client configured to talk with a statsd host
     """
     global _scoped_stats_client
-    _scoped_stats_client._client = stats_client
+    _scoped_stats_client.set_stats_client(stats_client)
 
 
 def get_stats_client(prefix: str) -> ScopedStatsClient:
