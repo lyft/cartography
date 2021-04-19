08616ab24031a99f085a534587d3b89d743619d9
Purusottam <mpurusottamc@gmail.com>
2021-04-19 16:07:03 -0700

    added eventgrid library

---
+++ libraries/eventgridlibrary.py
@@ -0,0 +1,41 @@
+import datetime
+import time
+
+from azure.core.credentials import AzureKeyCredential
+from azure.eventgrid import EventGridPublisherClient, EventGridEvent
+
+
+class EventGridLibrary():
+    def __init__(self, topic, access_key):
+        # authenticate client
+        self.credential = AzureKeyCredential(access_key)
+        self.client = EventGridPublisherClient(topic, self.credential)
+
+    def publish_event(self, message):
+        event_list = []
+
+        try:
+            event = EventGridEvent(
+                id=time.time(),
+                subject="inventoryviews-request",
+                data=message,
+                event_type="inventoryviews",
+                event_time=datetime.datetime.utcnow(),
+                data_version="1.0"
+            )
+
+            event_list.append(event)
+            self.client.send(event_list)
+
+            return {
+                "status": "success",
+                "message": "successfully published message to queue"
+            }
+
+        except Exception as e:
+            print(f'error while publishing message to queue: {e}')
+
+            return {
+                "status": "failure",
+                "message": "failed to publish message to queue"
+            }
