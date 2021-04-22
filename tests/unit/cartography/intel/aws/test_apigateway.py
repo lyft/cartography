c1c45abe41f1ad128c42f49b2b602185e029e7ed
ryohare <peyton.ohare@gmail.com>
2021-04-22 11:56:20 -0400

    fixing order

--- tests/unit/cartography/intel/aws/test_apigateway.py
+++ tests/unit/cartography/intel/aws/test_apigateway.py
@@ -1,5 +1,5 @@
-from cartography.intel.aws.apigateway import parse_policy
 import tests.data.aws.apigateway as test_data 
+from cartography.intel.aws.apigateway import parse_policy
 
 def test_parse_policy():
     res = parse_policy("1", test_data.DOUBLY_ESCAPED_POLICY)
