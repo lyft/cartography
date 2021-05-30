b1ff7de483b00c7635b84cb8b60d31d3d3da8cdd
Alex Chantavy <achantavy@lyft.com>
2021-05-29 21:53:30 -0700

    First draft of merge_nodes_with_unwind

--- cartography/graph/querybuilder.py
+++ cartography/graph/querybuilder.py
@@ -1,6 +1,12 @@
 from string import Template
+from typing import Dict
 from typing import List
 from typing import Tuple
+import logging
+
+import neo4j
+
+logger = logging.getLogger(__name__)
 
 
 def build_node_ingestion_query(node_label: str, id_field: str, field_list: List[Tuple[str, str]]) -> str:
@@ -103,3 +109,30 @@ def build_relationship_ingestion_query(
     else:
         ingest_query = ingest_preamble
     return ingest_query
+
+
+def merge_nodes_with_unwind(
+    tx: neo4j.Transaction, node_label: str, id_field: str, field_list: List[Tuple[str, str]],
+    node_data_list: List[Dict], batch_size: int, update_tag: int,
+) -> None:
+    if batch_size < 1:
+        raise ValueError("batch_size cannot be less than 1.")
+
+    ingest_query = build_node_ingestion_query(node_label, id_field, field_list)
+
+    chunk = node_data_list[:batch_size]
+    cursor = 0
+    while len(chunk) > 0:
+        query_args = {
+            'UpdateTag': update_tag,
+            'IdField': id_field,
+            'DictList': chunk,
+        }
+        try:
+            tx.run(ingest_query, **query_args)
+        except neo4j.ServiceUnavailable:
+            logger.error("Failed to merge nodes.", exc_info=True)
+            tx.rollback()
+            raise
+        cursor += batch_size
+        chunk = node_data_list[cursor:cursor+batch_size]
