c9c8740dc6b90b4d9c1c8fb7d319e1c4202ae332
Ryan Lane <ryan.lane@paypay-corp.co.jp>
2021-06-14 16:06:25 +0900

    RDS Cluster support

--- cartography/intel/aws/rds.py
+++ cartography/intel/aws/rds.py
@@ -2,6 +2,7 @@ import logging
 from typing import Any
 from typing import Dict
 from typing import List
+from typing import Optional
 
 import boto3
 import neo4j
@@ -11,6 +12,136 @@ from cartography.util import run_cleanup_job
 from cartography.util import timeit
 
 logger = logging.getLogger(__name__)
+
+
+@timeit
+def _dict_value_to_str(obj: Dict, key: str) -> Optional[str]:
+    """
+    Convert the value referenced by the key in the dict to a string, if it exists, and return it. If it doesn't exist,
+    return None.
+    """
+    value = obj.get(key)
+    if value is not None:
+        return str(value)
+    else:
+        return None
+
+
+@timeit
+@aws_handle_regions
+def get_rds_cluster_data(boto3_session: boto3.session.Session, region: str) -> List[Any]:
+    """
+    Create an RDS boto3 client and grab all the DBClusters.
+    """
+    client = boto3_session.client('rds', region_name=region)
+    paginator = client.get_paginator('describe_db_clusters')
+    instances: List[Any] = []
+    for page in paginator.paginate():
+        instances.extend(page['DBClusters'])
+
+    return instances
+
+
+@timeit
+def load_rds_clusters(
+    neo4j_session: neo4j.Session, data: Dict, region: str, current_aws_account_id: str,
+    aws_update_tag: int,
+) -> None:
+    """
+    Ingest the RDS clusters to neo4j and link them to necessary nodes.
+    """
+    ingest_rds_cluster = """
+    MERGE (cluster:RDSCluster{id: {DBClusterArn}})
+    ON CREATE SET cluster.firstseen = timestamp()
+    SET cluster.arn = {DBClusterArn},
+    cluster.allocated_storage = {AllocatedStorage},
+    cluster.availability_zones = {AvailabilityZones},
+    cluster.backup_retention_period = {BackupRetentionPeriod},
+    cluster.character_set_name = {CharacterSetName},
+    cluster.database_name = {DatabaseName},
+    cluster.db_cluster_identifier = {DBClusterIdentifier},
+    cluster.db_parameter_group = {DBClusterParameterGroup},
+    cluster.status = {Status},
+    cluster.earliest_restorable_time = {EarliestRestorableTime},
+    cluster.endpoint = {Endpoint},
+    cluster.reader_endpoint = {ReaderEndpoint},
+    cluster.multi_az = {MultiAZ},
+    cluster.engine = {Engine},
+    cluster.engine_version = {EngineVersion},
+    cluster.latest_restorable_time = {LatestRestorableTime},
+    cluster.port = {Port},
+    cluster.master_username = {MasterUsername},
+    cluster.preferred_backup_window = {PreferredBackupWindow},
+    cluster.preferred_maintenance_window = {PreferredMaintenanceWindow},
+    cluster.hosted_zone_id = {HostedZoneId},
+    cluster.storage_encrypted = {StorageEncrypted},
+    cluster.kms_key_id = {KmsKeyId},
+    cluster.db_cluster_resource_id = {DbClusterResourceId},
+    cluster.clone_group_id = {CloneGroupId},
+    cluster.cluster_create_time = {ClusterCreateTime},
+    cluster.earliest_backtrack_time = {EarliestBacktrackTime},
+    cluster.backtrack_window = {BacktrackWindow},
+    cluster.backtrack_consumed_change_records = {BacktrackConsumedChangeRecords},
+    cluster.capacity = {Capacity},
+    cluster.engine_mode = {EngineMode},
+    cluster.scaling_configuration_info_min_capacity = {ScalingConfigurationInfoMinCapacity},
+    cluster.scaling_configuration_info_max_capacity = {ScalingConfigurationInfoMaxCapacity},
+    cluster.scaling_configuration_info_auto_pause = {ScalingConfigurationInfoAutoPause},
+    cluster.deletion_protection = {DeletionProtection},
+    cluster.lastupdated = {aws_update_tag}
+    WITH cluster
+    MATCH (aa:AWSAccount{id: {AWS_ACCOUNT_ID}})
+    MERGE (aa)-[r:RESOURCE]->(cluster)
+    ON CREATE SET r.firstseen = timestamp()
+    SET r.lastupdated = {aws_update_tag}
+    """
+    for cluster in data:
+        # TODO: track read replicas
+        # TODO: track associated roles
+        # TODO: track security groups
+        # TODO: track subnet groups
+
+        neo4j_session.run(
+            ingest_rds_cluster,
+            DBClusterArn=cluster['DBClusterArn'],
+            AllocatedStorage=cluster.get('AllocatedStorage'),
+            AvailabilityZones=cluster.get('AvailabilityZones', []),
+            BackupRetentionPeriod=cluster.get('BackupRetentionPeriod'),
+            CharacterSetName=cluster.get('CharacterSetName'),
+            DatabaseName=cluster.get('DatabaseName'),
+            DBClusterIdentifier=cluster.get('DBClusterIdentifier'),
+            DBClusterParameterGroup=cluster.get('DBClusterParameterGroup'),
+            Status=cluster.get('Status'),
+            EarliestRestorableTime=_dict_value_to_str(cluster, 'EarliestRestorableTime'),
+            Endpoint=cluster.get('Endpoint'),
+            ReaderEndpoint=cluster.get('ReaderEndpoint'),
+            MultiAZ=cluster.get('MultiAZ'),
+            Engine=cluster.get('Engine'),
+            EngineVersion=cluster.get('EngineVersion'),
+            LatestRestorableTime=_dict_value_to_str(cluster, 'LatestRestorableTime'),
+            Port=cluster.get('Port'),
+            MasterUsername=cluster.get('MasterUsername'),
+            PreferredBackupWindow=cluster.get('PreferredBackupWindow'),
+            PreferredMaintenanceWindow=cluster.get('PreferredMaintenanceWindow'),
+            HostedZoneId=cluster.get('HostedZoneId'),
+            StorageEncrypted=cluster.get('StorageEncrypted'),
+            KmsKeyId=cluster.get('KmsKeyId'),
+            DbClusterResourceId=cluster.get('DbClusterResourceId'),
+            CloneGroupId=cluster.get('CloneGroupId'),
+            ClusterCreateTime=_dict_value_to_str(cluster, 'ClusterCreateTime'),
+            EarliestBacktrackTime=_dict_value_to_str(cluster, 'EarliestBacktrackTime'),
+            BacktrackWindow=cluster.get('BacktrackWindow'),
+            BacktrackConsumedChangeRecords=cluster.get('BacktrackConsumedChangeRecords'),
+            Capacity=cluster.get('Capacity'),
+            EngineMode=cluster.get('EngineMode'),
+            ScalingConfigurationInfoMinCapacity=cluster.get('ScalingConfigurationInfo', {}).get('MinCapacity'),
+            ScalingConfigurationInfoMaxCapacity=cluster.get('ScalingConfigurationInfo', {}).get('MaxCapacity'),
+            ScalingConfigurationInfoAutoPause=cluster.get('ScalingConfigurationInfo', {}).get('AutoPause'),
+            DeletionProtection=cluster.get('DeletionProtection'),
+            Region=region,
+            AWS_ACCOUNT_ID=current_aws_account_id,
+            aws_update_tag=aws_update_tag,
+        )
 
 
 @timeit
@@ -78,16 +209,18 @@ def load_rds_instances(
     SET r.lastupdated = {aws_update_tag}
     """
     read_replicas = []
+    clusters = []
 
     for rds in data:
-        instance_create_time = str(rds['InstanceCreateTime']) if 'InstanceCreateTime' in rds else None
-        latest_restorable_time = str(rds['LatestRestorableTime']) if 'LatestRestorableTime' in rds else None
-
         ep = _validate_rds_endpoint(rds)
 
         # Keep track of instances that are read replicas so we can attach them to their source instances later
         if rds.get("ReadReplicaSourceDBInstanceIdentifier"):
             read_replicas.append(rds)
+
+        # Keep track of instances that are cluster members so we can attach them to their source clusters later
+        if rds.get("DBClusterIdentifier"):
+            clusters.append(rds)
 
         neo4j_session.run(
             ingest_rds_instance,
@@ -97,7 +230,7 @@ def load_rds_instances(
             Engine=rds.get('Engine'),
             MasterUsername=rds.get('MasterUsername'),
             DBName=rds.get('DBName'),
-            InstanceCreateTime=instance_create_time,
+            InstanceCreateTime=_dict_value_to_str(rds, 'InstanceCreateTime'),
             AvailabilityZone=rds.get('AvailabilityZone'),
             MultiAZ=rds.get('MultiAZ'),
             EngineVersion=rds.get('EngineVersion'),
@@ -114,7 +247,7 @@ def load_rds_instances(
             DeletionProtection=rds.get('DeletionProtection'),
             BackupRetentionPeriod=rds.get('BackupRetentionPeriod'),
             PreferredBackupWindow=rds.get('PreferredBackupWindow'),
-            LatestRestorableTime=latest_restorable_time,
+            LatestRestorableTime=_dict_value_to_str(rds, 'LatestRestorableTime'),
             PreferredMaintenanceWindow=rds.get('PreferredMaintenanceWindow'),
             EndpointAddress=ep.get('Address'),
             EndpointHostedZoneId=ep.get('HostedZoneId'),
@@ -128,6 +261,7 @@ def load_rds_instances(
         _attach_ec2_security_groups(neo4j_session, rds, aws_update_tag)
         _attach_ec2_subnet_groups(neo4j_session, rds, region, current_aws_account_id, aws_update_tag)
     _attach_read_replicas(neo4j_session, read_replicas, aws_update_tag)
+    _attach_clusters(neo4j_session, clusters, aws_update_tag)
 
 
 @timeit
@@ -241,6 +375,27 @@ def _attach_read_replicas(neo4j_session: neo4j.Session, read_replicas: Dict, aws
             attach_replica_to_source,
             ReplicaArn=replica['DBInstanceArn'],
             SourceInstanceIdentifier=replica['ReadReplicaSourceDBInstanceIdentifier'],
+            aws_update_tag=aws_update_tag,
+        )
+
+
+@timeit
+def _attach_clusters(neo4j_session: neo4j.Session, cluster_members: Dict, aws_update_tag: int) -> None:
+    """
+    Attach cluster members to their source clusters
+    """
+    attach_member_to_source = """
+    MATCH (member:RDSInstance{id:{DBInstanceArn}}),
+    (source:RDSCluster{db_cluster_identifier:{DBClusterIdentifier}})
+    MERGE (member)-[r:IS_CLUSTER_MEMBER_OF]->(source)
+    ON CREATE SET r.firstseen = timestamp()
+    SET r.lastupdated = {aws_update_tag}
+    """
+    for member in cluster_members:
+        neo4j_session.run(
+            attach_member_to_source,
+            DBInstanceArn=member['DBInstanceArn'],
+            DBClusterIdentifier=member['DBClusterIdentifier'],
             aws_update_tag=aws_update_tag,
         )
 
