2a017318f5bb7b6aa54823a9a0d2c7f812a1633a
Ryan Lane <ryan.lane@paypay-corp.co.jp>
2021-07-19 15:29:02 +0900

    Add tests for encryption

--- tests/integration/cartography/intel/aws/test_s3.py
+++ tests/integration/cartography/intel/aws/test_s3.py
@@ -19,22 +19,25 @@ def test_load_s3_buckets(neo4j_session, *args):
             "bucket-1",
             "bucket-1",
             "eu-west-1",
+            False,
         ),
         (
             "bucket-2",
             "bucket-2",
             "me-south-1",
+            False,
         ),
         (
             "bucket-3",
             "bucket-3",
             None,
+            False,
         ),
     }
 
     nodes = neo4j_session.run(
         """
-        MATCH (s:S3Bucket) return s.id, s.name, s.region
+        MATCH (s:S3Bucket) return s.id, s.name, s.region, s.default_encryption
         """,
     )
     actual_nodes = {
@@ -42,6 +45,44 @@ def test_load_s3_buckets(neo4j_session, *args):
             n['s.id'],
             n['s.name'],
             n['s.region'],
+            n['s.default_encryption'],
+        )
+        for n in nodes
+    }
+    assert actual_nodes == expected_nodes
+
+
+def test_load_s3_encryption(neo4j_session, *args):
+    """
+    Ensure that expected bucket gets loaded with their encryption fields.
+    """
+    data = tests.data.aws.s3.GET_ENCRYPTION
+    cartography.intel.aws.s3.load_s3_encryption(neo4j_session, data, TEST_UPDATE_TAG)
+
+    expected_nodes = {
+        (
+            "bucket-1",
+            True,
+            "aws:kms",
+            "arn:aws:kms:eu-east-1:000000000000:key/9a1ad414-6e3b-47ce-8366-6b8f26ba467d",
+            False,
+        ),
+    }
+
+    nodes = neo4j_session.run(
+        """
+        MATCH (s:S3Bucket)
+        WHERE s.id = 'bucket-1'
+        RETURN s.id, s.default_encryption, s.encryption_algorithm, s.encryption_key_id, s.bucket_key_enabled
+        """,
+    )
+    actual_nodes = {
+        (
+            n['s.id'],
+            n['s.default_encryption'],
+            n['s.encryption_algorithm'],
+            n['s.encryption_key_id'],
+            n['s.bucket_key_enabled'],
         )
         for n in nodes
     }
