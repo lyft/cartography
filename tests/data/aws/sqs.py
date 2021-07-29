bd3c226bf0f071baac4f05f0797d42a882192dbe
Ryan Lane <ryan.lane@paypay-corp.co.jp>
2021-07-29 17:05:53 +0900

    Fix test data

--- tests/data/aws/sqs.py
+++ tests/data/aws/sqs.py
@@ -1,6 +1,6 @@
 GET_SQS_QUEUE_ATTRIBUTES = {
     'https://us-east-1.queue.amazonaws.com/000000000000/test-queue-1': {
-        'QueueARN': 'arn:aws:secretsmanager:us-east-1:000000000000:test-queue-1',
+        'QueueArn': 'arn:aws:secretsmanager:us-east-1:000000000000:test-queue-1',
         'CreatedTimestamp': '1627539901900',
         'LastModifiedTimestamp': '1627539901900',
         'RedrivePolicy': {
@@ -10,7 +10,7 @@ GET_SQS_QUEUE_ATTRIBUTES = {
         'VisibilityTimeout': '10',
     },
     'https://us-east-1.queue.amazonaws.com/000000000000/test-queue-2': {
-        'QueueARN': 'arn:aws:secretsmanager:us-east-1:000000000000:test-queue-2',
+        'QueueArn': 'arn:aws:secretsmanager:us-east-1:000000000000:test-queue-2',
         'CreatedTimestamp': '1627539901900',
         'LastModifiedTimestamp': '1627539901900',
         'VisibilityTimeout': '10',
