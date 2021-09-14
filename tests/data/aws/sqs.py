b0d71c1e4509988ad611d86680a9f20864b1d2c3
Ryan Lane <ryan.lane@paypay-corp.co.jp>
2021-09-14 10:17:41 +0900

    fix json string

--- tests/data/aws/sqs.py
+++ tests/data/aws/sqs.py
@@ -3,7 +3,7 @@ GET_SQS_QUEUE_ATTRIBUTES = {
         'QueueArn': 'arn:aws:secretsmanager:us-east-1:000000000000:test-queue-1',
         'CreatedTimestamp': '1627539901900',
         'LastModifiedTimestamp': '1627539901900',
-        'RedrivePolicy': "{'deadLetterTargetArn': 'arn:aws:secretsmanager:us-east-1:000000000000:test-queue-2', 'maxReceiveCount': '1'}",  # noqa: E501
+        'RedrivePolicy': '{"deadLetterTargetArn": "arn:aws:secretsmanager:us-east-1:000000000000:test-queue-2", "maxReceiveCount": "1"}',  # noqa: E501
         'VisibilityTimeout': '10',
     },
     'https://us-east-1.queue.amazonaws.com/000000000000/test-queue-2': {
