1755d2958afb3c8b0b7c9b72a2c061cbe8ceb347
Ganesh <ganesh@Ganeshs-MacBook-Pro.local>
2021-11-29 21:37:29 +0530

    minor change in init.py

--- cartography/intel/azure/__init__.py
+++ cartography/intel/azure/__init__.py
@@ -11,6 +11,7 @@ from . import sql
 from . import storage
 from . import subscription
 from . import tenant
+from . import iam
 from .util.credentials import Authenticator
 from .util.credentials import Credentials
 from cartography.config import Config
@@ -27,6 +28,8 @@ def _sync_one_subscription(
     cosmosdb.sync(neo4j_session, credentials.arm_credentials, subscription_id, update_tag, common_job_parameters)
     sql.sync(neo4j_session, credentials.arm_credentials, subscription_id, update_tag, common_job_parameters)
     storage.sync(neo4j_session, credentials.arm_credentials, subscription_id, update_tag, common_job_parameters)
+    iam.sync_tenant_applications(neo4j_session, credentials.aad_graph_credentials,
+                                 credentials.tenant_id, update_tag, common_job_parameters)
 
 
 def _sync_tenant(
