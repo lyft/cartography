f76a9a8db38cf5fec6444ef83ca57993d6f64d97
Ryan Lane <ryan.lane@paypay-corp.co.jp>
2022-01-28 06:30:45 +0900

    Add logo and favicon to textless svg icon (#755)

--- docs/conf.py
+++ docs/conf.py
@@ -149,12 +149,12 @@ html_sidebars = {
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-# html_logo = "images/logo-vertical.svg"
+html_logo = "images/logo-vertical.svg"
 
 # The name of an image file (relative to this directory) to use as a favicon of
 # the docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
 # pixels large.
-# html_favicon = 'favicon.ico'
+html_favicon = 'images/logo-vertical.svg'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
