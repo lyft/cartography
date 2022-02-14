ce83417438de4e1efc5e21eb4a693fac25fd3d41
Ramon Petgrave <ramonp@lyft.com>
2022-02-14 17:47:00 -0500

    max kubernetes to 21.7.0

--- setup.py
+++ setup.py
@@ -52,7 +52,7 @@ setup(
         "azure-mgmt-storage>=16.0.0",
         "azure-mgmt-sql<=1.0.0",
         "azure-identity>=1.5.0",
-        "kubernetes>=18.20.0",
+        "kubernetes>=18.20.0,<=21.7.0",
         "pdpyras>=4.3.0",
     ],
     extras_require={
