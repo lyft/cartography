9fbdbe11150acbd7b85c1ccc1442e232f0d54ce9
marco-lancini <info@marcolancini.it>
2022-04-19 16:49:38 +0100

    Remove unused import

--- cartography/intel/aws/ec2/elastic_ip_addresses.py
+++ cartography/intel/aws/ec2/elastic_ip_addresses.py
@@ -4,7 +4,6 @@ from typing import List
 
 import boto3
 import neo4j
-from botocore.exceptions import ClientError
 
 from .util import get_botocore_config
 from cartography.util import aws_handle_regions
