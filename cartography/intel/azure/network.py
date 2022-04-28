1247ae0be9a3bff231eca1a6ab2b179ce37cb8eb
Ganesh Jagdale <jagdaleganesh9545@gmail.com>
2022-04-28 17:59:21 +0530

    minor change for AzurePublicIpAdress

--- cartography/intel/azure/network.py
+++ cartography/intel/azure/network.py
@@ -499,8 +499,8 @@ def _load_public_ip_addresses_tx(
     ON CREATE SET n.firstseen = timestamp(),
     n.type = address.type,
     n.location = address.location,
-    n.region = address.location
-    n.isPublicFacing = true,
+    n.region = address.location,
+    n.isPublicFacing = true
     SET n.lastupdated = {update_tag},
     n.name = address.name,
     n.etag=address.etag
