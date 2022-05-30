e9907a46c228960882809c9f21f23519abd452be
Ryan Lane <ryan.lane@paypay-corp.co.jp>
2022-05-30 13:55:30 +0900

    Fix lint

--- cartography/intel/cve/__init__.py
+++ cartography/intel/cve/__init__.py
@@ -5,7 +5,7 @@ import neo4j
 
 from cartography.config import Config
 from cartography.intel.cve import feed
-from cartography.util import get_stats_client
+from cartography.stats import get_stats_client
 from cartography.util import merge_module_sync_metadata
 from cartography.util import timeit
 
