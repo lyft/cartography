b3f29ed65a6bf04fe1e4ab19b529ce007368c5b4
Ryan Lane <ryan.lane@paypay-corp.co.jp>
2022-06-04 09:06:13 +0900

    Add intel module to sync CVE data from NIST (#794)

--- cartography/sync.py
+++ cartography/sync.py
@@ -12,6 +12,7 @@ import cartography.intel.azure
 import cartography.intel.create_indexes
 import cartography.intel.crowdstrike
 import cartography.intel.crxcavator.crxcavator
+import cartography.intel.cve
 import cartography.intel.digitalocean
 import cartography.intel.gcp
 import cartography.intel.github
@@ -168,6 +169,7 @@ def build_default_sync():
         ('gcp', cartography.intel.gcp.start_gcp_ingestion),
         ('gsuite', cartography.intel.gsuite.start_gsuite_ingestion),
         ('crxcavator', cartography.intel.crxcavator.start_extension_ingestion),
+        ('cve', cartography.intel.cve.start_cve_ingestion),
         ('okta', cartography.intel.okta.start_okta_ingestion),
         ('github', cartography.intel.github.start_github_ingestion),
         ('digitalocean', cartography.intel.digitalocean.start_digitalocean_ingestion),
