c230cefd2d4b5e5935941621e965b479b6969532
Chaim Sanders <csanders-git@users.noreply.github.com>
2022-07-06 22:53:14 -0700

    Additional linting updates to utils.py

--- cartography/intel/oci/utils.py
+++ cartography/intel/oci/utils.py
@@ -1,10 +1,11 @@
 # Copyright (c) 2020, Oracle and/or its affiliates.
 # OCI intel module - utility functions
-from typing import Dict
-from typing import Any
-from typing import List
 
 import json
+from typing import Any
+from typing import Dict
+from typing import List
+
 import neo4j
 
 
@@ -29,7 +30,8 @@ def replace_char_in_dict(in_dict: Dict[str, Any]) -> Dict[str, Any]:
 # Grab list of all compartments and sub-compartments in neo4j already populated by iam.
 def get_compartments_in_tenancy(neo4j_session: neo4j.Session, tenancy_id: str) -> neo4j.BoltStatementResult:
     query = "MATCH (OCITenancy{ocid: {OCI_TENANCY_ID}})-[*]->(compartment:OCICompartment) " \
-            "return DISTINCT compartment.name as name, compartment.ocid as ocid, compartment.compartmentid as compartmentid;"
+            "return DISTINCT compartment.name as name, compartment.ocid as ocid, " \
+            "compartment.compartmentid as compartmentid;"
     return neo4j_session.run(query, OCI_TENANCY_ID=tenancy_id)
 
 
@@ -43,13 +45,13 @@ def get_groups_in_tenancy(neo4j_session: neo4j.Session, tenancy_id: str) -> neo4
 # Grab list of all policies in neo4j already populated by iam.
 def get_policies_in_tenancy(neo4j_session: neo4j.Session, tenancy_id: str) -> neo4j.BoltStatementResult:
     query = "MATCH (OCITenancy{ocid: {OCI_TENANCY_ID}})-[*]->(policy:OCIPolicy)" \
-            "return DISTINCT policy.name as name, policy.ocid as ocid, policy.statements as statements, policy.compartmentid as compartmentid;"
+            "return DISTINCT policy.name as name, policy.ocid as ocid, policy.statements as statements, " \
+            "policy.compartmentid as compartmentid;"
     return neo4j_session.run(query, OCI_TENANCY_ID=tenancy_id)
 
 
 # Grab list of all regions in neo4j already populated by iam.
 def get_regions_in_tenancy(neo4j_session: neo4j.Session, tenancy_id: str) -> neo4j.BoltStatementResult:
-    breakpoint()
     query = "MATCH (OCITenancy{ocid: {OCI_TENANCY_ID}})-->(region:OCIRegion)" \
             "return DISTINCT region.name as name, region.key as key;"
     return neo4j_session.run(query, OCI_TENANCY_ID=tenancy_id)
@@ -58,8 +60,10 @@ def get_regions_in_tenancy(neo4j_session: neo4j.Session, tenancy_id: str) -> neo
 # Grab list of all security groups in neo4j already populated by network. Need to handle regions for this one.
 def get_security_groups_in_tenancy(
     neo4j_session: neo4j.Session,
-    tenancy_id: str, region: str
+    tenancy_id: str, region: str,
 ) -> neo4j.BoltStatementResult:
-    query = "MATCH (OCITenancy{ocid: {OCI_TENANCY_ID}})-[*]->(security_group:OCINetworkSecurityGroup)-[OCI_REGION]->(region:OCIRegion{name: {OCI_REGION}})" \
-            "return DISTINCT security_group.name as name, security_group.ocid as ocid, security_group.compartmentid as compartmentid;"
+    query = "MATCH (OCITenancy{ocid: {OCI_TENANCY_ID}})-[*]->(security_group:OCINetworkSecurityGroup)-[OCI_REGION]->" \
+            "(region:OCIRegion{name: {OCI_REGION}})" \
+            "return DISTINCT security_group.name as name, security_group.ocid as ocid, security_group.compartmentid " \
+            "as compartmentid;"
     return neo4j_session.run(query, OCI_TENANCY_ID=tenancy_id, OCI_REGION=region)
