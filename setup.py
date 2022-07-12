8e8637188492842946c8dde1ee360baa638bf277
Ramon Petgrave <ramonp@lyft.com>
2022-07-12 12:18:43 -0400

    neo4j==4.4.5

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ setup(
         "boto3>=1.15.1",
         "botocore>=1.18.1",
         "dnspython>=1.15.0",
-        "neo4j==4.4.4",
+        "neo4j==4.4.5",
         "neobolt>=1.7.0,<4.0.0",
         "policyuniverse>=1.1.0.0",
         "google-api-python-client>=1.7.8",
