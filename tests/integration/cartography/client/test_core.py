d86201bee2449ee4c822cdada937c2f43d78c1d0
Alex Chantavy <achantavy@lyft.com>
2022-07-21 14:30:48 -0700

    Docs and tests

--- tests/integration/cartography/client/test_core.py
+++ tests/integration/cartography/client/test_core.py
@@ -25,6 +25,18 @@ def test_read_list_of_values_tx(neo4j_session):
     assert values == ["Homer", "Lisa", "Marge"]
 
 
+def test_read_list_of_values_tx_assert_returns_first_field_only(neo4j_session):
+    # Arrange
+    _ensure_test_data(neo4j_session)
+
+    # Act: test a query string that returns more than one field
+    query = "MATCH (a:TestNode) RETURN a.age, a.name ORDER BY a.name"
+    values = neo4j_session.read_transaction(read_list_of_tuples_tx, query)
+
+    # Assert that only the first field is returned for each value in the list.
+    assert values == ["Homer", "Lisa", "Marge"]
+
+
 def test_read_single_value_tx(neo4j_session):
     # Arrange
     _ensure_test_data(neo4j_session)
@@ -34,6 +46,18 @@ def test_read_single_value_tx(neo4j_session):
     value = neo4j_session.read_transaction(read_single_value_tx, query)
 
     # Assert
+    assert value == 8
+
+
+def test_read_single_value_tx_assert_returns_first_of_list_only(neo4j_session):
+    # Arrange
+    _ensure_test_data(neo4j_session)
+
+    # Act
+    query = """MATCH (a:TestNode) RETURN a.age ORDER BY a.age"""
+    value = neo4j_session.read_transaction(read_single_value_tx, query)
+
+    # Assert that we only return the first value if the query happesn to return more than one value
     assert value == 8
 
 
@@ -61,6 +85,18 @@ def test_read_single_dict_tx(neo4j_session):
 
     # Assert
     assert {'name': 'Homer', 'age': 39} == result
+
+
+def test_read_single_dict_tx_assert_return_first_of_list_only(neo4j_session):
+    # Arrange
+    _ensure_test_data(neo4j_session)
+
+    # Act
+    query = """MATCH (a:TestNode) RETURN a.name AS name, a.age AS age ORDER BY age"""
+    result = neo4j_session.read_transaction(read_single_dict_tx, query)
+
+    # Assert that this func returns the first item of the list and not the whole list
+    assert {'name': 'Lisa', 'age': 8} == result
 
 
 def test_read_list_of_tuples_tx(neo4j_session):
