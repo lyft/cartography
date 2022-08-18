37206afcba7b49586885ebf775e551d7b22b7831
Ganesh Jagdale <jagdaleganesh9545@gmail.com>
2022-08-18 17:02:42 +0530

    Added support for GCP Cloud Logging

---
+++ tests/data/gcp/cloud_logging.py
@@ -0,0 +1,12 @@
+TEST_METRICS=[
+    {
+        'metric_name': 'metric123',
+        'id': 'projects/project123/metrics/metric123',
+        'kmsKeyName': 'xyz',
+        'region': 'us-east-a1',
+        'description': 'jbjbjdqwnknkd',
+        'bucket_name': 'xyx',
+        'disabled': False,
+
+    }
+]
