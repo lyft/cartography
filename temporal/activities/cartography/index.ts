2d8b1b159b5d7ae9925dbd027a999813c96901ec
ong teck wu <teckwu@borneo.io>
2022-08-29 18:04:44 +0800

    move temporal code into cartography-service

--- temporal/activities/cartography/index.ts
+++
@@ -1,8 +0,0 @@
-import { CartographyResult } from './types'
-
-// Interfaces for activities implemented by Python workers
-type activities = {
-  cartographyRun: () => Promise<CartographyResult>
-}
-
-export default activities;
