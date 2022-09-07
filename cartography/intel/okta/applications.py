83149247f9bc2d64f47721fc9d703389fd050650
Ramon Petgrave <ramonp@lyft.com>
2022-09-07 18:17:33 -0400

    Revert "Revert "Revert "convert python code"""
    This reverts commit 60ca271feb5f133f69ec09950646e32b51b68131.

--- cartography/intel/okta/applications.py
+++ cartography/intel/okta/applications.py
@@ -252,9 +252,9 @@ def _load_okta_applications(
     :return: Nothing
     """
     ingest_statement = """
-    MATCH (org:OktaOrganization{id: $ORG_ID})
+    MATCH (org:OktaOrganization{id: {ORG_ID}})
     WITH org
-    UNWIND $APP_LIST as app_data
+    UNWIND {APP_LIST} as app_data
     MERGE (new_app:OktaApplication{id: app_data.id})
     ON CREATE SET new_app.firstseen = timestamp()
     SET new_app.name = app_data.name,
@@ -265,11 +265,11 @@ def _load_okta_applications(
     new_app.activated = app_data.activated,
     new_app.features = app_data.features,
     new_app.sign_on_mode = app_data.sign_on_mode,
-    new_app.lastupdated = $okta_update_tag
+    new_app.lastupdated = {okta_update_tag}
     WITH org, new_app
     MERGE (org)-[org_r:RESOURCE]->(new_app)
     ON CREATE SET org_r.firstseen = timestamp()
-    SET org_r.lastupdated = $okta_update_tag
+    SET org_r.lastupdated = {okta_update_tag}
     """
 
     neo4j_session.run(
@@ -294,14 +294,14 @@ def _load_application_user(
     :return: Nothing
     """
     ingest = """
-    MATCH (app:OktaApplication{id: $APP_ID})
+    MATCH (app:OktaApplication{id: {APP_ID}})
     WITH app
-    UNWIND $USER_LIST as user_id
+    UNWIND {USER_LIST} as user_id
     MATCH (user:OktaUser{id: user_id})
     WITH app, user
     MERGE (user)-[r:APPLICATION]->(app)
     ON CREATE SET r.firstseen = timestamp()
-    SET r.lastupdated = $okta_update_tag
+    SET r.lastupdated = {okta_update_tag}
     """
 
     neo4j_session.run(
@@ -326,14 +326,14 @@ def _load_application_group(
     :return: Nothing
     """
     ingest = """
-    MATCH (app:OktaApplication{id: $APP_ID})
+    MATCH (app:OktaApplication{id: {APP_ID}})
     WITH app
-    UNWIND $GROUP_LIST as group_id
+    UNWIND {GROUP_LIST} as group_id
     MATCH (group:OktaGroup{id: group_id})
     WITH app, group
     MERGE (group)-[r:APPLICATION]->(app)
     ON CREATE SET r.firstseen = timestamp()
-    SET r.lastupdated = $okta_update_tag
+    SET r.lastupdated = {okta_update_tag}
     """
 
     neo4j_session.run(
@@ -360,17 +360,17 @@ def _load_application_reply_urls(
     if not reply_urls:
         return
     ingest = """
-    MATCH (app:OktaApplication{id: $APP_ID})
+    MATCH (app:OktaApplication{id: {APP_ID}})
     WITH app
-    UNWIND $URL_LIST as url_list
+    UNWIND {URL_LIST} as url_list
     MERGE (uri:ReplyUri{id: url_list})
     ON CREATE SET uri.firstseen = timestamp()
     SET uri.uri = url_list,
-    uri.lastupdated = $okta_update_tag
+    uri.lastupdated = {okta_update_tag}
     WITH app, uri
     MERGE (uri)<-[r:REPLYURI]-(app)
     ON CREATE SET r.firstseen = timestamp()
-    SET r.lastupdated = $okta_update_tag
+    SET r.lastupdated = {okta_update_tag}
     """
 
     neo4j_session.run(
