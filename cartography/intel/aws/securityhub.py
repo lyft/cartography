8d8f82234b17e1119c93e24d16d43c35d0b18cbb
Ramon Petgrave <32398091+ramonpetgrave64@users.noreply.github.com>
2022-09-12 16:36:23 -0400

    convert securityhub syntax (#955)
    See #914

--- cartography/intel/aws/securityhub.py
+++ cartography/intel/aws/securityhub.py
@@ -39,16 +39,16 @@ def load_hub(
     aws_update_tag: int,
 ) -> None:
     ingest_hub = """
-    WITH {Hub} AS hub
+    WITH $Hub AS hub
     MERGE (n:SecurityHub{id: hub.HubArn})
     ON CREATE SET n.firstseen = timestamp()
     SET n.subscribed_at = hub.SubscribedAt, n.auto_enable_controls = hub.AutoEnableControls,
-        n.lastupdated = {aws_update_tag}
+        n.lastupdated = $aws_update_tag
     WITH n
-    MATCH (owner:AWSAccount{id: {AWS_ACCOUNT_ID}})
+    MATCH (owner:AWSAccount{id: $AWS_ACCOUNT_ID})
     MERGE (owner)-[r:RESOURCE]->(n)
     ON CREATE SET r.firstseen = timestamp()
-    SET r.lastupdated = {aws_update_tag}
+    SET r.lastupdated = $aws_update_tag
     """
     neo4j_session.run(
         ingest_hub,
