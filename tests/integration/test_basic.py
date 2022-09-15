2032d2513195a54fea2e7c38612bf0fbab5040a0
Ramon Petgrave <32398091+ramonpetgrave64@users.noreply.github.com>
2022-09-15 18:02:35 -0400

    convert syntax for indexes (#985)
    See Neo4j 4.x Upgrade #914

--- tests/integration/test_basic.py
+++ tests/integration/test_basic.py
@@ -1,5 +1,6 @@
 import neo4j
 
+from cartography.intel import create_indexes
 from tests.integration import settings
 
 
@@ -7,3 +8,9 @@ def test_neo4j_connection():
     driver = neo4j.GraphDatabase.driver(settings.get("NEO4J_URL"))
     with driver.session() as session:
         session.run("CALL db.indexes();")
+
+
+def test_create_indexes(neo4j_session):
+    # ensure the idempotency of creating indexes
+    create_indexes.run(neo4j_session, None)
+    create_indexes.run(neo4j_session, None)
