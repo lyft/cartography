9282e93622aabdf3476f976c744ee0b6e9e47092
Ajinkya Jawalkar <ajinkya.jawalkar@yahoo.com>
2022-10-03 17:09:12 +0530

    Added key vault, keys,secrets and certificates

--- cartography/intel/azure/key_vaults.py
+++ cartography/intel/azure/key_vaults.py
@@ -5,6 +5,9 @@ from typing import List
 import neo4j
 from azure.core.exceptions import HttpResponseError
 from azure.mgmt.keyvault import KeyVaultManagementClient
+from azure.keyvault.secrets import SecretClient
+from azure.keyvault.keys import KeyClient
+from azure.keyvault.certificates.aio import CertificateClient
 from cloudconsolelink.clouds.azure import AzureLinker
 
 from .util.credentials import Credentials
@@ -18,6 +21,29 @@ azure_console_link = AzureLinker()
 def load_key_vaults(session: neo4j.Session, subscription_id: str, data_list: List[Dict], update_tag: int) -> None:
     session.write_transaction(_load_key_vaults_tx, subscription_id, data_list, update_tag)
 
+def load_key_vaults_keys(session: neo4j.Session, subscription_id: str, data_list: List[Dict], update_tag: int) -> None:
+    session.write_transaction(_load_key_vaults_keys_tx, subscription_id, data_list, update_tag)
+
+def load_key_vaults_secrets(session: neo4j.Session, subscription_id: str, data_list: List[Dict], update_tag: int) -> None:
+    session.write_transaction(_load_key_vault_secrets_tx, subscription_id, data_list, update_tag)
+
+def load_key_vaults_certificates(session: neo4j.Session, subscription_id: str, data_list: List[Dict], update_tag: int) -> None:
+    session.write_transaction(_load_key_vault_certificates_tx, subscription_id, data_list, update_tag)
+
+@timeit
+def get_key_vault_keys_client(credentials: Credentials, vault_url: str) -> KeyClient:
+    client = KeyClient(vault_url, credentials.arm_credentials)
+    return client
+
+@timeit
+def get_key_vault_secrets_client(credentials: Credentials, vault_url: str) -> SecretClient:
+    client = SecretClient(vault_url,credentials)
+    return client
+
+@timeit
+def get_key_vault_certificates_client(credentials: Credentials, vault_url: str) -> CertificateClient:
+    client = CertificateClient(vault_url, credentials)
+    return client
 
 @timeit
 def get_key_vaults_client(credentials: Credentials, subscription_id: str) -> KeyVaultManagementClient:
@@ -28,7 +54,7 @@ def get_key_vaults_client(credentials: Credentials, subscription_id: str) -> Key
 @timeit
 def get_key_vaults_list(client: KeyVaultManagementClient, regions: list, common_job_parameters: Dict) -> List[Dict]:
     try:
-        key_vaults_list = list(map(lambda x: x.as_dict(), client.vaults.list()))
+        key_vaults_list = list(map(lambda x: x.as_dict(), client.vaults.list_by_subscription()))
         key_vaults_data = []
         for vault in key_vaults_list:
             x = vault['id'].split('/')
@@ -57,6 +83,7 @@ def _load_key_vaults_tx(
     k.type = vault.type,
     k.location = vault.location,
     k.region = vault.location,
+    k.uri = vault.properties.vault_uri,
     k.consolelink = vault.consolelink,
     k.resourcegroup = vault.resource_group
     SET k.lastupdated = {update_tag},
@@ -75,6 +102,125 @@ def _load_key_vaults_tx(
         update_tag=update_tag,
     )
 
+@timeit
+def get_key_vault_keys_list(client: KeyClient, vault_id: str, regions: list, common_job_parameters: Dict) -> List[Dict]:
+
+    try:
+        keys_list = list(map(lambda x:x.as_dict, client.list_properties_of_keys()))
+        keys_data = []
+        for key in keys_list:
+            key['vault_id'] = vault_id
+            keys_data.append(key)
+        return keys_data
+    except HttpResponseError as e:
+        logger.warning(f"Error while retrieving key vaults keys - {e}")
+        return []
+
+def _load_key_vaults_keys_tx(
+    tx: neo4j.Transaction, subscription_id: str, key_vault_keys: List[Dict], update_tag: int,
+) -> None:
+
+    ingest_keys = """
+    UNWIND {KEYS} as key
+    MERGE (k:AzureKeyVaultKey{id: key.kid})
+    ON CREATE SET k.firstssen = timestamp(),
+    k.kid = key.kid,
+    k.managed = key.managed
+    SET k.lastupdated = {update_tag}
+    WITH k, key
+    MATCH (keyvault:AzureKeyVault{id: key.vault_id})
+    MERGE (keyvault)-[r:HAS_KEY]->(k)
+    ON CREATE SET r.firstseen = timestamp()
+    SET r.lastupdated = {update_tag}
+    """
+
+    tx.run(
+        ingest_keys,
+        KEYS = key_vault_keys,
+        SUBSCRIPTION_ID=subscription_id,
+        update_tag=update_tag,
+    )
+
+@timeit
+def get_key_vault_secrets(client: SecretClient, vault_id: str, regions: list, common_job_parameters: Dict) -> List[Dict]:
+
+    try: 
+        secrets_list = list(map(lambda x:x.as_dict(),client.list_properties_of_secrets()))
+        secrets_data = []
+        for secret in secrets_list:
+            secret['vault_id'] = vault_id
+            secrets_data.append(secret)
+        return secrets_data
+    except HttpResponseError as e:
+            logger.warning(f"Error while retrieving secrets list  - {e}")
+            return []
+
+@timeit
+def _load_key_vault_secrets_tx(
+    tx: neo4j.Transaction, subscription_id: str, key_vault_secrets: List[Dict], update_tag: int,
+) -> None:
+
+    ingest_secrets = """
+    UNWIND {SECRETS} as sec
+    MERGE (s:AzureKeyVaultSecret{id: sec.id})
+    ON CREATE SET s.firstseen = timestamp(),
+    s.id = sec.id,
+    s.contentType = sec.content_type,
+    s.managed = s.managed
+    SET s.lasupdated = {update_tag}
+    WITH s, sec
+    MATCH (keyvault:AzureKeyVault{id: sec.vault_id})
+    MERGE (keyvault)-[r:HAS_SECRET]->(s)
+    ON CREATE SET r.firstseen = timestamp()
+    SET r.lastupdated = {update_tag}
+    """
+
+    tx.run(
+        ingest_secrets,
+        SECRETS = key_vault_secrets,
+        SUBSCRIPTION_ID=subscription_id,
+        update_tag=update_tag,
+    )
+
+@timeit
+def get_key_vault_certificates(client: CertificateClient, vault_id: str, regions: list, common_job_parameters: Dict) -> List[Dict]:
+
+    try:
+        certificates = list(map(lambda x:x.as_dict(), client.list_properties_of_certificates()))
+        certificates_data = []
+        for certificate in certificates:
+            certificate['vault_id'] = vault_id
+        return certificates_data
+    except HttpResponseError as e:
+            logger.warning(f"Error while retrieving certificates list  - {e}")
+            return []
+
+@timeit
+def _load_key_vault_certificates_tx(
+    tx: neo4j.Transaction, subscription_id: str, key_vault_certificates: List[Dict], update_tag: int,
+) -> None:
+
+    ingest_certificates = """
+    UNWIND {CERTS} as cert
+    MERGE (c:AzureKeyVaultCertificate{id: cert.id})
+    ON CREATE SET c.firstseen = timestamp(),
+    c.id = cert.id,
+    c.contentType = cert.content_type,
+    c.keyId = cert.kid,
+    c.secretId = cert.sid
+    SET c.lasupdated = {update_tag}
+    WITH c, cert
+    MATCH (keyvault:AzureKeyVault{id: cert.vault_id})
+    MERGE (keyvault)-[r:HAS_CERTIFICATE]->(c)
+    ON CREATE SET r.firstseen = timestamp()
+    SET r.lastupdated = {update_tag}
+    """
+    tx.run(
+        ingest_certificates,
+        CERTS = key_vault_certificates,
+        SUBSCRIPTION_ID=subscription_id,
+        update_tag=update_tag,
+    )
 
 def cleanup_key_vaults(neo4j_session: neo4j.Session, common_job_parameters: Dict) -> None:
     run_cleanup_job('azure_import_key_vaults_cleanup.json', neo4j_session, common_job_parameters)
@@ -105,6 +251,22 @@ def sync_key_vaults(
             has_next_page = True
             key_vaults_list = key_vaults_list[page_start:page_end]
             common_job_parameters['pagination']['key_vaults']['hasNextPage'] = has_next_page
+
+    for key_vault in key_vaults_list:
+        # KEY VAULT KEYS
+        key_client = get_key_vault_keys_client(credentials, key_vault.get('properties',{}).get('vault_uri',None))
+        keys_list = get_key_vault_keys_list(key_client, key_vault.get('id',None), regions, common_job_parameters)
+        load_key_vaults_keys(neo4j_session, subscription_id, keys_list, update_tag)
+
+        # KEY VAULT SECRETS
+        secrets_client = get_key_vault_secrets_client(credentials, key_vault.get('properties',{}).get('vault_uri',None))
+        secrets_list = get_key_vault_secrets(secrets_client, key_vault.get('id',None), regions, common_job_parameters)
+        load_key_vaults_secrets(neo4j_session, subscription_id, secrets_list, update_tag)
+
+        # KEY VAULT CERTIFICATES
+        certificate_client = get_key_vault_certificates_client(credentials, key_vault.get('properties',{}).get('vault_uri',None))
+        certificates_list = get_key_vault_certificates(certificate_client, key_vault.get('id',None), regions, common_job_parameters)
+        load_key_vaults_certificates(neo4j_session, subscription_id, certificates_list, update_tag)
 
     load_key_vaults(neo4j_session, subscription_id, key_vaults_list, update_tag)
     cleanup_key_vaults(neo4j_session, common_job_parameters)
--- tests/integration/cartography/intel/azure/test_key_vaults.py
+++ tests/integration/cartography/intel/azure/test_key_vaults.py
@@ -1,5 +1,5 @@
 from cartography.intel.azure import key_vaults
-from tests.data.azure.key_vaults import DESCRIBE_KEYVAULTS
+from tests.data.azure.key_vaults import DESCRIBE_KEYVAULTS, DESCRIBE_KEYS, DESCRIBE_SECRETS, DESCRIBE_CERTIFICATES
 
 TEST_SUBSCRIPTION_ID = '00-00-00-00'
 TEST_RESOURCE_GROUP = 'TestRG'
@@ -65,3 +65,153 @@ def test_load_key_vaults_relationships(neo4j_session):
     actual = {(r['n1.id'], r['n2.id']) for r in result}
 
     assert actual == expected
+
+def test_load_key_vault_keys(neo4j_session):
+    key_vaults.load_key_vaults_keys(
+        neo4j_session,
+        TEST_SUBSCRIPTION_ID,
+        DESCRIBE_KEYS,
+        TEST_UPDATE_TAG,
+    )
+
+    expected_nodes = {
+        "https://vault1.vault.azure.net/keys/key1",
+        "https://vault2.vault.azure.net/keys/key2",
+    }
+
+    nodes = neo4j_session.run(
+        """
+        MATCH (r:AzureKeyVaultKey) RETURN r.id;
+        """, )
+    actual_nodes = {n['r.id'] for n in nodes}
+
+    assert actual_nodes == expected_nodes
+
+def test_load_key_vauly_key_relationship(neo4j_session):
+    key_vaults.load_key_vaults(
+        neo4j_session,
+        TEST_SUBSCRIPTION_ID,
+        DESCRIBE_KEYVAULTS,
+        TEST_UPDATE_TAG,
+    )
+
+    key_vaults.load_key_vaults_keys(
+        neo4j_session,
+        TEST_SUBSCRIPTION_ID,
+        DESCRIBE_KEYS,
+        TEST_UPDATE_TAG,
+    )
+
+    expected_nodes ={
+        ("/subscriptions/00-00-00-00/resourceGroups/TestRG/providers/Microsoft.KeyVault/vaults/vault1", "https://vault1.vault.azure.net/keys/key1",),
+        ("/subscriptions/00-00-00-00/resourceGroups/TestRG/providers/Microsoft.KeyVault/vaults/vault2", "https://vault2.vault.azure.net/keys/key2",),
+    }
+
+    result = neo4j_session.run(
+        """
+        MATCH (n1:AzureKeyVault)-[:HAS_KEY]->(n2:AzureKeyVaultKey) RETURN n1.id, n2.id;
+        """, )
+
+    actual_nodes = {(r['n1.id'], r['n2.id']) for r in result}
+
+    assert actual_nodes == expected_nodes
+
+def test_load_key_vault_secrets(neo4j_session):
+    key_vaults.load_key_vaults_secrets(
+        neo4j_session,
+        TEST_SUBSCRIPTION_ID,
+        DESCRIBE_SECRETS,
+        TEST_UPDATE_TAG,
+    )
+
+    expected_nodes = {
+        "https://vault1.vault.azure.net/secrets/secret1/abcdefg",
+        "https://vault1.vault.azure.net/secrets/secret2/hijklm",
+    }
+
+    nodes = neo4j_session.run(
+        """
+        MATCH (r:AzureKeyVaultSecret) RETURN r.id;
+        """, )
+    actual_nodes = {n['r.id'] for n in nodes}
+
+    assert actual_nodes == expected_nodes
+
+def test_key_vault_secrets_relationship(neo4j_session):
+    key_vaults.load_key_vaults(
+        neo4j_session,
+        TEST_SUBSCRIPTION_ID,
+        DESCRIBE_KEYVAULTS,
+        TEST_UPDATE_TAG,
+    )
+
+    key_vaults.load_key_vaults_secrets(
+        neo4j_session,
+        TEST_SUBSCRIPTION_ID,
+        DESCRIBE_SECRETS,
+        TEST_UPDATE_TAG,
+    )
+
+    expected_nodes ={
+        ("/subscriptions/00-00-00-00/resourceGroups/TestRG/providers/Microsoft.KeyVault/vaults/vault1", "https://vault1.vault.azure.net/secrets/secret1/abcdefg",),
+        ("/subscriptions/00-00-00-00/resourceGroups/TestRG/providers/Microsoft.KeyVault/vaults/vault2", "https://vault1.vault.azure.net/secrets/secret2/hijklm",),
+    }
+
+    result = neo4j_session.run(
+        """
+        MATCH (n1:AzureKeyVault)-[:HAS_SECRET]->(n2:AzureKeyVaultSecret) RETURN n1.id, n2.id;
+        """, )
+
+    actual_nodes = {(r['n1.id'], r['n2.id']) for r in result}
+
+    assert actual_nodes == expected_nodes
+
+def test_load_key_vault_certificates(neo4j_session):
+    key_vaults.load_key_vaults_certificates(
+        neo4j_session,
+        TEST_SUBSCRIPTION_ID,
+        DESCRIBE_CERTIFICATES,
+        TEST_UPDATE_TAG,
+    )
+
+    expected_nodes = {
+        "https://vault1.vault.azure.net/certificates/selfSignedCert01/012abc",
+        "https://vault1.vault.azure.net/certificates/selfSignedCert02/123ghj",
+    }
+
+    nodes = neo4j_session.run(
+        """
+        MATCH (r:AzureKeyVaultCertificate) RETURN r.id;
+        """, )
+    actual_nodes = {n['r.id'] for n in nodes}
+
+    assert actual_nodes == expected_nodes
+
+def test_key_vault_certificates_relationship(neo4j_session):
+    key_vaults.load_key_vaults(
+        neo4j_session,
+        TEST_SUBSCRIPTION_ID,
+        DESCRIBE_KEYVAULTS,
+        TEST_UPDATE_TAG,
+    )
+
+    key_vaults.load_key_vaults_certificates(
+        neo4j_session,
+        TEST_SUBSCRIPTION_ID,
+        DESCRIBE_CERTIFICATES,
+        TEST_UPDATE_TAG,
+    )
+
+    expected_nodes ={
+        ("/subscriptions/00-00-00-00/resourceGroups/TestRG/providers/Microsoft.KeyVault/vaults/vault1", "https://vault1.vault.azure.net/certificates/selfSignedCert01/012abc",),
+        ("/subscriptions/00-00-00-00/resourceGroups/TestRG/providers/Microsoft.KeyVault/vaults/vault2", "https://vault1.vault.azure.net/certificates/selfSignedCert02/123ghj",),
+    }
+
+    result = neo4j_session.run(
+        """
+        MATCH (n1:AzureKeyVault)-[:HAS_CERTIFICATE]->(n2:AzureKeyVaultCertificate) RETURN n1.id, n2.id;
+        """, )
+
+    actual_nodes = {(r['n1.id'], r['n2.id']) for r in result}
+
+    assert actual_nodes == expected_nodes
