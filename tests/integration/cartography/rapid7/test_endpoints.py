00f5901164c822767c68a4ebaa01e50dd52d48e6
juju4 <juju4@users.noreply.github.com>
2022-11-05 16:14:33 +0000

    tests/integration: fix filename changed

--- tests/integration/cartography/rapid7/test_endpoints.py
+++ tests/integration/cartography/rapid7/test_endpoints.py
@@ -1,12 +1,12 @@
 import cartography.intel.mde.endpoints
-import tests.data.rapid7.endpoints
+import tests.data.rapid7.rapid7_endpoints
 
 
 TEST_UPDATE_TAG = 123456789
 
 
 def test_load_host_data(neo4j_session, *args):
-    data = tests.data.rapid7.endpoints.GET_HOSTS
+    data = tests.data.rapid7.rapid7_endpoints.GET_HOSTS
     cartography.intel.rapid7.endpoints.load_host_data(neo4j_session, data, TEST_UPDATE_TAG)
 
     expected_nodes = {
