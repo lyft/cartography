7ec98d00979528d81837fdf8a0259c8e719b4c1f
juju4 <juju4@users.noreply.github.com>
2022-11-13 03:51:20 +0000

    fix mypy: another tuple

--- cartography/intel/sumologic/endpoints.py
+++ cartography/intel/sumologic/endpoints.py
@@ -5,6 +5,7 @@ cartography/intel/sumologic/endpoints
 import logging
 from typing import Dict
 from typing import List
+from typing import Tuple
 
 import neo4j
 
@@ -18,7 +19,7 @@ logger = logging.getLogger(__name__)
 def sync_hosts(
     neo4j_session: neo4j.Session,
     update_tag: int,
-    authorization: tuple[str, str, str],
+    authorization: Tuple[str, str, str],
 ) -> None:
     sumologic_hosts_list = sumologic_hosts(authorization)
     for host_data in sumologic_hosts_list:
