5d01215b9a68762119e35569caa317f56b382232
Alex Chantavy <achantavy@lyft.com>
2023-01-02 22:01:23 -0800

    Support selected_rels, test exceptions

--- cartography/graph/job.py
+++ cartography/graph/job.py
@@ -13,6 +13,7 @@ from typing import Union
 import neo4j
 
 from cartography.graph.cleanupbuilder import build_cleanup_queries
+from cartography.models.core.relationships import CartographyRelSchema
 from cartography.graph.statement import get_job_shortname
 from cartography.graph.statement import GraphStatement
 from cartography.models.core.nodes import CartographyNodeSchema
