becee16defd5b9f5d16959bda1c478113e329be3
Alex Chantavy <achantavy@lyft.com>
2023-01-25 15:29:38 -0800

    Fix tests

--- tests/unit/cartography/graph/test_querybuilder_indexcreation.py
+++ tests/unit/cartography/graph/test_querybuilder_indexcreation.py
@@ -5,19 +5,26 @@ from tests.data.graph.querybuilder.sample_models.interesting_asset import Intere
 
 def test_build_create_index_queries():
     result = build_create_index_queries(InterestingAssetSchema())
-    assert result == [
+    assert set(result) == {
         'CREATE INDEX IF NOT EXISTS FOR (n:InterestingAsset) ON (n.id);',
+        'CREATE INDEX IF NOT EXISTS FOR (n:InterestingAsset) ON (n.lastupdated);',
         'CREATE INDEX IF NOT EXISTS FOR (n:AnotherNodeLabel) ON (n.id);',
         'CREATE INDEX IF NOT EXISTS FOR (n:YetAnotherNodeLabel) ON (n.id);',
         'CREATE INDEX IF NOT EXISTS FOR (n:SubResource) ON (n.id);',
         'CREATE INDEX IF NOT EXISTS FOR (n:HelloAsset) ON (n.id);',
         'CREATE INDEX IF NOT EXISTS FOR (n:WorldAsset) ON (n.id);',
-    ]
+    }
 
 
 def test_build_create_index_queries_for_emr():
+    """
+    The EMR sync is our poster child for testing out the Cartography data model. This is a realistic scenario of index
+    creation.
+    """
     result = build_create_index_queries(EMRClusterSchema())
-    assert result == [
+    assert set(result) == {
         'CREATE INDEX IF NOT EXISTS FOR (n:EMRCluster) ON (n.id);',
+        'CREATE INDEX IF NOT EXISTS FOR (n:EMRCluster) ON (n.lastupdated);',
         'CREATE INDEX IF NOT EXISTS FOR (n:AWSAccount) ON (n.id);',
-    ]
+        'CREATE INDEX IF NOT EXISTS FOR (n:EMRCluster) ON (n.arn);'
+    }
