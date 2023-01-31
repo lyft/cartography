3c237df6e882e2bec9bf7832c84aab6ca0f53fa0
Alex Chantavy <achantavy@lyft.com>
2023-01-31 11:45:00 -0800

    Consolidate cleanup node and rel queries

--- cartography/graph/cleanupbuilder.py
+++ cartography/graph/cleanupbuilder.py
@@ -51,51 +51,34 @@ def build_cleanup_queries(
     result = []
     if other_rels:
         for rel in other_rels.rels:
-            result.append(
-                _build_cleanup_node_query(node_schema, rel),
-            )
-            result.append(
-                _build_cleanup_rel_query(node_schema, rel),
-            )
+            result.extend(_build_cleanup_node_and_rel_queries(node_schema, rel))
 
-    # Make sure that the sub resource one is last in the list; order matters.
-    result.append(
-        _build_cleanup_node_query(node_schema),
-    )
-    result.append(
-        _build_cleanup_rel_query(node_schema),
-    )
+    # Make sure that the sub resource cleanup job is last in the list; order matters.
+    result.extend(_build_cleanup_node_and_rel_queries(node_schema, sub_resource_rel))
     # Note that auto-cleanups for a node with no relationships does not happen at all - we don't support it.
     return result
 
 
-def _build_cleanup_query(
+def _build_cleanup_node_and_rel_queries(
         node_schema: CartographyNodeSchema,
-        delete_action_clause: str,
-        selected_relationship: Optional[CartographyRelSchema] = None,
-) -> str:
+        selected_relationship: CartographyRelSchema,
+) -> List[str]:
     """
     Private function that performs the main string template logic for generating cleanup node and relationship queries.
     :param node_schema: The given CartographyNodeSchema to generate cleanup queries for.
-    :param delete_action_clause: A neo4j clause of the form
-        `WHERE $var.lastupdated <> $UPDATE_TAG
-         WITH $var LIMIT $LIMIT_SIZE
-         [DETACH] DELETE $var;`.
-    This clause is used to control whether the returned query deletes nodes, relationships, or the sub resource
-    relationship.
-    :param selected_relationship: If specified, generate a cleanup query for the node_schema and the given
-    selected_relationship. selected_relationship must be in the set {node_schema.sub_resource_relationship} +
-    node_schema.other_relationships. If not specified, we clean up the sub resource relationship.
-    :return: A cleanup query.
+    :param selected_relationship: Determines what relationship on the node_schema to build cleanup queries for.
+    selected_relationship must be in the set {node_schema.sub_resource_relationship} + node_schema.other_relationships.
+    :return: A list of 2 cleanup queries. The first one cleans up stale nodes attached to the given
+    selected_relationships, and the second one cleans up stale selected_relationships. For example outputs, see
+    tests.unit.cartography.graph.autocleanup.test_cleanupbuilder.
     """
-    # Validation
     if not node_schema.sub_resource_relationship:
         raise ValueError(
             f"_build_cleanup_node_query() failed: '{node_schema.label}' does not have a sub_resource_relationship "
             "defined, so we cannot generate a query to clean it up. Please verify that the class definition is what "
             "you expect.",
         )
-    if selected_relationship and not rel_present_on_node_schema(node_schema, selected_relationship):
+    if not rel_present_on_node_schema(node_schema, selected_relationship):
         raise ValueError(
             f"_build_cleanup_node_query(): Attempted to build cleanup query for node '{node_schema.label}' and "
             f"relationship {selected_relationship.rel_label} but that relationship is not present on the node. Please "
@@ -111,19 +94,31 @@ def _build_cleanup_query(
         SubResourceRelLabel=node_schema.sub_resource_relationship.rel_label,
     )
 
-    selected_rel_clause = ""
-    if selected_relationship and selected_relationship != node_schema.sub_resource_relationship:
-        # Draw selected relationship with correct direction
-        if selected_relationship.direction == LinkDirection.INWARD:
-            selected_rel_template = Template("<-[r:$SelectedRelLabel]-")
-        else:
-            selected_rel_template = Template("-[r:$SelectedRelLabel]->")
-        selected_rel = selected_rel_template.safe_substitute(SelectedRelLabel=selected_relationship.rel_label)
-
-        selected_rel_clause_template = Template("""MATCH (n)$selected_rel(:$other_node_label)""")
-        selected_rel_clause = selected_rel_clause_template.safe_substitute(
-            selected_rel=selected_rel,
-            other_node_label=selected_relationship.target_node_label,
+    # The cleanup node query must always be before the cleanup rel query
+    delete_action_clauses = [
+        """
+        WHERE n.lastupdated <> $UPDATE_TAG
+        WITH n LIMIT $LIMIT_SIZE
+        DETACH DELETE n;
+        """,
+    ]
+    # Now clean up the relationships
+    if selected_relationship == node_schema.sub_resource_relationship:
+        _validate_target_node_matcher_for_cleanup_job(node_schema.sub_resource_relationship.target_node_matcher)
+        delete_action_clauses.append(
+            """
+            WHERE s.lastupdated <> $UPDATE_TAG
+            WITH s LIMIT $LIMIT_SIZE
+            DELETE s;
+            """,
+        )
+    else:
+        delete_action_clauses.append(
+            """
+            WHERE r.lastupdated <> $UPDATE_TAG
+            WITH r LIMIT $LIMIT_SIZE
+            DELETE r;
+            """,
         )
 
     # Ensure the node is attached to the sub resource and delete the node
@@ -134,141 +129,38 @@ def _build_cleanup_query(
         $delete_action_clause
         """,
     )
-    return query_template.safe_substitute(
-        node_label=node_schema.label,
-        sub_resource_link=sub_resource_link,
-        sub_resource_label=node_schema.sub_resource_relationship.target_node_label,
-        match_sub_res_clause=_build_match_clause(node_schema.sub_resource_relationship.target_node_matcher),
-        selected_rel_clause=selected_rel_clause,
-        delete_action_clause=delete_action_clause,
-    )
+    return [
+        query_template.safe_substitute(
+            node_label=node_schema.label,
+            sub_resource_link=sub_resource_link,
+            sub_resource_label=node_schema.sub_resource_relationship.target_node_label,
+            match_sub_res_clause=_build_match_clause(node_schema.sub_resource_relationship.target_node_matcher),
+            selected_rel_clause=(
+                "" if selected_relationship == node_schema.sub_resource_relationship
+                else _build_selected_rel_clause(selected_relationship)
+            ),
+            delete_action_clause=delete_action_clause,
+        ) for delete_action_clause in delete_action_clauses
+    ]
 
 
-def _build_cleanup_node_query(
-        node_schema: CartographyNodeSchema,
-        selected_relationship: Optional[CartographyRelSchema] = None,
-) -> str:
+def _build_selected_rel_clause(selected_relationship: CartographyRelSchema) -> str:
     """
-    Generates an optimized and tuned node deletion query for the given node schema. Ensures that the node type to be
-    cleaned up is connected to a sub resource, and (optionally) to another relationship -- this way it is much less
-    likely to delete nodes that you weren't expecting.
-    Example 1: no selected_relationship
-    ```
-    class InterestingAssetSchema(CartographyNodeSchema):
-        label: str = 'InterestingAsset'
-        sub_resource_relationship: InterestingAssetToSubResourceRel = InterestingAssetToSubResourceRel()
-        other_relationships: Optional[OtherRelationships] = OtherRelationships(
-            [
-                InterestingAssetToHelloAssetRel(),
-                InterestingAssetToWorldAssetRel(),
-            ],
-        )
-        # ... other attrs ...
-
-    _build_cleanup_node_query(InterestingAssetSchema())
-    -->
-        MATCH (n:InterestingAsset)<-[s:RELATIONSHIP_LABEL]-(:SubResource{id: $sub_resource_id})
-        WHERE n.lastupdated <> $UPDATE_TAG
-        WITH n LIMIT $LIMIT_SIZE
-        DETACH DELETE n;
-    ```
-    Example 2: specifying a selected rel: in this case we anchor the InterestingAsset to the HelloAsset.
-    _build_cleanup_node_query(InterestingAssetSchema(), InterestingAssetToHelloAssetRel())
-    -->
-        MATCH (n:InterestingAsset)<-[s:RELATIONSHIP_LABEL]-(:SubResource{id: $sub_resource_id})
-        MATCH (n)-[r:ASSOCIATED_WITH]->(:HelloAsset)
-        WHERE n.lastupdated <> $UPDATE_TAG
-        WITH n LIMIT $LIMIT_SIZE
-        DETACH DELETE n;
-
-    :param node_schema: The node_schema to generate a query from.
-    :param selected_relationship: If specified, generate a cleanup query for the node_schema and the given
-    selected_relationship. selected_relationship must be in the set {node_schema.sub_resource_relationship} +
-    node_schema.other_relationships. If not specified, this defaults to None, meaning that we generate a query to clean
-    up stale nodes attached to the sub resource relationship.
-    :return: A Neo4j query to clean up stale nodes of the given type.
+    Draw selected relationship with correct direction. Returns a string that looks like either
+    MATCH (n)<-[r:$SelectedRelLabel]-(:$other_node_label) or
+    MATCH (n)-[r:$SelectedRelLabel]->(:$other_node_label)
     """
-    delete_action_clause = """
-        WHERE n.lastupdated <> $UPDATE_TAG
-        WITH n LIMIT $LIMIT_SIZE
-        DETACH DELETE n;
-    """
-    return _build_cleanup_query(node_schema, delete_action_clause, selected_relationship)
-
-
-def _build_cleanup_rel_query(
-        node_schema: CartographyNodeSchema,
-        selected_relationship: Optional[CartographyRelSchema] = None,
-) -> str:
-    """
-    Generates an optimized and tuned relationship deletion query for the given node schema.
-    Ensures that the node-relationship to be cleaned up is connected to a given sub resource, and (optionally) to
-    another relationship -- this way it is much less likely to delete relationships that you weren't expecting.
-
-    Example 1: no selected relationship
-    ```
-    class InterestingAssetSchema(CartographyNodeSchema):
-        label: str = 'InterestingAsset'
-        sub_resource_relationship: InterestingAssetToSubResourceRel = InterestingAssetToSubResourceRel()
-        other_relationships: Optional[OtherRelationships] = OtherRelationships(
-            [
-                InterestingAssetToHelloAssetRel(),
-                InterestingAssetToWorldAssetRel(),
-            ],
-        )
-        # ... other attrs ...
-
-    _build_cleanup_rel_query(InterestingAssetSchema())
-    -->
-        MATCH (n:InterestingAsset)<-[s:RELATIONSHIP_LABEL]-(:SubResource{id: $sub_resource_id})
-        WHERE s.lastupdated <> $UPDATE_TAG
-        WITH s LIMIT $LIMIT_SIZE
-        DELETE s;
-    ```
-
-    Example 2: with a selected relationship
-    _build_cleanup_rel_query(
-        InterestingAssetSchema(),
-        InterestingAssetToHelloAssetRel(),
-    )
-    -->
-        MATCH (n:InterestingAsset)<-[s:RELATIONSHIP_LABEL]-(:SubResource{id: $sub_resource_id})
-        MATCH (n)-[r:ASSOCIATED_WITH]->(:HelloAsset)
-        WHERE r.lastupdated <> $UPDATE_TAG
-        WITH r LIMIT $LIMIT_SIZE
-        DELETE r;
-
-    :param node_schema: The node_schema to generate a query from.
-    :param selected_relationship: If specified, generate a cleanup query including the node_schema and the given
-    selected_relationship. selected_relationship must be in the set {node_schema.sub_resource_relationship} +
-    node_schema.other_relationships. If not specified, this defaults to None, meaning that we generate a query to clean
-    up stale sub resource relationships.
-    :return: A Neo4j query to clean up stale relationships of the given type in the given node_schema.
-    """
-    if selected_relationship:
-        # Clean up a relationship defined on node_schema.other_relationships
-        delete_action_clause = """
-            WHERE r.lastupdated <> $UPDATE_TAG
-            WITH r LIMIT $LIMIT_SIZE
-            DELETE r;
-        """
+    if selected_relationship.direction == LinkDirection.INWARD:
+        selected_rel_template = Template("<-[r:$SelectedRelLabel]-")
     else:
-        if not node_schema.sub_resource_relationship:
-            raise ValueError(
-                f"_build_cleanup_rel_query() failed: '{node_schema.label}' does not have a sub_resource_relationship "
-                "defined, so we cannot generate a query to clean up rels. Please verify that the class definition is "
-                "what you expect. If this is intended, then please write a manual cleanup job in json as auto cleanup "
-                "jobs without sub resource rels is currently not supported.",
-            )
-        _validate_target_node_matcher_for_cleanup_job(node_schema.sub_resource_relationship.target_node_matcher)
-
-        # Clean up the sub resource relationship
-        delete_action_clause = """
-            WHERE s.lastupdated <> $UPDATE_TAG
-            WITH s LIMIT $LIMIT_SIZE
-            DELETE s;
-        """
-    return _build_cleanup_query(node_schema, delete_action_clause, selected_relationship)
+        selected_rel_template = Template("-[r:$SelectedRelLabel]->")
+    selected_rel = selected_rel_template.safe_substitute(SelectedRelLabel=selected_relationship.rel_label)
+    selected_rel_clause_template = Template("""MATCH (n)$selected_rel(:$other_node_label)""")
+    selected_rel_clause = selected_rel_clause_template.safe_substitute(
+        selected_rel=selected_rel,
+        other_node_label=selected_relationship.target_node_label,
+    )
+    return selected_rel_clause
 
 
 def _validate_target_node_matcher_for_cleanup_job(tgm: TargetNodeMatcher):
--- tests/unit/cartography/graph/test_cleanupbuilder.py
+++ tests/unit/cartography/graph/test_cleanupbuilder.py
@@ -1,120 +1,76 @@
+from typing import List
+
 import pytest
 
-from cartography.graph.cleanupbuilder import _build_cleanup_node_query
-from cartography.graph.cleanupbuilder import _build_cleanup_rel_query
+from cartography.graph.cleanupbuilder import _build_cleanup_node_and_rel_queries
 from cartography.graph.cleanupbuilder import build_cleanup_queries
 from cartography.graph.job import get_parameters
 from cartography.intel.aws.emr import EMRClusterToAWSAccount
 from tests.data.graph.querybuilder.sample_models.interesting_asset import InterestingAssetSchema
 from tests.data.graph.querybuilder.sample_models.interesting_asset import InterestingAssetToHelloAssetRel
 from tests.data.graph.querybuilder.sample_models.interesting_asset import InterestingAssetToSubResourceRel
-from tests.data.graph.querybuilder.sample_models.simple_node import SimpleNodeSchema
-from tests.unit.cartography.graph.helpers import remove_leading_whitespace_and_empty_lines
+from tests.unit.cartography.graph.helpers import clean_query_list
 
 
-def test_cleanup_node_sub_rel_only():
+def test_cleanup_sub_rel():
     """
-    Test that we correctly generate a query to delete stale nodes that are attached to a sub resource.
+    Test that we correctly generate cleanup queries when a selected rel is not specified.
     """
-    actual_query: str = _build_cleanup_node_query(InterestingAssetSchema())
-    expected_query = """
+    actual_queries: List[str] = _build_cleanup_node_and_rel_queries(
+        InterestingAssetSchema(),
+        InterestingAssetToSubResourceRel(),
+    )
+    expected_queries = [
+        """
         MATCH (n:InterestingAsset)<-[s:RELATIONSHIP_LABEL]-(:SubResource{id: $sub_resource_id})
         WHERE n.lastupdated <> $UPDATE_TAG
         WITH n LIMIT $LIMIT_SIZE
         DETACH DELETE n;
-    """
-    assert remove_leading_whitespace_and_empty_lines(actual_query) == \
-        remove_leading_whitespace_and_empty_lines(expected_query)
+        """,
+        """
+        MATCH (n:InterestingAsset)<-[s:RELATIONSHIP_LABEL]-(:SubResource{id: $sub_resource_id})
+        WHERE s.lastupdated <> $UPDATE_TAG
+        WITH s LIMIT $LIMIT_SIZE
+        DELETE s;
+        """,
+    ]
+    assert clean_query_list(actual_queries) == clean_query_list(expected_queries)
 
 
-def test_cleanup_node_sub_rel_only_no_sub_res_raises_exc():
+def test_cleanup_with_selected_rel():
     """
-    Test that we raise a ValueError when building cleanup query for node_schema without a sub resource relationship.
+    Test that we correctly generate cleanup queries when a selected rel is specified.
     """
-    with pytest.raises(ValueError):
-        _build_cleanup_node_query(SimpleNodeSchema())
-
-
-def test_cleanup_node_with_selected_rel():
-    """
-    Test that we correctly generate a query to delete stale nodes that are attached to a sub resource and to one other
-    node type.
-    """
-    actual_query: str = _build_cleanup_node_query(InterestingAssetSchema(), InterestingAssetToHelloAssetRel())
-    expected = """
+    actual_queries: List[str] = _build_cleanup_node_and_rel_queries(
+        InterestingAssetSchema(),
+        InterestingAssetToHelloAssetRel(),
+    )
+    expected_queries = [
+        """
         MATCH (n:InterestingAsset)<-[s:RELATIONSHIP_LABEL]-(:SubResource{id: $sub_resource_id})
         MATCH (n)-[r:ASSOCIATED_WITH]->(:HelloAsset)
         WHERE n.lastupdated <> $UPDATE_TAG
         WITH n LIMIT $LIMIT_SIZE
         DETACH DELETE n;
-    """
-    assert remove_leading_whitespace_and_empty_lines(actual_query) == \
-        remove_leading_whitespace_and_empty_lines(expected)
-
-
-def test_cleanup_node_with_invalid_selected_rel_raises_exc():
-    """
-    Test that we raise a ValueError if we try to cleanup a node and provide a specified rel but the rel doesn't exist on
-    the node schema.
-    """
-    with pytest.raises(ValueError):
-        _build_cleanup_node_query(InterestingAssetSchema(), EMRClusterToAWSAccount())
-
-
-def test_cleanup_rel_sub_rel_only():
-    """
-    Test that we correctly generate a query to delete stale relationships between a target node (HelloAsset in this
-    case) its sub resource (SubResource in this case).
-    """
-    actual_query: str = _build_cleanup_rel_query(InterestingAssetSchema())
-    expected_query = """
-        MATCH (n:InterestingAsset)<-[s:RELATIONSHIP_LABEL]-(:SubResource{id: $sub_resource_id})
-        WHERE s.lastupdated <> $UPDATE_TAG
-        WITH s LIMIT $LIMIT_SIZE
-        DELETE s;
-    """
-    assert remove_leading_whitespace_and_empty_lines(actual_query) == \
-        remove_leading_whitespace_and_empty_lines(expected_query)
-
-
-def test_cleanup_rel_sub_rel_only_no_sub_resource_raises_exc():
-    """
-    Test that _build_cleanup_rel_query() raises a ValueError when trying to generate a query for a node_schema without a
-    sub resource relationship.
-    """
-    with pytest.raises(ValueError):
-        _build_cleanup_rel_query(SimpleNodeSchema())
-
-
-def test_cleanup_rel_with_selected_rel():
-    """
-    Test that we correctly generate a query to delete stale relationships between 2 target nodes (InterestingAsset and
-    HelloAsset in this test case) where the main target node is bound to a sub resource.
-    """
-    actual_query: str = _build_cleanup_rel_query(
-        InterestingAssetSchema(),
-        InterestingAssetToHelloAssetRel(),
-    )
-    expected_query = """
+        """,
+        """
         MATCH (n:InterestingAsset)<-[s:RELATIONSHIP_LABEL]-(:SubResource{id: $sub_resource_id})
         MATCH (n)-[r:ASSOCIATED_WITH]->(:HelloAsset)
         WHERE r.lastupdated <> $UPDATE_TAG
         WITH r LIMIT $LIMIT_SIZE
         DELETE r;
-    """
-    assert remove_leading_whitespace_and_empty_lines(actual_query) == \
-        remove_leading_whitespace_and_empty_lines(expected_query)
+        """,
+    ]
+    assert clean_query_list(actual_queries) == clean_query_list(expected_queries)
 
 
-def test_cleanup_rel_with_selected_rel_that_does_not_exist_raises_exc():
+def test_cleanup_with_invalid_selected_rel_raises_exc():
     """
-    Test we raise a ValueError when a selected rel is specified that does not exist to _build_cleanup_rel_query.
+    Test that we raise a ValueError if we try to cleanup a node and provide a specified rel but the rel doesn't exist on
+    the node schema.
     """
     with pytest.raises(ValueError):
-        _build_cleanup_rel_query(
-            InterestingAssetSchema(),
-            EMRClusterToAWSAccount(),
-        )
+        _build_cleanup_node_and_rel_queries(InterestingAssetSchema(), EMRClusterToAWSAccount())
 
 
 def test_build_cleanup_queries():
@@ -163,9 +119,7 @@ def test_build_cleanup_queries():
         WITH s LIMIT $LIMIT_SIZE
         DELETE s;""",
     ]
-    assert [remove_leading_whitespace_and_empty_lines(a) for a in actual_queries] == [
-        remove_leading_whitespace_and_empty_lines(e) for e in expected_queries
-    ]
+    assert clean_query_list(actual_queries) == clean_query_list(expected_queries)
 
 
 def test_get_params_from_queries():
@@ -189,7 +143,7 @@ def test_get_build_cleanup_queries_selected_rels():
 
 def test_get_build_cleanup_queries_selected_rels_no_sub_res_raises_exc():
     """
-    Test that not specifying the sub resource rel raises an exception
+    Test that not specifying the sub resource rel as a selected_relationship in build_cleanup_queries raises exception
     """
     with pytest.raises(ValueError):
         build_cleanup_queries(
