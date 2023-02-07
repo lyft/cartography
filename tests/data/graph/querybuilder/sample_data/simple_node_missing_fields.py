0840a9bf63ed9a399503c7138e91c0cb3d8abcaa
Alex Chantavy <achantavy@lyft.com>
2023-02-07 09:12:32 -0800

    #1106: Add test to ensure we can load data with subset of node schema fields

---
+++ tests/data/graph/querybuilder/sample_data/simple_node_missing_fields.py
@@ -0,0 +1,18 @@
+SIMPLE_NODE_MISSING_PROPS = [
+    {
+        'Id': 'SimpleNode1',
+        'property1': 'The',
+    },
+    {
+        'Id': 'SimpleNode2',
+        'property2': 'Quick',
+    },
+    {
+        'Id': 'SimpleNode3',
+        'property1': 'Brown',
+    },
+    {
+        'Id': 'SimpleNode4',
+        'property2': 'Fox',
+    },
+]
