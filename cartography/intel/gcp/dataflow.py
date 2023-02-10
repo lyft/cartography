71dc09aa779fb4494bcc219d5ee524ccc6e69a2d
Gaurav Chawda <grchawda16@gmail.com>
2023-02-10 20:06:48 +0530

    added few more properties

--- cartography/intel/gcp/dataflow.py
+++ cartography/intel/gcp/dataflow.py
@@ -45,7 +45,7 @@ def get_dataflow_jobs(dataflow: Resource, project_id: str, regions: list, common
 def transform_jobs(jobs: List[Dict], project_id: str) -> List[Dict]:
     transformed_jobs = []
     for job in jobs:
-        job['consolelink'] = ''  # gcp_console_link.get_console_link(project_id=project_id, topic_id=topic['topic_name'], resource_name='cloud_pubsub_topic')
+        job['consolelink'] = ''  # TODO
         transformed_jobs.append(job)
     return transformed_jobs
 
@@ -69,7 +69,6 @@ def load_dataflow_jobs_tx(
     SET
         job.lastupdated = $gcp_update_tag,
         job.region = record.location,
-        job.id = record.id,
         job.name = record.name,
         job.replace_job_id = record.replaceJobId,
         job.type = record.type,
@@ -77,11 +76,13 @@ def load_dataflow_jobs_tx(
         job.dataset = record.environment.dataset,
         job.service_account_email = record.environment.serviceAccountEmail,
         job.service_kms_key_name = record.environment.serviceKmsKeyName,
-        job.shuffle_mode = record.en
+        job.shuffle_mode = record.environment.shuffleMode,
+        job.worker_region = record.environment.workerRegion,
+        job.worker_zone = record.environment.workerZone,
+        job.start_time = record.startTime,
         job.current_state = record.currentState,
         job.requested_state = record.requestedState,
         job.consolelink = record.consolelink,
-        
         topic.satisfies_pzs = record.satisfiesPzs,
     WITH topic
     MATCH (owner:GCPProject{id: $ProjectId})
@@ -99,52 +100,37 @@ def load_dataflow_jobs_tx(
 
 
 @timeit
-def cleanup_pubsub_topics(neo4j_session: neo4j.Session, common_job_parameters: Dict) -> None:
-    run_cleanup_job('gcp_pubsub_topics_cleanup.json', neo4j_session, common_job_parameters)
+def transform_job_worker_pools(jobs: List[Dict], project_id: str):
+    transformed_worker_pools = []
+    for job in jobs:
+        worker_pools = job.get('environment', {}).get('workerPools', [])
+        for worker_pool in worker_pools:
+            worker_pool['job'] = job['id']
+
+
+@timeit
+def cleanup_dataflow_jobs(neo4j_session: neo4j.Session, common_job_parameters: Dict) -> None:
+    run_cleanup_job('gcp_dataflow_jobs_cleanup.json', neo4j_session, common_job_parameters)
 
 
 @timeit
 def sync(
-    neo4j_session: neo4j.Session, pubsub: Resource, project_id: str, gcp_update_tag: int,
+    neo4j_session: neo4j.Session, dataflow: Resource, project_id: str, gcp_update_tag: int,
     common_job_parameters: Dict, regions: List,
 ) -> None:
 
     tic = time.perf_counter()
-    logger.info("Syncing Pubsub for project '%s', at %s.", project_id, tic)
+    logger.info("Syncing Dataflow for project '%s', at %s.", project_id, tic)
 
-    tops = get_pubsub_topics(pubsub, project_id, regions, common_job_parameters)
-    topics = transform_topics(tops, project_id)
-    load_pubsub_topics(neo4j_session, topics, project_id, gcp_update_tag)
+    jobs = get_dataflow_jobs(dataflow, project_id, regions, common_job_parameters)
+    transformed_jobs = transform_jobs(jobs, project_id)
+    load_dataflow_jobs(neo4j_session, transformed_jobs, project_id, gcp_update_tag)
 
     label.sync_labels(
-        neo4j_session, topics, gcp_update_tag,
-        common_job_parameters, 'pubsub_topics', 'GCPPubsubTopic',
+        neo4j_session, transformed_jobs, gcp_update_tag,
+        common_job_parameters, 'dataflow_jobs', 'GCPDataFlowJob',
     )
-
-    if common_job_parameters.get('pagination', {}).get('pubsub', None):
-        if not common_job_parameters.get('pagination', {}).get('pubsub', {}).get('hasNextPage', False):
-            subs = get_pubsub_subscriptions(pubsub, project_id, regions, common_job_parameters)
-            subscriptions = transform_subscriptions(subs, project_id)
-            load_pubsub_subscriptions(neo4j_session, subscriptions, project_id, gcp_update_tag)
-
-            cleanup_pubsub_subscriptions(neo4j_session, common_job_parameters)
-            label.sync_labels(
-                neo4j_session, subscriptions, gcp_update_tag,
-                common_job_parameters, 'pubsub_subscription', 'GCPPubsubSubscription',
-            )
-
-    else:
-        subs = get_pubsub_subscriptions(pubsub, project_id, regions, common_job_parameters)
-        subscriptions = transform_subscriptions(subs, project_id)
-        load_pubsub_subscriptions(neo4j_session, subscriptions, project_id, gcp_update_tag)
-
-        cleanup_pubsub_subscriptions(neo4j_session, common_job_parameters)
-        label.sync_labels(
-            neo4j_session, subscriptions, gcp_update_tag,
-            common_job_parameters, 'pubsub_subscription', 'GCPPubsubSubscription',
-        )
-
-    cleanup_pubsub_topics(neo4j_session, common_job_parameters)
+    cleanup_dataflow_jobs(neo4j_session, common_job_parameters)
 
     toc = time.perf_counter()
-    logger.info(f"Time to process Pubsub: {toc - tic:0.4f} seconds")
+    logger.info(f"Time to process dataflow: {toc - tic:0.4f} seconds")
