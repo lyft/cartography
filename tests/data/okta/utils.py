3f9aa194456a10b0d4e337f8299225b39728086b
Taya Steere <taya.steere@grammarly.com>
2023-03-01 15:13:45 -0800

    Updated based on feedback Added maximum time limit check to avoid long sleep Updated unit test with new check Mocked sleep in unit test

--- tests/data/okta/utils.py
+++ tests/data/okta/utils.py
@@ -7,7 +7,7 @@ def create_response():
     response = Response()
     response.headers['x-rate-limit-limit'] = '300'
     response.headers['x-rate-limit-remaining'] = '299'
-    response.headers['x-rate-limit-reset'] = str(int(time.time()) + 60)
+    response.headers['x-rate-limit-reset'] = str(int(time.time()) + 59)
     return response
 
 
@@ -16,4 +16,12 @@ def create_throttled_response():
     response.headers['x-rate-limit-limit'] = '300'
     response.headers['x-rate-limit-remaining'] = '3'
     response.headers['x-rate-limit-reset'] = str(int(time.time()) + 3)
+    return response
+
+
+def create_long_timeout_response():
+    response = Response()
+    response.headers['x-rate-limit-limit'] = '300'
+    response.headers['x-rate-limit-remaining'] = '3'
+    response.headers['x-rate-limit-reset'] = str(int(time.time()) + 120)
     return response
