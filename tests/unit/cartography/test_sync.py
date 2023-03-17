f948aa2936c05143c3eba19175191965cbfd72f0
Alex Chantavy <achantavy@lyft.com>
2023-03-17 00:06:03 -0700

    Remove todo

--- tests/unit/cartography/test_sync.py
+++ tests/unit/cartography/test_sync.py
@@ -23,18 +23,6 @@ def test_build_sync():
     assert [name for name in sync._stages.keys()] == selected_modules.split(', ')
 
 
-# TODO - this test enforces that we put analysis at the end. idk if we want to own this logic though.
-# def test_build_sync_out_of_order_args():
-#     # Arrange
-#     selected_modules = 'analysis,aws,gcp'
-#
-#     # Act
-#     sync = build_sync(selected_modules)
-#
-#     # Assert
-#     assert [name for name in sync._stages.keys()] == ['aws', 'gcp', 'analysis']
-
-
 def test_parse_and_validate_selected_modules():
     no_spaces = "aws,gcp,oci,analysis"
     assert parse_and_validate_selected_modules(no_spaces) == ['aws', 'gcp', 'oci', 'analysis']
