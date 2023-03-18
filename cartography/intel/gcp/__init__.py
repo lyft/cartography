08f3c6af53f6850834309e06b3b9b38367c4c9d9
DESKTOP-VRE7QCS\User <aryan.dande@gmail.com>
2023-03-18 17:37:15 +0530

    gcp kms keyring analysis added

--- cartography/intel/gcp/__init__.py
+++ cartography/intel/gcp/__init__.py
@@ -516,6 +516,11 @@ def _sync_multiple_projects(
                 neo4j_session,
                 common_job_parameters,
             )
+            run_analysis_job(
+                'gcp_kms_keyring_analysis.json',
+                neo4j_session,
+                common_job_parameters,
+            )
 
     del common_job_parameters["GCP_PROJECT_ID"]
 
