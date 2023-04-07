4f2565e36eb18c4f32a1151fd2811d6ecbd94662
Phi Xu <phixu@lyft.com>
2023-04-07 16:10:18 -0700

    fix test and use new data model

--- tests/integration/cartography/intel/aws/test_dynamodb.py
+++ tests/integration/cartography/intel/aws/test_dynamodb.py
@@ -1,33 +1,78 @@
+from unittest.mock import MagicMock
+from unittest.mock import patch
+
 import cartography.intel.aws.dynamodb
 import tests.data.aws.dynamodb
+from tests.integration.util import check_nodes
+from tests.integration.util import check_rels
 
 TEST_ACCOUNT_ID = '000000000000'
 TEST_REGION = 'us-east-1'
 TEST_UPDATE_TAG = 123456789
 
 
-def test_load_dynamodb(neo4j_session):
-    data = tests.data.aws.dynamodb.LIST_DYNAMODB_TABLES['Tables']
+@patch.object(cartography.intel.aws.dynamodb, 'get_dynamodb_tables', return_value=tests.data.aws.dynamodb.LIST_DYNAMODB_TABLES['Tables'])
+def test_load_dynamodb(mock_get_instances, neo4j_session):
+    """
+    Ensure that instances actually get loaded and have their key fields
+    """
+    # Arrange
+    boto3_session = MagicMock()
 
-    cartography.intel.aws.dynamodb.load_dynamodb_tables(
+    # Act
+    cartography.intel.aws.dynamodb.sync_dynamodb_tables(
         neo4j_session,
-        data,
+        boto3_session,
         TEST_REGION,
         TEST_ACCOUNT_ID,
         TEST_UPDATE_TAG,
+        {'UPDATE_TAG': TEST_UPDATE_TAG, 'AWS_ID': TEST_ACCOUNT_ID},
     )
-    expected_rows = 1000000
-    expected_nodes = {
-        ("arn:aws:dynamodb:us-east-1:000000000000:table/example-table", expected_rows),
-        ("arn:aws:dynamodb:us-east-1:000000000000:table/sample-table", expected_rows),
-        ("arn:aws:dynamodb:us-east-1:000000000000:table/model-table", expected_rows),
-        ("arn:aws:dynamodb:us-east-1:000000000000:table/basic-table", expected_rows),
+
+    # Assert ddb table nodes exist
+    assert check_nodes(neo4j_session, 'DynamoDBTable', ['id', 'rows']) == {
+        ("arn:aws:dynamodb:us-east-1:000000000000:table/example-table", 1000000),
+        ("arn:aws:dynamodb:us-east-1:000000000000:table/sample-table", 1000000),
+        ("arn:aws:dynamodb:us-east-1:000000000000:table/model-table", 1000000),
+        ("arn:aws:dynamodb:us-east-1:000000000000:table/basic-table", 1000000),
     }
 
-    nodes = neo4j_session.run(
-        """
-        MATCH (d:DynamoDBTable) return d.arn, d.rows
-        """,
-    )
-    actual_nodes = {(n['d.arn'], n['d.rows']) for n in nodes}
-    assert actual_nodes == expected_nodes
+    # Assert ddb gsi nodes exist
+    assert check_nodes(neo4j_session, 'DynamoDBGlobalSecondaryIndex', ['id']) == {
+        ('arn:aws:dynamodb:us-east-1:table/example-table/index/sample_2-index',),
+        ('arn:aws:dynamodb:us-east-1:table/model-table/index/sample_2-index',),
+        ('arn:aws:dynamodb:us-east-1:table/model-table/index/sample_3-index',),
+        ('arn:aws:dynamodb:us-east-1:table/model-table/index/sample_1-index',),
+        ('arn:aws:dynamodb:us-east-1:table/example-table/index/sample_1-index',),
+        ('arn:aws:dynamodb:us-east-1:table/sample-table/index/sample_2-index',),
+        ('arn:aws:dynamodb:us-east-1:table/sample-table/index/sample_1-index',),
+        ('arn:aws:dynamodb:us-east-1:table/sample-table/index/sample_3-index',),
+    }
+
+    # Assert DynamoDBTable -> DynamoDBGlobalSecondaryIndex
+    assert check_rels(
+        neo4j_session,
+        'DynamoDBTable',
+        'id',
+        'DynamoDBGlobalSecondaryIndex',
+        'id',
+        'GLOBAL_SECONDARY_INDEX',
+        rel_direction_right=True,
+    ) == {
+        ('arn:aws:dynamodb:us-east-1:000000000000:table/example-table',
+        'arn:aws:dynamodb:us-east-1:table/example-table/index/sample_1-index'),
+        ('arn:aws:dynamodb:us-east-1:000000000000:table/example-table',
+        'arn:aws:dynamodb:us-east-1:table/example-table/index/sample_2-index'),
+        ('arn:aws:dynamodb:us-east-1:000000000000:table/model-table',
+        'arn:aws:dynamodb:us-east-1:table/model-table/index/sample_1-index'),
+        ('arn:aws:dynamodb:us-east-1:000000000000:table/model-table',
+        'arn:aws:dynamodb:us-east-1:table/model-table/index/sample_2-index'),
+        ('arn:aws:dynamodb:us-east-1:000000000000:table/model-table',
+        'arn:aws:dynamodb:us-east-1:table/model-table/index/sample_3-index'),
+        ('arn:aws:dynamodb:us-east-1:000000000000:table/sample-table',
+        'arn:aws:dynamodb:us-east-1:table/sample-table/index/sample_1-index'),
+        ('arn:aws:dynamodb:us-east-1:000000000000:table/sample-table',
+        'arn:aws:dynamodb:us-east-1:table/sample-table/index/sample_2-index'),
+        ('arn:aws:dynamodb:us-east-1:000000000000:table/sample-table',
+        'arn:aws:dynamodb:us-east-1:table/sample-table/index/sample_3-index'),
+    }
