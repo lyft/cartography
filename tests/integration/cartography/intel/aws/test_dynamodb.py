2021feedcd8e91e001bcc0c35c56430bc12384c8
Phi Xu <phixu@lyft.com>
2023-04-13 13:28:03 -0700

    fix ddb int tests

--- tests/integration/cartography/intel/aws/test_dynamodb.py
+++ tests/integration/cartography/intel/aws/test_dynamodb.py
@@ -3,6 +3,7 @@ from unittest.mock import patch
 
 import cartography.intel.aws.dynamodb
 import tests.data.aws.dynamodb
+from tests.integration.cartography.intel.aws.common import create_test_account
 from tests.integration.util import check_nodes
 from tests.integration.util import check_rels
 
@@ -21,6 +22,7 @@ def test_load_dynamodb(mock_get_instances, neo4j_session):
     """
     # Arrange
     boto3_session = MagicMock()
+    create_test_account(neo4j_session, TEST_ACCOUNT_ID, TEST_UPDATE_TAG)
 
     # Act
     cartography.intel.aws.dynamodb.sync_dynamodb_tables(
@@ -53,10 +55,10 @@ def test_load_dynamodb(mock_get_instances, neo4j_session):
     }
 
 
-    # Assert AWSAccount -> DynamoDBTABLE
+    # Assert AWSAccount -> DynamoDBTable
     assert check_rels(
         neo4j_session,
-        'NetworkInterface',
+        'DynamoDBTable',
         'id',
         'AWSAccount',
         'id',
@@ -155,4 +157,38 @@ def test_load_dynamodb(mock_get_instances, neo4j_session):
             'arn:aws:dynamodb:us-east-1:000000000000:table/sample-table',
             'arn:aws:dynamodb:us-east-1:table/sample-table/index/sample_3-index',
         ),
+    }
+
+    # Arrange: load in an unrelated EC2 instance. This should not be affected by the EMR module's cleanup job.
+    neo4j_session.run(
+        '''
+        MERGE (i:EC2Instance{id:1234, lastupdated: $lastupdated})<-[r:RESOURCE]-(:AWSAccount{id: $aws_account_id})
+        SET r.lastupdated = $lastupdated
+        ''',
+        aws_account_id=TEST_ACCOUNT_ID,
+        lastupdated=TEST_UPDATE_TAG,
+    )
+
+    # [Pre-test] Assert that the unrelated EC2 instance exists
+    assert check_rels(neo4j_session, 'AWSAccount', 'id', 'EC2Instance', 'id', 'RESOURCE') == {
+        (TEST_ACCOUNT_ID, 1234),
+    }
+
+    # Act: run the cleanup job
+    common_job_parameters = {
+        'UPDATE_TAG': TEST_UPDATE_TAG + 1,  # Simulate a new sync run finished so the old update tag is obsolete now
+        'AWS_ID': TEST_ACCOUNT_ID,
+        # Add in extra params that may have been added by other modules.
+        # Expectation: These should not affect cleanup job execution.
+        'permission_relationships_file': '/path/to/perm/rels/file',
+        'OKTA_ORG_ID': 'my-org-id',
+    }
+    cartography.intel.aws.dynamodb.cleanup_dynamodb_tables(neo4j_session, common_job_parameters)
+
+    # Assert: Expect no ddb nodes in the graph now
+    assert check_nodes(neo4j_session, 'DynamoDBTable', ['id']) == set()
+    assert check_nodes(neo4j_session, 'DynamoDBGlobalSecondaryIndex', ['id']) == set()
+    # Assert: Expect that the unrelated EC2 instance was not touched by the cleanup job
+    assert check_rels(neo4j_session, 'AWSAccount', 'id', 'EC2Instance', 'id', 'RESOURCE') == {
+        (TEST_ACCOUNT_ID, 1234),
     }
