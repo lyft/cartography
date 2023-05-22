763b714a47528b48808b025d641dcd8e11e9eb67
RohitJ1995 <59526662+RohitJ1995@users.noreply.github.com>
2023-05-22 23:55:04 +0530

    - review comments

--- cartography/intel/aws/resourcegroupstaggingapi.py
+++ cartography/intel/aws/resourcegroupstaggingapi.py
@@ -225,11 +225,11 @@ def compute_resource_id(tag_mapping: Dict, resource_type: str) -> str:
 def cleanup(neo4j_session: neo4j.Session, common_job_parameters: Dict) -> None:
     cleanup_scripts = ['aws_import_tags_cleanup.json']
     if common_job_parameters['aws_resource_type'] == 's3':
-        cleanup_scripts.append('aws_import_tags_s3_cleanup.json')
+        cleanup_scripts.append('aws_import_tags_cleanup_s3.json')
     if common_job_parameters['aws_resource_type'] == 'rds':
         cleanup_scripts.append('aws_import_tags_cleanup_rds.json')
     if not common_job_parameters['aws_resource_type']:
-        cleanup_scripts = cleanup_scripts + ['aws_import_tags_cleanup_rds.json', 'aws_import_tags_cleanup_aws_vpc.json', 'aws_import_tags_cleanup_ec2_security_group.json', 'aws_import_tags_cleanup_ec2_subnet.json', 'aws_import_tags_cleanup_es_domain.json', 'aws_import_tags_cleanup_network_interface.json', 'aws_import_tags_cleanup_redshift.json', 'aws_import_tags_ec2_cleanup.json', 'aws_import_tags_s3_cleanup.json']
+        cleanup_scripts = cleanup_scripts + ['aws_import_tags_cleanup_rds.json', 'aws_import_tags_cleanup_aws_vpc.json', 'aws_import_tags_cleanup_ec2_security_group.json', 'aws_import_tags_cleanup_ec2_subnet.json', 'aws_import_tags_cleanup_es_domain.json', 'aws_import_tags_cleanup_network_interface.json', 'aws_import_tags_cleanup_redshift.json', 'aws_import_tags_cleanup_ec2.json', 'aws_import_tags_cleanup_s3.json']
     for cleanup_file in cleanup_scripts:
         print('running cleanup_file', cleanup_file)
         run_cleanup_job(cleanup_file, neo4j_session, common_job_parameters)
