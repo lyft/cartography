020e6f54ed36ef97fe1505f8aab4ef1745abb38f
Dean Liu <dliu@lyft.com>
2023-07-17 10:55:17 -0700

    1 liner

--- cartography/driftdetect/get_states.py
+++ cartography/driftdetect/get_states.py
@@ -145,8 +145,7 @@ def get_state(session: neo4j.Session, state: State) -> None:
     logger.debug(f"Updating results for {state.name}")
 
     # The keys will be the same across all items in the returned list
-    if len(new_results) > 0:
-        state.properties = list(new_results[0].keys())
+    state.properties = list(new_results[0].keys()) if len(new_results) > 0 else []
 
     results = []
     for record in new_results:
