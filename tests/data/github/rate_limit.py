ad86889bbd711a563a2450c49c0921e27c88572c
Ramon Petgrave <32398091+ramonpetgrave64@users.noreply.github.com>
2023-10-10 23:20:20 -0400

    lint

--- tests/data/github/rate_limit.py
+++ tests/data/github/rate_limit.py
@@ -5,35 +5,35 @@ RATE_LIMIT_RESPONSE_JSON = {
             "remaining": 60,
             "reset": 1696988282,
             "used": 0,
-            "resource": "core"
+            "resource": "core",
         },
         "graphql": {
             "limit": 0,
             "remaining": 0,
             "reset": 1696987997,
             "used": 0,
-            "resource": "graphql"
+            "resource": "graphql",
         },
         "integration_manifest": {
             "limit": 5000,
             "remaining": 5000,
             "reset": 1696988282,
             "used": 0,
-            "resource": "integration_manifest"
+            "resource": "integration_manifest",
         },
         "search": {
             "limit": 10,
             "remaining": 10,
             "reset": 1696984742,
             "used": 0,
-            "resource": "search"
-        }
+            "resource": "search",
+        },
     },
     "rate": {
         "limit": 60,
         "remaining": 60,
         "reset": 1696988282,
         "used": 0,
-        "resource": "core"
-    }
+        "resource": "core",
+    },
 }
