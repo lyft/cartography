c2560ec3ed333025e60fc4c8f42faee663c2e8a0
sagar navale <sagar@cloudanix.com>
2023-11-02 16:05:33 +0530

    merge with aws lambda function

--- cartography/intel/aws/lambda_function.py
+++ cartography/intel/aws/lambda_function.py
@@ -1,18 +1,25 @@
+import time
 import logging
 from typing import Any
 from typing import Dict
+from typing import Generator
 from typing import List
 from typing import Tuple
 
 import boto3
 import botocore
 import neo4j
+from cloudconsolelink.clouds.aws import AWSLinker
 
+from botocore.exceptions import ClientError
 from cartography.util import aws_handle_regions
 from cartography.util import run_cleanup_job
 from cartography.util import timeit
+from policyuniverse.policy import Policy
+import json
 
 logger = logging.getLogger(__name__)
+aws_console_link = AWSLinker()
 
 
 @timeit
@@ -26,59 +33,93 @@ def get_lambda_data(boto3_session: boto3.session.Session, region: str) -> List[D
     lambda_functions = []
     for page in paginator.paginate():
         for each_function in page['Functions']:
+            each_function['region'] = region
+            each_function['consolelink'] = aws_console_link.get_console_link(arn=each_function['FunctionArn'])
             lambda_functions.append(each_function)
+
+    return lambda_functions
+
+@timeit
+@aws_handle_regions
+def get_lambda_policies(boto3_session: boto3.session.Session, region: str, lambda_functions: List[Dict]) -> List[Dict]:
+    """
+    Fetch policies for lambdas
+    """
+    client = boto3_session.client('lambda', region_name=region)
+    for lambda_function in lambda_functions:
+        try:
+            policy = client.get_policy(FunctionName=lambda_function['FunctionArn'])
+            if policy is not None:
+                parsed_policy = Policy(json.loads(policy['Policy']))
+                lambda_function['anonymous_access'] = parsed_policy.is_internet_accessible()
+                lambda_function['anonymous_actions'] = list(parsed_policy.internet_accessible_actions())
+
+        except ClientError as e:
+            if e.response['Error']['Code'] in ("ResourceNotFoundException"):
+                logger.debug(f"unable to fetch function policy: {region} - {lambda_function['FunctionArn']} - {e}")
+                continue
+
+            else:
+                logger.debug(f"unable to fetch function policy: {region} - {lambda_function['FunctionArn']} - {e}")
+                continue
+
     return lambda_functions
 
 
 @timeit
 def load_lambda_functions(
-        neo4j_session: neo4j.Session, data: List[Dict], region: str, current_aws_account_id: str, aws_update_tag: int,
+        neo4j_session: neo4j.Session, data: List[Dict], current_aws_account_id: str, aws_update_tag: int,
 ) -> None:
     ingest_lambda_functions = """
     UNWIND $lambda_functions_list AS lf
-        MERGE (lambda:AWSLambda{id: lf.FunctionArn})
-        ON CREATE SET lambda.firstseen = timestamp()
-        SET lambda.name = lf.FunctionName,
-        lambda.modifieddate = lf.LastModified,
-        lambda.runtime = lf.Runtime,
-        lambda.description = lf.Description,
-        lambda.timeout = lf.Timeout,
-        lambda.memory = lf.MemorySize,
-        lambda.codesize = lf.CodeSize,
-        lambda.handler = lf.Handler,
-        lambda.version = lf.Version,
-        lambda.tracingconfigmode = lf.TracingConfig.Mode,
-        lambda.revisionid = lf.RevisionId,
-        lambda.state = lf.State,
-        lambda.statereason = lf.StateReason,
-        lambda.statereasoncode = lf.StateReasonCode,
-        lambda.lastupdatestatus = lf.LastUpdateStatus,
-        lambda.lastupdatestatusreason = lf.LastUpdateStatusReason,
-        lambda.lastupdatestatusreasoncode = lf.LastUpdateStatusReasonCode,
-        lambda.packagetype = lf.PackageType,
-        lambda.signingprofileversionarn = lf.SigningProfileVersionArn,
-        lambda.signingjobarn = lf.SigningJobArn,
-        lambda.codesha256 = lf.CodeSha256,
-        lambda.architectures = lf.Architectures,
-        lambda.masterarn = lf.MasterArn,
-        lambda.kmskeyarn = lf.KMSKeyArn,
-        lambda.lastupdated = $aws_update_tag
-        WITH lambda, lf
-        MATCH (owner:AWSAccount{id: $AWS_ACCOUNT_ID})
-        MERGE (owner)-[r:RESOURCE]->(lambda)
-        ON CREATE SET r.firstseen = timestamp()
-        SET r.lastupdated = $aws_update_tag
-        WITH lambda, lf
-        MATCH (role:AWSPrincipal{arn: lf.Role})
-        MERGE (lambda)-[r:STS_ASSUME_ROLE_ALLOW]->(role)
-        ON CREATE SET r.firstseen = timestamp()
-        SET r.lastupdated = $aws_update_tag
+    MERGE (lambda:AWSLambda{id: lf.FunctionArn})
+    ON CREATE SET lambda.firstseen = timestamp()
+    SET lambda.name = lf.FunctionName,
+    lambda.arn = lf.FunctionArn,
+    lambda.region = lf.region,
+    lambda.modifieddate = lf.LastModified,
+    lambda.runtime = lf.Runtime,
+    lambda.anonymous_access = lf.anonymous_access,
+    lambda.anonymous_actions = lf.anonymous_actions,
+    lambda.consolelink = lf.consolelink,
+    lambda.description = lf.Description,
+    lambda.timeout = lf.Timeout,
+    lambda.memory = lf.MemorySize,
+    lambda.codesize = lf.CodeSize,
+    lambda.handler = lf.Handler,
+    lambda.version = lf.Version,
+    lambda.tracingconfigmode = lf.TracingConfig.Mode,
+    lambda.revisionid = lf.RevisionId,
+    lambda.state = lf.State,
+    lambda.statereason = lf.StateReason,
+    lambda.statereasoncode = lf.StateReasonCode,
+    lambda.lastupdatestatus = lf.LastUpdateStatus,
+    lambda.lastupdatestatusreason = lf.LastUpdateStatusReason,
+    lambda.lastupdatestatusreasoncode = lf.LastUpdateStatusReasonCode,
+    lambda.packagetype = lf.PackageType,
+    lambda.signingprofileversionarn = lf.SigningProfileVersionArn,
+    lambda.signingjobarn = lf.SigningJobArn,
+    lambda.codesha256 = lf.CodeSha256,
+    lambda.architectures = lf.Architectures,
+    lambda.masterarn = lf.MasterArn,
+    lambda.kmskeyarn = lf.KMSKeyArn,
+    lambda.lastupdated = $aws_update_tag,
+    lambda.vpc_id = CASE WHEN lf.VpcConfig.VpcId is not null then lf.VpcConfig.VpcId ELSE lambda.vpc_id END
+    WITH lambda, lf
+    MATCH (owner:AWSAccount{id: $AWS_ACCOUNT_ID})
+    MERGE (owner)-[r:RESOURCE]->(lambda)
+    ON CREATE SET r.firstseen = timestamp()
+    SET r.lastupdated = $aws_update_tag
+    WITH lambda, lf
+    MATCH (role:AWSPrincipal{arn: lf.Role})
+    MERGE (lambda)-[r:STS_ASSUME_ROLE_ALLOW]->(role)
+    ON CREATE SET r.firstseen = timestamp()
+    SET r.lastupdated = $aws_update_tag
     """
 
     neo4j_session.run(
         ingest_lambda_functions,
         lambda_functions_list=data,
-        Region=region,
         AWS_ACCOUNT_ID=current_aws_account_id,
         aws_update_tag=aws_update_tag,
     )
@@ -109,36 +150,46 @@ def get_event_source_mappings(lambda_function: Dict, client: botocore.client.Bas
 @timeit
 @aws_handle_regions
 def get_lambda_function_details(
-        boto3_session: boto3.session.Session, data: List[Dict], region: str,
-) -> List[Tuple[str, List[Any], List[Any], List[Any]]]:
-    client = boto3_session.client('lambda', region_name=region)
-    details = []
+        boto3_session: boto3.session.Session, data: List[Dict],
+) -> Generator[Any, Any, None]:
     for lambda_function in data:
+        region = lambda_function['region']
+        client = boto3_session.client('lambda', region_name=region)
         function_aliases = get_function_aliases(lambda_function, client)
         event_source_mappings = get_event_source_mappings(lambda_function, client)
         layers = lambda_function.get('Layers', [])
-        details.append((lambda_function['FunctionArn'], function_aliases, event_source_mappings, layers))
-    return details
+        yield lambda_function['FunctionArn'], function_aliases, event_source_mappings, layers, region
 
 
 @timeit
 def load_lambda_function_details(
         neo4j_session: neo4j.Session, lambda_function_details: List[Tuple[str, List[Dict], List[Dict], List[Dict]]],
-        update_tag: int,
+        update_tag: int, current_aws_account_id: str,
 ) -> None:
     lambda_aliases: List[Dict] = []
     lambda_event_source_mappings: List[Dict] = []
     lambda_layers: List[Dict] = []
-    for function_arn, aliases, event_source_mappings, layers in lambda_function_details:
+    for function_arn, aliases, event_source_mappings, layers, region in lambda_function_details:
         if len(aliases) > 0:
             for alias in aliases:
                 alias['FunctionArn'] = function_arn
+                function_name = function_arn.split(':')[-1]
+                alias['region'] = region
+                alias['consolelink'] = aws_console_link.get_console_link(arn=f"arn:aws:lambda::{current_aws_account_id}:alias/{function_name}")
             lambda_aliases.extend(aliases)
         if len(event_source_mappings) > 0:
+            for event in event_source_mappings:
+                event['FunctionArn'] = function_arn
+                function_name = function_arn.split(':')[-1]
+                event['region'] = region
+                event['consolelink'] = aws_console_link.get_console_link(arn=f"arn:aws:lambda::{current_aws_account_id}:event-source-mapping/{function_name}")
             lambda_event_source_mappings.extend(event_source_mappings)
         if len(layers) > 0:
             for layer in layers:
                 layer['FunctionArn'] = function_arn
+                function_name = function_arn.split(':')[-1]
+                layer['consolelink'] = aws_console_link.get_console_link(arn=f"arn:aws:lambda::{current_aws_account_id}:layer/{function_name}")
+                layer['region'] = region
             lambda_layers.extend(layers)
 
     _load_lambda_function_aliases(neo4j_session, lambda_aliases, update_tag)
@@ -150,18 +201,21 @@ def load_lambda_function_details(
 def _load_lambda_function_aliases(neo4j_session: neo4j.Session, lambda_aliases: List[Dict], update_tag: int) -> None:
     ingest_aliases = """
     UNWIND $aliases_list AS alias
-        MERGE (a:AWSLambdaFunctionAlias{id: alias.AliasArn})
-        ON CREATE SET a.firstseen = timestamp()
-        SET a.aliasname = alias.Name,
-        a.functionversion = alias.FunctionVersion,
-        a.description = alias.Description,
-        a.revisionid = alias.RevisionId,
-        a.lastupdated = $aws_update_tag
-        WITH a, alias
-        MATCH (lambda:AWSLambda{id: alias.FunctionArn})
-        MERGE (lambda)-[r:KNOWN_AS]->(a)
-        ON CREATE SET r.firstseen = timestamp()
-        SET r.lastupdated = $aws_update_tag
+    MERGE (a:AWSLambdaFunctionAlias{id: alias.AliasArn})
+    ON CREATE SET a.firstseen = timestamp()
+    SET a.aliasname = alias.Name,
+    a.region = alias.region,
+    a.functionversion = alias.FunctionVersion,
+    a.description = alias.Description,
+    a.consolelink = alias.consolelink,
+    a.revisionid = alias.RevisionId,
+    a.lastupdated = $aws_update_tag,
+    a.arn = alias.AliasArn
+    WITH a, alias
+    MATCH (lambda:AWSLambda{id: alias.FunctionArn})
+    MERGE (lambda)-[r:KNOWN_AS]->(a)
+    ON CREATE SET r.firstseen = timestamp()
+    SET r.lastupdated = $aws_update_tag
     """
 
     neo4j_session.run(
@@ -177,27 +231,31 @@ def _load_lambda_event_source_mappings(
 ) -> None:
     ingest_esms = """
     UNWIND $esm_list AS esm
-        MERGE (e:AWSLambdaEventSourceMapping{id: esm.UUID})
-        ON CREATE SET e.firstseen = timestamp()
-        SET e.batchsize = esm.BatchSize,
-        e.startingposition = esm.StartingPosition,
-        e.startingpositiontimestamp = esm.StartingPositionTimestamp,
-        e.parallelizationfactor = esm.ParallelizationFactor,
-        e.maximumbatchingwindowinseconds = esm.MaximumBatchingWindowInSeconds,
-        e.eventsourcearn = esm.EventSourceArn,
-        e.lastmodified = esm.LastModified,
-        e.lastprocessingresult = esm.LastProcessingResult,
-        e.state = esm.State,
-        e.maximumrecordage = esm.MaximumRecordAgeInSeconds,
-        e.bisectbatchonfunctionerror = esm.BisectBatchOnFunctionError,
-        e.maximumretryattempts = esm.MaximumRetryAttempts,
-        e.tumblingwindowinseconds = esm.TumblingWindowInSeconds,
-        e.lastupdated = $aws_update_tag
-        WITH e, esm
-        MATCH (lambda:AWSLambda{id: esm.FunctionArn})
-        MERGE (lambda)-[r:RESOURCE]->(e)
-        ON CREATE SET r.firstseen = timestamp()
-        SET r.lastupdated = $aws_update_tag
+    MERGE (e:AWSLambdaEventSourceMapping{id: esm.EventSourceArn})
+    ON CREATE SET e.firstseen = timestamp()
+    SET e.batchsize = esm.BatchSize,
+    e.startingposition = esm.StartingPosition,
+    e.region = esm.region,
+    e.startingpositiontimestamp = esm.StartingPositionTimestamp,
+    e.parallelizationfactor = esm.ParallelizationFactor,
+    e.maximumbatchingwindowinseconds = esm.MaximumBatchingWindowInSeconds,
+    e.eventsourcearn = esm.EventSourceArn,
+    e.UUID = esm.UUID,
+    e.consolelink = esm.consolelink,
+    e.lastmodified = esm.LastModified,
+    e.lastprocessingresult = esm.LastProcessingResult,
+    e.state = esm.State,
+    e.maximumrecordage = esm.MaximumRecordAgeInSeconds,
+    e.bisectbatchonfunctionerror = esm.BisectBatchOnFunctionError,
+    e.maximumretryattempts = esm.MaximumRetryAttempts,
+    e.tumblingwindowinseconds = esm.TumblingWindowInSeconds,
+    e.lastupdated = $aws_update_tag,
+    e.arn = esm.EventSourceArn
+    WITH e, esm
+    MATCH (lambda:AWSLambda{id: esm.FunctionArn})
+    MERGE (lambda)-[r:RESOURCE]->(e)
+    ON CREATE SET r.firstseen = timestamp()
+    SET r.lastupdated = $aws_update_tag
     """
 
     neo4j_session.run(
@@ -208,20 +266,24 @@ def _load_lambda_event_source_mappings(
 
 
 @timeit
-def _load_lambda_layers(neo4j_session: neo4j.Session, lambda_layers: List[Dict], update_tag: int) -> None:
+def _load_lambda_layers(neo4j_session: neo4j.Session, lambda_layers: List[Dict], update_tag: int,) -> None:
     ingest_layers = """
     UNWIND $layers_list AS layer
-        MERGE (l:AWSLambdaLayer{id: layer.Arn})
-        ON CREATE SET l.firstseen = timestamp()
-        SET l.codesize = layer.CodeSize,
-        l.signingprofileversionarn  = layer.SigningProfileVersionArn,
-        l.signingjobarn = layer.SigningJobArn,
-        l.lastupdated = $aws_update_tag
-        WITH l, layer
-        MATCH (lambda:AWSLambda{id: layer.FunctionArn})
-        MERGE (lambda)-[r:HAS]->(l)
-        ON CREATE SET r.firstseen = timestamp()
-        SET r.lastupdated = $aws_update_tag
+    MERGE (l:AWSLambdaLayer{id: layer.Arn})
+    ON CREATE SET l.firstseen = timestamp()
+    SET l.codesize = layer.CodeSize,
+    l.region = layer.region,
+    l.name = layer.LayerName,
+    l.consolelink = layer.consolelink,
+    l.signingprofileversionarn  = layer.SigningProfileVersionArn,
+    l.signingjobarn = layer.SigningJobArn,
+    l.lastupdated = $aws_update_tag,
+    l.arn = layer.Arn
+    WITH l, layer
+    MATCH (lambda:AWSLambda{id: layer.FunctionArn})
+    MERGE (lambda)-[r:HAS]->(l)
+    ON CREATE SET r.firstseen = timestamp()
+    SET r.lastupdated = $aws_update_tag
     """
 
     neo4j_session.run(
@@ -241,12 +303,18 @@ def sync_lambda_functions(
         neo4j_session: neo4j.Session, boto3_session: boto3.session.Session, regions: List[str],
         current_aws_account_id: str, aws_update_tag: int, common_job_parameters: Dict,
 ) -> None:
+    data = []
     for region in regions:
         logger.info("Syncing Lambda for region in '%s' in account '%s'.", region, current_aws_account_id)
-        data = get_lambda_data(boto3_session, region)
-        load_lambda_functions(neo4j_session, data, region, current_aws_account_id, aws_update_tag)
-        lambda_function_details = get_lambda_function_details(boto3_session, data, region)
-        load_lambda_function_details(neo4j_session, lambda_function_details, aws_update_tag)
+        lambdas = get_lambda_data(boto3_session, region)
+        lambdas = get_lambda_policies(boto3_session, region, lambdas)
+        data.extend(lambdas)
+
+    logger.info(f"Total Lambdas: {len(data)}")
+
+    load_lambda_functions(neo4j_session, data, current_aws_account_id, aws_update_tag)
+    lambda_function_details = get_lambda_function_details(boto3_session, data)
+    load_lambda_function_details(neo4j_session, lambda_function_details, aws_update_tag, current_aws_account_id)
 
     cleanup_lambda(neo4j_session, common_job_parameters)
 
@@ -256,6 +324,13 @@ def sync(
         neo4j_session: neo4j.Session, boto3_session: boto3.session.Session, regions: List[str],
         current_aws_account_id: str, update_tag: int, common_job_parameters: Dict,
 ) -> None:
+    tic = time.perf_counter()
+
+    logger.info("Syncing Lambda for account '%s', at %s.", current_aws_account_id, tic)
+
     sync_lambda_functions(
         neo4j_session, boto3_session, regions, current_aws_account_id, update_tag, common_job_parameters,
     )
+
+    toc = time.perf_counter()
+    logger.info(f"Time to process Lambda: {toc - tic:0.4f} seconds")
