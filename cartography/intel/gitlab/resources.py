b7185936a1af9132d61de883074fe9d35e10d6b1
Apple <apple@Apples-MacBook-Air.local>
2024-04-01 13:28:00 +0530

    resource added

--- cartography/intel/gitlab/resources.py
+++ cartography/intel/gitlab/resources.py
@@ -3,10 +3,13 @@ from typing import Dict
 from . import members
 from . import projects
 from . import repositories
+from . import dependencies
 
 
 RESOURCE_FUNCTIONS: Dict = {
     'members': members.sync,
     'projects': projects.sync,
     'repositories': repositories.sync,
+    'dependencies': dependencies.sync,
+
 }
