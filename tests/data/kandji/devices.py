98b9141dcd9205d86b4d29c686cf8f2676361024
chandanchowdhury <chandan.chowdhury@hotmail.com>
2024-04-05 20:22:44 -0400

    feat: add test

---
+++ tests/data/kandji/devices.py
@@ -0,0 +1,22 @@
+DEVICES = {
+    'devices': [
+        {
+            "device_id": "fc60decb-30cb-4db1-b3ec-2fa8ea1b83ed",
+            "device_name": "Test MacBook Air",
+            "model": "MacBook Air (M1, 2020)",
+            "serial_number": "AAAAAAAA",
+            "platform": "Mac",
+            "os_version": "13.4",
+            "last_check_in": "2021-03-30T17:46:31.864498Z",
+        },
+        {
+            "device_id": "f27bcd08-f653-4930-83b0-51970e923b98",
+            "device_name": "Test MacBook Pro",
+            "model": "MacBook Pro",
+            "serial_number": "BBBBBBBB",
+            "platform": "Mac",
+            "os_version": "14.4.1",
+            "last_check_in": "2024-04-01T17:46:31.864498Z",
+        },
+    ],
+}
---
+++ tests/integration/cartography/intel/kandji/test_kandji.py
@@ -0,0 +1,27 @@
+import cartography.intel.kandji
+import tests.data.kandji.devices
+
+TEST_UPDATE_TAG = 123456789
+
+
+def test_load_kandji_devices_data(neo4j_session):
+    _ensure_local_neo4j_has_test_kandjidevice_data(neo4j_session)
+
+    # Test that the node was created
+    expected_nodes = {
+        'fc60decb-30cb-4db1-b3ec-2fa8ea1b83ed',
+        'f27bcd08-f653-4930-83b0-51970e923b98',
+    }
+    nodes = neo4j_session.run(
+        """
+        MATCH (d:KandjiDevice) RETURN d.id;
+        """,
+    )
+    actual_nodes = {n['d.id'] for n in nodes}
+    assert actual_nodes == expected_nodes
+
+
+def _ensure_local_neo4j_has_test_kandjidevice_data(neo4j_session):
+    """Pre-load the Neo4j instance with test data"""
+    devices = tests.data.kandji.devices.DEVICES['devices']
+    cartography.intel.kandji.devices._load_devices(devices, neo4j_session, TEST_UPDATE_TAG)
