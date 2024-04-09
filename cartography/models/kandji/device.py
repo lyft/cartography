e8ccb1999011003d68f5dfe6d875614be2350025
chandanchowdhury <chandan.chowdhury@hotmail.com>
2024-04-09 13:16:16 -0400

    fix: change code order

--- cartography/models/kandji/device.py
+++ cartography/models/kandji/device.py
@@ -3,18 +3,6 @@ from dataclasses import dataclass
 from cartography.models.core.common import PropertyRef
 from cartography.models.core.nodes import CartographyNodeProperties
 from cartography.models.core.nodes import CartographyNodeSchema
-from cartography.models.core.relationships import CartographyRelProperties
-from cartography.models.core.relationships import CartographyRelSchema
-from cartography.models.core.relationships import LinkDirection
-from cartography.models.core.relationships import make_target_node_matcher
-from cartography.models.core.relationships import OtherRelationships
-from cartography.models.core.relationships import TargetNodeMatcher
-
-
-@dataclass(frozen=True)
-class KandjiDeviceSchema(CartographyNodeSchema):
-    label: str = 'KandjiDevice'  # The label of the node
-    properties: KandjiDeviceNodeProperties = KandjiDeviceNodeProperties()  # An object representing all properties
 
 
 @dataclass(frozen=True)
@@ -27,3 +15,9 @@ class KandjiDeviceNodeProperties(CartographyNodeProperties):
     platform: PropertyRef = PropertyRef('platform')
     os_version: PropertyRef = PropertyRef('os_version')
     last_check_in: PropertyRef = PropertyRef('last_check_in')
+
+
+@dataclass(frozen=True)
+class KandjiDeviceSchema(CartographyNodeSchema):
+    label: str = 'KandjiDevice'  # The label of the node
+    properties: KandjiDeviceNodeProperties = KandjiDeviceNodeProperties()  # An object representing all properties
