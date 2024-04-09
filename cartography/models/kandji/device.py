ac8368ad9ed60a57efdcca1360cb82407545f216
chandanchowdhury <chandan.chowdhury@hotmail.com>
2024-04-09 12:33:53 -0400

    feat: adding Kandji model

---
+++ cartography/models/kandji/device.py
@@ -0,0 +1,23 @@
+from dataclasses import dataclass
+
+from cartography.models.core.common import PropertyRef
+from cartography.models.core.nodes import CartographyNodeProperties
+from cartography.models.core.nodes import CartographyNodeSchema
+from cartography.models.core.relationships import CartographyRelProperties
+from cartography.models.core.relationships import CartographyRelSchema
+from cartography.models.core.relationships import LinkDirection
+from cartography.models.core.relationships import make_target_node_matcher
+from cartography.models.core.relationships import OtherRelationships
+from cartography.models.core.relationships import TargetNodeMatcher
+
+
+@dataclass(frozen=True)
+class KandjiDeviceNodeProperties(CartographyNodeProperties):
+    id: PropertyRef = PropertyRef('id')
+    lastupdated: PropertyRef = PropertyRef('lastupdated')
+    serial_number: PropertyRef = PropertyRef('serial_number')
+    device_name: PropertyRef = PropertyRef('device_name')
+    model: PropertyRef = PropertyRef('device.model')
+    platform: PropertyRef = PropertyRef('platform')
+    os_version: PropertyRef = PropertyRef('os_version')
+    last_check_in: PropertyRef = PropertyRef('last_check_in')
