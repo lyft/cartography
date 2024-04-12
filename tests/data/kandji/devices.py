008747162900f864a919b8ffdb80756384759dd5
chandanchowdhury <chandan.chowdhury@hotmail.com>
2024-04-11 20:06:15 -0400

    feat: adding tests

---
+++ tests/data/kandji/devices.py
@@ -0,0 +1,22 @@
+DEVICES = {
+    'devices': [
+        {
+            "device_id": "fc60decb-30cb-4db1-b3ec-2fa8ea1b83ed",
+            "device_name": "Test MacBook Air",
+            "last_check_in": "2021-03-30T17:46:31.864498Z",
+            "model": "MacBook Air (M1, 2020)",
+            "os_version": "13.4",
+            "platform": "Mac",
+            "serial_number": "AAAAAAAA",
+        },
+        {
+            "device_id": "f27bcd08-f653-4930-83b0-51970e923b98",
+            "device_name": "Test MacBook Pro",
+            "last_check_in": "2024-04-01T17:46:31.864498Z",
+            "model": "MacBook Pro",
+            "os_version": "14.4.1",
+            "platform": "Mac",
+            "serial_number": "BBBBBBBB",
+        },
+    ],
+}
