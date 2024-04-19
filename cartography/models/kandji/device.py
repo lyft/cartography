ca85571d5f1d5ca60c2cbec094bb035ea781cfdd
chandanchowdhury <chandan.chowdhury@hotmail.com>
2024-04-19 18:10:04 -0500

    feat: update model with tenant relationship

--- cartography/models/kandji/device.py
+++ cartography/models/kandji/device.py
@@ -3,12 +3,18 @@ from dataclasses import dataclass
 from cartography.models.core.common import PropertyRef
 from cartography.models.core.nodes import CartographyNodeProperties
 from cartography.models.core.nodes import CartographyNodeSchema
+from cartography.models.core.relationships import CartographyRelProperties
+from cartography.models.core.relationships import CartographyRelSchema
+from cartography.models.core.relationships import LinkDirection
+from cartography.models.core.relationships import make_target_node_matcher
+from cartography.models.core.relationships import TargetNodeMatcher
 
 
 @dataclass(frozen=True)
 class KandjiDeviceNodeProperties(CartographyNodeProperties):
     id: PropertyRef = PropertyRef('id')
     lastupdated: PropertyRef = PropertyRef('lastupdated')
+
     device_id: PropertyRef = PropertyRef('device_id')
     device_name: PropertyRef = PropertyRef('device_name')
     last_check_in: PropertyRef = PropertyRef('last_check_in')
@@ -19,6 +25,24 @@ class KandjiDeviceNodeProperties(CartographyNodeProperties):
 
 
 @dataclass(frozen=True)
+class KandjiTenantToKandjiDeviceRelProperties(CartographyRelProperties):
+    lastupdated: PropertyRef = PropertyRef('lastupdated', set_in_kwargs=True)
+
+
+@dataclass(frozen=True)
+# (:KandjiTenant)<-[:DEVICE]-(:KandjiDevice)
+class KandjiTenantToKandjiDeviceRel(CartographyRelSchema):
+    target_node_label: str = 'KandjiTenant'
+    target_node_matcher: TargetNodeMatcher = make_target_node_matcher(
+        {'id': PropertyRef('TENANT_ID', set_in_kwargs=True)},
+    )
+    direction: LinkDirection = LinkDirection.OUTWARD
+    rel_label: str = "ENROLLED_TO"
+    properties: KandjiTenantToKandjiDeviceRelProperties = KandjiTenantToKandjiDeviceRelProperties()
+
+
+@dataclass(frozen=True)
 class KandjiDeviceSchema(CartographyNodeSchema):
     label: str = 'KandjiDevice'  # The label of the node
     properties: KandjiDeviceNodeProperties = KandjiDeviceNodeProperties()  # An object representing all properties
+    sub_resource_relationship: KandjiTenantToKandjiDeviceRel = KandjiTenantToKandjiDeviceRel()
