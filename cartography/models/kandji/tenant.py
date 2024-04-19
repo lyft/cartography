2490113c1cc7ac15213338f975a5a4f5739457ed
chandanchowdhury <chandan.chowdhury@hotmail.com>
2024-04-19 18:04:08 -0500

    feat: add kandji tenant model

---
+++ cartography/models/kandji/tenant.py
@@ -0,0 +1,16 @@
+from dataclasses import dataclass
+
+from cartography.models.core.common import PropertyRef
+from cartography.models.core.nodes import CartographyNodeProperties
+from cartography.models.core.nodes import CartographyNodeSchema
+
+
+@dataclass(frozen=True)
+class KandjiTenantNodeProperties(CartographyNodeProperties):
+    id: PropertyRef = PropertyRef('id')
+    lastupdated: PropertyRef = PropertyRef('lastupdated')
+
+@dataclass(frozen=True)
+class KandjiTenantSchema(CartographyNodeSchema):
+    label: str = 'KandjiTenant'  # The label of the node
+    properties: KandjiTenantNodeProperties = KandjiTenantNodeProperties()  # An object representing all properties
